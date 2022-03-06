953d86a12f4698efdb56675e278345ed84e2bc20
Reza Rahman <rezrah@github.com>
2022-03-06 17:56:30 +0000

    switch from  esbuild to webpack

---
+++ src/Hero/Hero.module.css.d.ts
@@ -0,0 +1,6 @@
+declare const styles: {
+  readonly "hero": string;
+  readonly "heading": string;
+};
+export = styles;
+
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+
+  output: {
+    path: path.resolve(__dirname, 'lib'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
+    publicPath: 'lib'
+  },
+  externals: {
+    react: 'react'
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.build.json')
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[name]__[local]___[hash:base64:5]'
+              },
+              importLoaders: 1
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [['postcss-preset-env', 'autoprefixer']]
+              }
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.css']
+  }
+}
