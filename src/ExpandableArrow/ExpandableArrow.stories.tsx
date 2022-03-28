50210dce43320dd89650a784ad195a5a08e42c50
Cole Bemis <colebemis@github.com>
2022-03-28 12:18:03 -0700

    Create ExpandableArrow component

---
+++ src/ExpandableArrow/ExpandableArrow.stories.tsx
@@ -0,0 +1,29 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+import React from 'react'
+import {ExpandableArrow} from '.'
+
+export default {
+  title: 'Components/ExpandableArrow',
+  component: ExpandableArrow
+} as ComponentMeta<typeof ExpandableArrow>
+
+const Template: ComponentStory<typeof ExpandableArrow> = args => <ExpandableArrow {...args} />
+
+export const Default = Template.bind({})
+Default.args = {
+  expanded: false
+}
+
+export const Expanded = Template.bind({})
+Expanded.args = {
+  expanded: true
+}
+
+export const ExpandOnHover = () => {
+  const [isHovered, setIsHovered] = React.useState(false)
+  return (
+    <div style={{cursor: 'pointer'}} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <ExpandableArrow expanded={isHovered} />
+    </div>
+  )
+}
