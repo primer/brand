933a4a649a16b1fe1340f2564136c2844478794a
Cole Bemis <colebemis@github.com>
2022-03-30 13:51:02 -0700

    Create `ExpandableArrow` component (#13)
    * Create ExpandableArrow component
    
    * Use ExpandableArrow in LinkButton
    
    * Add JSDoc comment to `expanded` prop

--- src/LinkButton/LinkButton.tsx
+++ src/LinkButton/LinkButton.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx'
 import React from 'react'
+import {ExpandableArrow} from '../ExpandableArrow'
 import styles from './LinkButton.module.css'
 
 // NOTE: We may rename this component `LinkStyledAsButton` or consolidate it into
@@ -11,6 +12,41 @@ export type LinkButtonProps = {
   variant?: 'primary' | 'secondary'
 } & React.ComponentPropsWithoutRef<'a'>
 
-export function LinkButton({variant = 'secondary', className, ...props}: LinkButtonProps) {
-  return <a className={clsx(styles.LinkButton, styles[`LinkButton--${variant}`], className)} {...props} />
+export function LinkButton({
+  variant = 'secondary',
+  className,
+  children,
+  onMouseEnter,
+  onMouseLeave,
+  onFocus,
+  onBlur,
+  ...props
+}: LinkButtonProps) {
+  const [isHovered, setIsHovered] = React.useState(false)
+  const [isFocused, setIsFocused] = React.useState(false)
+  return (
+    <a
+      className={clsx(styles.LinkButton, styles[`LinkButton--${variant}`], className)}
+      onMouseEnter={event => {
+        setIsHovered(true)
+        onMouseEnter?.(event)
+      }}
+      onMouseLeave={event => {
+        setIsHovered(false)
+        onMouseLeave?.(event)
+      }}
+      onFocus={event => {
+        setIsFocused(true)
+        onFocus?.(event)
+      }}
+      onBlur={event => {
+        setIsFocused(false)
+        onBlur?.(event)
+      }}
+      {...props}
+    >
+      {children}
+      <ExpandableArrow className={styles['LinkButton-arrow']} expanded={isHovered || isFocused} />
+    </a>
+  )
 }
