6bd641466b7d6aab432c86b81e091a95607897b9
Reza Rahman <rezrah@github.com>
2022-05-03 15:35:36 +0100

    normalise heading and text scales

--- src/stylesheets.ts
+++ src/stylesheets.ts
@@ -5,6 +5,5 @@
 // Primitives
 import '../lib/css/gh-variables.color.css'
 import '../lib/design-tokens/css/tokens/base/size/size.css'
-import '../lib/design-tokens/css/tokens/base/typography/typography.css'
 import '../lib/design-tokens/css/tokens/functional/typography/typography-responsive.css'
 import '../lib/design-tokens/css/tokens/functional/size/breakpoints.css'
