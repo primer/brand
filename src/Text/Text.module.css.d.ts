5cc2663d785e41073d2654861a12005d6dab90d3
Reza Rahman <rezrah@github.com>
2022-05-05 14:04:03 +0100

    fix camelCasing in token names

--- src/Text/Text.module.css.d.ts
+++ src/Text/Text.module.css.d.ts
@@ -1,5 +1,7 @@
 declare const styles: {
   readonly "Text": string;
+  readonly "Text--default": string;
+  readonly "Text--muted": string;
   readonly "Text--700": string;
   readonly "Text--600": string;
   readonly "Text--500": string;
