063b2a88e5864d71af90df3067fbc4c44afe2db4
Reza Rahman <rezrah@github.com>
2022-05-09 11:48:59 +0100

    fix tests

--- src/Heading/Heading.test.tsx
+++ src/Heading/Heading.test.tsx
@@ -1,7 +1,7 @@
 import React, {render, cleanup} from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-import {Heading, defaultHeadingTag} from './Heading'
+import {Heading} from './Heading'
 import {axe, toHaveNoViolations} from 'jest-axe'
 
 expect.extend(toHaveNoViolations)
@@ -25,33 +25,23 @@ describe('Heading', () => {
     expect(results).toHaveNoViolations()
   })
 
-  test('renders default tag correctly', () => {
+  test('renders default tag and size correctly', () => {
+    const expectedClass = 'Heading--2'
+    const expectedTag = 'h2'
     const {getByRole} = render(<Heading>{mockHeading}</Heading>)
     const headingEl = getByRole('heading')
 
-    expect(headingEl.tagName).toBe(defaultHeadingTag.toUpperCase())
-  })
-
-  test('renders alternative tags correctly', () => {
-    const {getByRole} = render(<Heading as="h1">{mockHeading}</Heading>)
-    const headingEl = getByRole('heading')
-
-    expect(headingEl.tagName).toBe('H1')
-  })
-
-  test('renders the correct size for the default tag', () => {
-    const expectedClass = 'Heading--1'
-    const {getByRole} = render(<Heading>{mockHeading}</Heading>)
-    const headingEl = getByRole('heading')
-
+    expect(headingEl.tagName).toBe(expectedTag.toUpperCase())
     expect(headingEl.classList).toContain(expectedClass)
   })
 
-  test('renders the correct size for alternative tags', () => {
-    const expectedClass = 'Heading--0'
-    const {getByRole} = render(<Heading as="h1">{mockHeading}</Heading>)
+  test('renders alternative tag correctly with corresponding size', () => {
+    const expectedTag = 'h1'
+    const expectedClass = 'Heading--1'
+    const {getByRole} = render(<Heading as={expectedTag}>{mockHeading}</Heading>)
     const headingEl = getByRole('heading')
 
+    expect(headingEl.tagName).toBe(expectedTag.toUpperCase())
     expect(headingEl.classList).toContain(expectedClass)
   })
 })
