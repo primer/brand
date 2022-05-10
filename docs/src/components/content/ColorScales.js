2f2894aa21b6afa6164d1d87ff797a2ad492b220
Reza Rahman <rezrah@github.com>
2022-05-10 21:51:35 +0100

    add black and white

--- docs/src/components/content/ColorScales.js
+++ docs/src/components/content/ColorScales.js
@@ -13,6 +13,46 @@ import {ColorModesEnum} from '../../../../src'
 export function ColorScales() {
   const availableColorModes = Object.values(ColorModesEnum)
   const [colorTheme, setCurrentMode] = React.useState(ColorModesEnum.LIGHT)
+  const predicateFn = (colorEntry) => ['black', 'white'].includes(colorEntry[0])
+
+  const rgbScales = Object.entries(colors.base.color.scale).filter(
+    (color) => !predicateFn(color),
+  )
+
+  const blackWhiteScales = Object.entries(colors.base.color.scale).filter(
+    (color) => predicateFn(color),
+  )
+
+  const renderScale = (scale) => {
+    const [name, colorScale] = scale
+    return (
+      <Box key={name}>
+        {Object.entries(colorScale).map(([key, obj]) => {
+          const value = colorTheme === 'dark' ? obj.darkValue : obj.value
+          return (
+            <Box
+              key={`${key}-${value}`}
+              sx={{
+                color: readableColor(value),
+                bg: value,
+                p: 2,
+                display: 'flex',
+                justifyContent: 'space-between',
+                fontFamily: 'mono',
+                fontSize: 1,
+              }}
+            >
+              <Text>
+                {name}.{key}
+              </Text>
+              <Text>{value}</Text>
+            </Box>
+          )
+        })}
+      </Box>
+    )
+  }
+
   return (
     <PRCThemeProvider
       colorMode={
@@ -23,7 +63,16 @@ export function ColorScales() {
           : ColorModesEnum.AUTO
       }
     >
-      <Box pt={2} px={2} display="flex" justifyContent="flex-end">
+      <Box
+        sx={{
+          paddingTop: 2,
+          paddingLeft: 2,
+          paddingRight: 2,
+          marginBottom: 3,
+          display: 'flex',
+          justifyContent: 'flex-end',
+        }}
+      >
         <ActionMenu>
           <ActionMenu.Button>{colorTheme}</ActionMenu.Button>
 
@@ -52,37 +101,24 @@ export function ColorScales() {
           p: 3,
           boxShadow: 'inset 0 0 0 1px rgba(0,0,0,0.1)',
           borderRadius: 2,
+          backgroundColor: 'canvas.default',
+          marginBottom: 2,
         }}
       >
-        {Object.entries(colors.base.color.scale).map((scale) => {
-          const [name, colorScale] = scale
-          return (
-            <Box key={name}>
-              {Object.entries(colorScale).map(([key, obj]) => {
-                const value = colorTheme === 'dark' ? obj.darkValue : obj.value
-                return (
-                  <Box
-                    key={`${key}-${value}`}
-                    sx={{
-                      color: readableColor(value),
-                      bg: value,
-                      p: 2,
-                      display: 'flex',
-                      justifyContent: 'space-between',
-                      fontFamily: 'mono',
-                      fontSize: 1,
-                    }}
-                  >
-                    <Text>
-                      {name}.{key}
-                    </Text>
-                    <Text>{value}</Text>
-                  </Box>
-                )
-              })}
-            </Box>
-          )
-        })}
+        {rgbScales.map(renderScale)}
+      </Box>
+      <Box
+        sx={{
+          display: 'grid',
+          gridGap: 3,
+          gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
+          p: 3,
+          boxShadow: 'inset 0 0 0 1px rgba(0,0,0,0.1)',
+          borderRadius: 2,
+          backgroundColor: 'canvas.default',
+        }}
+      >
+        {blackWhiteScales.map(renderScale)}
       </Box>
     </PRCThemeProvider>
   )
