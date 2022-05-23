a55a77925ac75b3217ac3a8b5bc60fcacd3525e8
Reza Rahman <rezrah@github.com>
2022-05-23 12:46:01 +0100

    fix mona-sans playground debounce bugs

--- src/MonaSans/MonaSans.stories.tsx
+++ src/MonaSans/MonaSans.stories.tsx
@@ -6,20 +6,12 @@ export default {
   title: 'Misc/MonaSans',
   component: MonaSans,
   parameters: {
-    backgrounds: {
-      grid: {
-        cellSize: 20,
-        opacity: 0.5,
-        cellAmount: 5,
-        offsetX: 16,
-        offsetY: 16
-      }
-    }
+    layout: 'centered'
   },
   argTypes: {
-    size: {control: {type: 'range', min: 9, max: 128, step: 1}, defaultValue: 64},
-    weight: {control: {type: 'range', min: 200, max: 900, step: 1}, defaultValue: 900},
-    width: {control: {type: 'range', min: 75, max: 125, step: 1}},
+    size: {control: {type: 'range', min: 9, max: 128, step: 4}, defaultValue: 64},
+    weight: {control: {type: 'range', min: 200, max: 900, step: 10}, defaultValue: 900},
+    width: {control: {type: 'range', min: 75, max: 125, step: 5}},
     italic: {control: {type: 'range', min: 1, max: 10, step: 1}}
   }
 } as ComponentMeta<typeof MonaSans>
--- src/MonaSans/MonaSans.tsx
+++ src/MonaSans/MonaSans.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Text} from '../'
 import styles from './MonaSans.module.css'
 
 type MonaSansProps = {
@@ -19,14 +20,17 @@ export function MonaSans({children, italic, size, weight, width}: MonaSansProps)
     fontVariationSettings: `${fontVariationSettings}`
   }
   const grid = `
-  
+  body {
+    background-image: url("data:image/svg+xml,%3Csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3E%3Cdefs%3E%3Cpattern id='smallGrid' width='10' height='10' patternUnits='userSpaceOnUse'%3E%3Cpath d='M 10 0 L 0 0 0 10' fill='none' stroke='gray' stroke-width='0.5'/%3E%3C/pattern%3E%3Cpattern id='grid' width='100' height='100' patternUnits='userSpaceOnUse'%3E%3Crect width='100' height='100' fill='url(%23smallGrid)'/%3E%3Cpath d='M 100 0 L 0 0 0 100' fill='none' stroke='gray' stroke-width='1'/%3E%3C/pattern%3E%3C/defs%3E%3Crect width='100%25' height='100%25' fill='url(%23grid)' /%3E%3C/svg%3E");
+    background-position: -2px -2px;
+  }
 `
   return (
     <>
       <style>{grid}</style>
-      <p className={styles.MonaSans} style={style}>
+      <Text className={styles.MonaSans} style={style} as="p">
         {children}
-      </p>
+      </Text>
     </>
   )
 }
