5b138ec10d6f222fabaf094c216ee810789dbe7e
Reza Rahman <rezrah@github.com>
2022-06-17 11:20:21 +0100

    add accent tokens

--- src/FAQ/FAQ.module.css.d.ts
+++ src/FAQ/FAQ.module.css.d.ts
@@ -1,7 +1,6 @@
 declare const styles: {
   readonly "FAQ": string;
   readonly "FAQ__subheading": string;
-  readonly "FAQ__heading": string;
   readonly "FAQ__heading--left": string;
   readonly "FAQ__heading--center": string;
   readonly "FAQ__heading--large": string;
