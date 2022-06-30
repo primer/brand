3b9c3e0b3934bcd2c2c6cf2d0005cf7c135384e7
Reza Rahman <rezrah@github.com>
2022-06-30 17:23:54 +0100

    add additional states for InlineLink

--- src/FAQ/FAQ.module.css.d.ts
+++ src/FAQ/FAQ.module.css.d.ts
@@ -4,6 +4,7 @@ declare const styles: {
   readonly "FAQ__subheading": string;
   readonly "FAQ__heading--start": string;
   readonly "FAQ__heading--center": string;
+  readonly "FAQ__heading--large": string;
   readonly "FAQ__heading--with-margin": string;
 };
 export = styles;
