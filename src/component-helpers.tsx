5ef49fdafcc051fae9e329b0ffaa3d6b95ae1ca2
Ricardo Fearing <9965014+rfearing@users.noreply.github.com>
2022-08-08 18:28:30 +0000

    minor: update code comment

--- src/component-helpers.tsx
+++ src/component-helpers.tsx
@@ -11,12 +11,12 @@ export const Container = ({children}) => <div style={{maxWidth: 1024, margin: '0
 
 /**
  * Component helper type to be extended by component types, e.g.:
- * type CompNameProps = BaseProps<HTMLDivElement> & { ... }
+ * type CustomComponentProps = BaseProps<HTMLDivElement> & { ... }
  *
  * Example use:
- *   const CompName = forwardRef<HTMLDivElement, CompNameProps>(({className, ...props}, ref) => { ... })
+ *   const CustomComponent = forwardRef<HTMLDivElement, CustomComponentProps>(({className, ...props}, ref) => { ... })
  *   // OR:
- *   const CompName = forwardRef(({className: CompNameProps, ...props}, ref: Ref<HTMLDivElement>) => { ... })
+ *   const CustomComponent = forwardRef(({className: CustomComponentProps, ...props}, ref: Ref<HTMLDivElement>) => { ... })
  */
 export type BaseProps<T> = {
   className?: string
