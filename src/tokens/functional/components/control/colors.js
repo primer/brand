d7e94d71426d10a77a401d1c9d4e8cd5dba28ba4
Reza Rahman <rezrah@github.com>
2022-09-13 11:25:38 +0100

    refactor style dictionary color formatter to support n themes

--- src/tokens/functional/components/control/colors.js
+++ src/tokens/functional/components/control/colors.js
@@ -5,27 +5,27 @@ module.exports = {
         border: {
           default: {
             value: "var(--base-color-scale-gray-4)",
-            darkValue: "var(--base-color-scale-gray-4)"
+            dark: "var(--base-color-scale-gray-4)"
           },
           disabled: {
             value: "var(--base-color-scale-gray-2)",
-            darkValue: "var(--base-color-scale-gray-5)"
+            dark: "var(--base-color-scale-gray-5)"
           }
         },
         canvas: {
           disabled: {
             value: "var(--base-color-scale-gray-1)",
-            darkValue: "var(--base-color-scale-gray-8)"
+            dark: "var(--base-color-scale-gray-8)"
           }
         },
         fg: {
           default: {
             value: "var(--brand-color-text-default)",
-            darkValue: "var(--brand-color-text-default)"
+            dark: "var(--brand-color-text-default)"
           },
           disabled: {
             value: "var(--base-color-scale-gray-4)",
-            darkValue: "var(--base-color-scale-gray-5)"
+            dark: "var(--base-color-scale-gray-5)"
           }
         }
       },
@@ -33,43 +33,43 @@ module.exports = {
         bg: {
           default: {
             value: "var(--base-color-scale-white-0)",
-            darkValue: "var(--base-color-scale-gray-7)"
+            dark: "var(--base-color-scale-gray-7)"
           },
           checked: {
             value: "var(--base-color-scale-black-0)",
-            darkValue: "var(--base-color-scale-white-0)"
+            dark: "var(--base-color-scale-white-0)"
           },
           indeterminate: {
             value: "var(--base-color-scale-black-0)",
-            darkValue: "var(--base-color-scale-white-0)"
+            dark: "var(--base-color-scale-white-0)"
           },
           disabled: {
             value: "var(--base-color-scale-gray-1)",
-            darkValue: "var(--base-color-scale-gray-4)"
+            dark: "var(--base-color-scale-gray-4)"
           }
         },
         fg: {
           checked: {
             value: "var(--base-color-scale-white-0)",
-            darkValue: "var(--base-color-scale-black-0)"
+            dark: "var(--base-color-scale-black-0)"
           }
         },
         border: {
           default: {
             value: "var(--base-color-scale-gray-4)",
-            darkValue: "var(--base-color-scale-gray-4)"
+            dark: "var(--base-color-scale-gray-4)"
           },
           checked: {
             value: "var(--base-color-scale-black-0)",
-            darkValue: "var(--base-color-scale-white-0)"
+            dark: "var(--base-color-scale-white-0)"
           },
           indeterminate: {
             value: "var(--base-color-scale-black-0)",
-            darkValue: "var(--base-color-scale-white-0)"
+            dark: "var(--base-color-scale-white-0)"
           },
           disabled: {
             value: "var(--base-color-scale-gray-2)",
-            darkValue: "var(--base-color-scale-gray-4)"
+            dark: "var(--base-color-scale-gray-4)"
           }
         }
       }
