d58aa86f290e3297f91d303d4dc305129385d130
Joshua Bowden <jbowden@Joshuas-MBP.localdomain>
2022-10-27 13:07:55 -0400

    updated component - simplified button reduction - removed Stack in favor of custom CSS

--- packages/react/src/ButtonGroup/ButtonGroup.tsx
+++ packages/react/src/ButtonGroup/ButtonGroup.tsx
@@ -3,31 +3,23 @@ import React, {forwardRef, type Ref} from 'react'
 import '@primer/brand-primitives/lib/design-tokens/css/tokens/functional/components/button/colors-with-modes.css'
 import type {BaseProps} from '../component-helpers'
 import {Button, ButtonProps} from '../Button'
-import {Stack, StackProps} from '../Stack'
 import styles from './ButtonGroup.module.css'
 
 // TODO: Use generics properly to accept any type of button acceptable
 // Using 'size' over 'buttonSize' causes an issue of the value not being passed to the clone children
 export type ButtonGroupProps = BaseProps<HTMLDivElement> & {
   children: React.ReactElement<ButtonProps<'button'>>[]
-  stackDirection?: StackProps['direction']
+  direction?: 'horizontal' | 'vertical'
   buttonSize?: ButtonProps<'button'>['size']
   buttonsAs?: React.ElementType
 }
 
 export const ButtonGroup = forwardRef(
   (
-    {stackDirection = 'horizontal', buttonSize = 'medium', buttonsAs, className, children, ...props}: ButtonGroupProps,
+    {direction = 'horizontal', buttonSize = 'medium', buttonsAs, className, children, ...props}: ButtonGroupProps,
     ref: Ref<HTMLDivElement>
   ) => {
-    // I would prefer not to have to loop twice for this
-    const onlyButtons = React.Children.toArray(children).map(child => {
-      if (React.isValidElement(child) && typeof child.type !== 'string' && child.type === Button) {
-        return child
-      }
-    })
-    const buttonsToRender = React.Children.toArray(onlyButtons)
-      .slice(0, 2)
+    const buttonsToRender = React.Children.toArray(children)
       .map((child, index) => {
         if (React.isValidElement(child) && typeof child.type !== 'string' && child.type === Button) {
           return React.cloneElement(child, {
@@ -39,11 +31,16 @@ export const ButtonGroup = forwardRef(
           } as ButtonProps<'button'>)
         }
       })
+      .filter(Boolean)
+      .slice(0, 2)
 
     return (
-      <section ref={ref} className={clsx(styles.ButtonGroup, className)} {...props}>
-        <Stack direction={'horizontal'}>{buttonsToRender as React.ReactElement[]}</Stack>
-        {/* {buttonsToRender} */}
+      <section
+        ref={ref}
+        className={clsx(styles.ButtonGroup, direction === 'horizontal' && styles['ButtonGroup--horizontal'], className)}
+        {...props}
+      >
+        {buttonsToRender as React.ReactElement[]}
       </section>
     )
   }
