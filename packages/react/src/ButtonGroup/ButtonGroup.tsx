2569d4f4a94000c164740c6a5efc42268a1f08f4
Josh Bowden <31220082+JoshBowdenConcepts@users.noreply.github.com>
2022-11-03 08:27:21 -0400

    Button group component (#110)
    * First Pass at ButtonGroup
    
    * updated component
    - simplified button reduction
    - removed Stack in favor of custom CSS
    
    * creating types for button group
    
    * updated type to match generic
    
    * add vrt snapshots for button group
    
    * Improvements to Button Documentation and Typing
    - Updated type to only accept button or anchor
    - Updated stories to include more examples
    
    * Update packages/react/src/ButtonGroup/ButtonGroup.module.css
    
    Co-authored-by: Rez <rezrah@github.com>
    
    * Removal of duplicate lines
    
    * Created a Responsive Button Layout
    
    * Fixed final comments
    
    * updated vrt snapshots
    
    * Create rude-parents-sing.md
    
    * Update rude-parents-sing.md
    
    * Removed direction property and updated docs
    
    * update visual snapshots
    
    * Removed unused line
    
    * Removed unused import
    
    Co-authored-by: Joshua Bowden <jbowden@Joshuas-MBP.localdomain>
    Co-authored-by: Reza Rahman <rezrah@github.com>

---
+++ packages/react/src/ButtonGroup/ButtonGroup.tsx
@@ -0,0 +1,36 @@
+import React, {forwardRef, type Ref} from 'react'
+import type {BaseProps} from '../component-helpers'
+import {Button, ButtonProps} from '../Button'
+import styles from './ButtonGroup.module.css'
+
+type PrimerBrandButtonType = React.ReactElement<ButtonProps<React.ElementType<'button' | 'a'>>>
+
+export type ButtonGroupProps = BaseProps<HTMLDivElement> & {
+  children: PrimerBrandButtonType[] | PrimerBrandButtonType
+  buttonSize?: ButtonProps<'button' | 'a'>['size']
+  buttonsAs?: React.ElementType
+}
+
+export const ButtonGroup = forwardRef(
+  ({buttonSize = 'medium', buttonsAs, className, children, ...props}: ButtonGroupProps, ref: Ref<HTMLDivElement>) => {
+    const buttonsToRender = React.Children.toArray(children)
+      .map((child, index) => {
+        if (React.isValidElement(child) && typeof child.type !== 'string' && child.type === Button) {
+          return React.cloneElement(child, {
+            size: buttonSize,
+            as: buttonsAs,
+            variant: index === 0 ? 'primary' : 'secondary',
+            ...child.props
+          } as ButtonProps<'button'>)
+        }
+      })
+      .filter(Boolean)
+      .slice(0, 2)
+
+    return (
+      <section ref={ref} className={styles.ButtonGroup} {...props}>
+        {buttonsToRender as React.ReactElement[]}
+      </section>
+    )
+  }
+)
