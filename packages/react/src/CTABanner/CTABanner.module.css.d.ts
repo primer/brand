02c784093b13d365a59951bc942fbe7831c78ee8
Joshua Bowden <jbowden@Joshuas-MBP.localdomain>
2022-11-07 08:38:40 -0500

    Starting work on CTABanner

--- packages/react/src/CTABanner/CTABanner.module.css.d.ts
+++ packages/react/src/CTABanner/CTABanner.module.css.d.ts
@@ -1,13 +1,12 @@
 declare const styles: {
-  readonly "CTABanner": string;
-  readonly "CTABanner--Shadow": string;
-  readonly "CTABanner--RightShadow": string;
-  readonly "CTABanner--LeftShadow": string;
-  readonly "CTABanner--content": string;
-  readonly "CTABanner--content--noshadow": string;
-  readonly "CTABanner--border": string;
-  readonly "CTABanner--center": string;
-  readonly "CTABannerDescription": string;
-};
-export = styles;
-
+  readonly CTABanner: string
+  readonly 'CTABanner--Shadow': string
+  readonly 'CTABanner--RightShadow': string
+  readonly 'CTABanner--LeftShadow': string
+  readonly 'CTABanner--content': string
+  readonly 'CTABanner--content--noshadow': string
+  readonly 'CTABanner--border': string
+  readonly 'CTABanner--center': string
+  readonly CTABannerDescription: string
+}
+export = styles
