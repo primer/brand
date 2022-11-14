c16551f65bd1f2ceb62f9979c0ae909e58aed348
Rez <rezrah@github.com>
2022-11-14 14:56:34 +0000

    Improvements to Button component appearance and documentation (#127)

--- packages/design-tokens/src/tokens/functional/components/button/colors.js
+++ packages/design-tokens/src/tokens/functional/components/button/colors.js
@@ -9,6 +9,24 @@ module.exports = {
         overlay: {
           value: `linear-gradient(180deg, hsla(0, 0%, 100%, 0.15) 0, hsla(0, 0%, 100%, 0) 100%), var(--base-color-scale-black-0)`,
           dark: `linear-gradient(180deg, hsla(0,0%,100%,0.15) 0,hsla(0,0%,100%,0) 100%),var(--base-color-scale-white-0)`
+        },
+        disabled: {
+          value: `var(--base-color-scale-gray-4)`,
+          dark: `var(--base-color-scale-gray-4)`
+        }
+      },
+      fg: {
+        primary: {
+          disabled: {
+            value: `var(--base-color-scale-gray-2)`,
+            dark: `var(--base-color-scale-gray-2)`
+          }
+        },
+        secondary: {
+          disabled: {
+            value: `var(--base-color-scale-gray-3)`,
+            dark: `var(--base-color-scale-gray-4)`
+          }
         }
       },
       shadow: {
@@ -22,8 +40,8 @@ module.exports = {
           dark: '0 4px 7px rgba(0, 0, 0, 0.15), 0 100px 80px rgba(255, 255, 255, 0.02), 0 42px 33px rgba(255, 255, 255, 0.024), 0 22px 18px rgba(255, 255, 255, 0.028), 0 12px 10px rgba(255, 255, 255, 0.034), 0 7px 5px rgba(255, 255, 255, 0.04), 0 3px 2px rgba(255, 255, 255, 0.07)'
         },
         focus: {
-          value: 'hsl(var(--base-color-scale-black-0-hsl) / 15%) 0 0 0 4px;',
-          dark: 'hsl(var(--base-color-scale-white-0-hsl) / 25%) 0 0 0 4px;'
+          value: 'var(--brand-color-focus)',
+          dark: 'var(--brand-color-focus)'
         }
       }
     }
