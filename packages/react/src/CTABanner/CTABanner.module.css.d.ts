da57d28517b5bfb60070c55bbdfb95b5f45f85b4
JoshBowdenConcepts <JoshBowdenConcepts@users.noreply.github.com>
2022-11-29 17:03:56 +0000

    github-actions[bot] Regenerated snapshots

--- packages/react/src/CTABanner/CTABanner.module.css.d.ts
+++ packages/react/src/CTABanner/CTABanner.module.css.d.ts
@@ -1,9 +1,10 @@
 declare const styles: {
   readonly "CTABanner": string;
   readonly "CTABanner--shadow": string;
+  readonly "CTABanner-container": string;
   readonly "CTABanner-content": string;
-  readonly "CTABanner-content--background": string;
-  readonly "CTABanner-content--border": string;
+  readonly "CTABanner-container--background": string;
+  readonly "CTABanner-container--border": string;
   readonly "CTABanner-content--center": string;
   readonly "CTABanner-description": string;
 };
