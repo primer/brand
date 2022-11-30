b4c7292e0407a4418a3c29fa5073edd94ff60f03
Reza Rahman <rezrah@github.com>
2022-11-30 20:07:41 +0000

    refactored DevGrid component - added responsive and removed imperitive code

--- packages/react/src/DevGrid/DevGrid.tsx
+++ packages/react/src/DevGrid/DevGrid.tsx
@@ -1,10 +1,19 @@
-import React, {useEffect, useState, type ReactElement} from 'react'
+/**
+ * TODO
+ * - support all breakpoints
+ * - make columns declarative
+ * - use design tokens for breakpoints
+ */
+
+import React, {useCallback, useEffect, useState, type ReactElement} from 'react'
 import clsx from 'clsx'
 import {useDevGridKeyCombo} from '../hooks/useDevGridKeyCombo'
+import {useWindowSize} from '../hooks/useWindowSize'
 
 /**
  * Design tokens
  */
+import breakpoints from '@primer/brand-primitives/lib/design-tokens/ts/tokens/functional/size/breakpoints'
 import '@primer/brand-primitives/lib/design-tokens/css/tokens/functional/size/size.css'
 
 /**
@@ -12,10 +21,19 @@ import '@primer/brand-primitives/lib/design-tokens/css/tokens/functional/size/si
  */
 import styles from './DevGrid.module.css'
 
+/*
+ * Generate from design tokens or FIgma
+ */
+enum Breakpoints {
+  NARROW = 4,
+  MEDIUM = 8,
+  WIDE = 12
+}
+
 /**
  * Defaults
  */
-export const defaultGridSize = 12
+export const defaultGridSize = Breakpoints.NARROW
 
 /**
  * A Toggle-able grid to assist developers in front-end development.
@@ -26,21 +44,49 @@ export type DevGridProps = {
 
 export const DevGrid = ({size = defaultGridSize}: DevGridProps) => {
   const [gridIsOpen, setIsGridOpen] = useState(false)
+  const [numCols, setNumCols] = useState(size)
   const handleToggle = () => setIsGridOpen(!gridIsOpen)
 
   useDevGridKeyCombo(handleToggle)
+  const {width} = useWindowSize()
+
+  const convertPxToRem = useCallback((rem: string): number => {
+    return parseFloat(rem) * parseFloat(getComputedStyle(document.documentElement).fontSize)
+  }, [])
+
+  const mediumBreakpointToPx = convertPxToRem(breakpoints.brand.breakpoint.medium)
+  const largeBreakpointToPx = convertPxToRem(breakpoints.brand.breakpoint.large)
+
+  useEffect(() => {
+    if (width) {
+      if (width < mediumBreakpointToPx) {
+        setNumCols(Breakpoints.NARROW)
+      }
+      if (width >= mediumBreakpointToPx && width < largeBreakpointToPx) {
+        setNumCols(Breakpoints.MEDIUM)
+      }
+      if (width >= largeBreakpointToPx) {
+        setNumCols(Breakpoints.WIDE)
+      }
+    }
+  }, [numCols, setNumCols, width, mediumBreakpointToPx, largeBreakpointToPx])
+
   useEffect(() => {
     const root = document.documentElement
-    root.style.setProperty('--devGrid-grid-size', `${size}`)
-  }, [size])
+    root.style.setProperty('--devGrid-grid-size', `${numCols}`)
+  }, [numCols])
 
-  let col = size
-  const columns: ReactElement[] = []
-
-  while (col > 0) {
-    col--
-    columns.push(<div className={styles.DevGrid__column} key={`DevGrid__Column-${col}`} />)
-  }
-
-  return <div className={clsx(styles.DevGrid, gridIsOpen && styles['DevGrid--show'])}>{columns}</div>
+  return (
+    <div className={clsx(styles.DevGrid, gridIsOpen && styles['DevGrid--show'])}>
+      {numCols > 0 &&
+        Array.from(Array(numCols).keys()).map(value => {
+          return (
+            <div
+              className={clsx(styles.DevGrid__column, styles[`DevGrid__Column-${value + 1}`])}
+              key={`DevGrid__Column-${value}`}
+            />
+          )
+        })}
+    </div>
+  )
 }
