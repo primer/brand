8796f1025c7a142f3c379827d72916a931f9e6f4
Reza Rahman <rezrah@github.com>
2022-12-08 20:57:38 +0000

    fixing anchornav link label underline

--- packages/e2e/scripts/playwright/playwright.generate-tests.ts
+++ packages/e2e/scripts/playwright/playwright.generate-tests.ts
@@ -48,6 +48,7 @@
     'components-anchornav--playground': 500, // for the animation
     'components-anchornav-features--fewer-anchor-links': 1000, // for the animation
     'components-anchornav-features--custom-background': 1000, // for the animation
+    'components-anchornav-features--shorter-labels': 1000, // for the animation
     'components-anchornav-features--narrow-view': 1000, // for the interaction test
     'components-anchornav-features--narrow-view-menu-open': 1000, // for the interaction test
     'components-anchornav-features--regular-view': 1000, // for the interaction test
--- packages/react/src/AnchorNav/AnchorNav.visual.spec.ts
+++ packages/react/src/AnchorNav/AnchorNav.visual.spec.ts
@@ -16,6 +16,15 @@ test.describe('Visual Comparison: AnchorNav', () => {
     expect(await page.screenshot()).toMatchSnapshot()
   })
 
+  test('AnchorNav / Shorter labels', async ({page}) => {
+    await page.goto(
+      'http://localhost:6006/iframe.html?args=&id=components-anchornav-features--shorter-labels&viewMode=story'
+    )
+
+    await page.waitForTimeout(1000)
+    expect(await page.screenshot()).toMatchSnapshot()
+  })
+
   test('AnchorNav / Custom Background', async ({page}) => {
     await page.goto(
       'http://localhost:6006/iframe.html?args=&id=components-anchornav-features--custom-background&viewMode=story'
