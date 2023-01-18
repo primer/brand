289d28fc497b39ec096c519fc41bd0fc8f4f6964
danielguillan <danielguillan@users.noreply.github.com>
2023-01-18 12:54:56 +0000

    github-actions[bot] Regenerated snapshots

--- packages/react/src/SectionIntro/SectionIntro.module.css.d.ts
+++ packages/react/src/SectionIntro/SectionIntro.module.css.d.ts
@@ -1,5 +1,6 @@
 declare const styles: {
   readonly "SectionIntro": string;
+  readonly "SectionIntro--align-start": string;
   readonly "SectionIntro--align-center": string;
   readonly "SectionIntro-heading": string;
   readonly "SectionIntro-description": string;
