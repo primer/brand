869463e0961b94e3cb251f4985816de03723a636
Joshua Bowden <jbowden@Joshuas-MBP.localdomain>
2023-01-19 11:33:15 -0500

    Updated component and created Story file

--- packages/react/src/CTAForm/CTAForm.stories.tsx
+++ packages/react/src/CTAForm/CTAForm.stories.tsx
@@ -5,9 +5,42 @@ import {CTAForm} from './CTAForm'
 
 export default {
   title: 'Components/CTAForm',
-  component: CTAForm
+  component: CTAForm,
+  args: {
+    inputLabel: 'Your work email address',
+    placeholder: 'me@github.com',
+    buttonLabel: 'Subscribe',
+    checkboxLabel: (
+      <>
+        I agree to the <a href="www.github.com">terms and conditions</a> and <a href="www.github.com">privacy policy</a>
+      </>
+    )
+  },
+  argTypes: {
+    inputLabel: {
+      control: {
+        type: 'text'
+      }
+    },
+    inputType: {
+      control: {
+        type: 'select',
+        options: ['email', 'text']
+      }
+    },
+    placeholder: {
+      control: {
+        type: 'text'
+      }
+    },
+    buttonLabel: {
+      control: {
+        type: 'text'
+      }
+    }
+  }
 } as ComponentMeta<typeof CTAForm>
 
-export const Playground: ComponentStory<typeof CTAForm> = _args => <CTAForm />
+export const Playground: ComponentStory<typeof CTAForm> = args => <CTAForm {...args} />
 
 export const Default = Playground.bind({})
--- packages/react/src/CTAForm/CTAForm.tsx
+++ packages/react/src/CTAForm/CTAForm.tsx
@@ -1,24 +1,37 @@
 import React from 'react'
+import clsx from 'clsx'
 import {FormControl, Button, TextInput, Checkbox} from '../'
 
 import styles from './CTAForm.module.css'
 
-export const CTAForm = () => {
+type CTAFormProps = {
+  inputLabel: string
+  inputType?: 'email' | 'text'
+  placeholder?: string
+  buttonLabel: string
+  checkboxLabel: React.ReactNode[] | React.ReactNode | string
+} & React.HTMLAttributes<HTMLFormElement>
+
+export const CTAForm = ({
+  inputLabel,
+  inputType = 'email',
+  placeholder,
+  buttonLabel,
+  checkboxLabel,
+  ...args
+}: CTAFormProps) => {
   return (
-    <form className={styles.CTAForm}>
+    <form {...args} className={clsx(styles.CTAForm, args.className)}>
       <FormControl required size="large" className={styles['CTAForm-input']}>
-        <FormControl.Label>Your work email address</FormControl.Label>
-        <TextInput />
+        <FormControl.Label>{inputLabel}</FormControl.Label>
+        <TextInput type={inputType} placeholder={placeholder} />
       </FormControl>
       <FormControl required fullWidth size="large" className={styles['CTAForm-legal']}>
-        <FormControl.Label>
-          I agree to the <a href="www.github.com">terms and conditions</a> and{' '}
-          <a href="www.github.com">privacy policy</a>
-        </FormControl.Label>
+        <FormControl.Label>{checkboxLabel}</FormControl.Label>
         <Checkbox />
       </FormControl>
       <Button type="submit" variant="primary" className={styles['CTAForm-button']}>
-        Subscribe
+        {buttonLabel}
       </Button>
     </form>
   )
