3eeda651c9fcae44cc2025cf9271b47482789d61
Rez <rezrah@github.com>
2023-01-31 10:52:00 +0000

    Add MinimalFooter component (#165)

---
+++ packages/react/src/MinimalFooter/MinimalFooter.stories.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+import {MinimalFooter} from '.'
+import {InlineLink, Text} from '../'
+
+export default {
+  title: 'Components/MinimalFooter',
+  component: MinimalFooter
+} as ComponentMeta<typeof MinimalFooter>
+
+export const Playground: ComponentStory<typeof MinimalFooter> = args => <MinimalFooter {...args} />
+
+export const Default = () => (
+  <MinimalFooter>
+    <MinimalFooter.Footnotes>
+      <Text>
+        <sup>1</sup>By signing up, GitHub may use your information to personalize and measure the effectiveness of
+        enterprise business ads, including those you see off of GitHub, promotional communications or marketing you
+        receive related to the Enterprise Marketing Pages. We will send you relevant emails and promotional information
+        based on your GitHub profile and any additional information provided in the sign-up form. If you change your
+        mind, you can unsubscribe at any time (an unsubscribe link is provided in every email). For more information on
+        how GitHub uses your personal information, please see the{' '}
+        <InlineLink
+          href="https://docs.github.com/en/site-policy/privacy-policies/github-privacy-statement"
+          target="_blank"
+        >
+          GitHub Privacy Statement
+        </InlineLink>
+        .
+      </Text>
+    </MinimalFooter.Footnotes>
+    <MinimalFooter.Link href="https://github.com/organizations/enterprise_plan">Try GitHub for free</MinimalFooter.Link>
+    <MinimalFooter.Link href="https://github.com/enterprise">Enterprise</MinimalFooter.Link>
+    <MinimalFooter.Link href="https://github.com/enterprise/contact">Email us</MinimalFooter.Link>
+  </MinimalFooter>
+)
