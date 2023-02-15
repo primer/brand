9458481db622ee38878cc68171af7502700baa44
Joshua Bowden <jbowden@Joshuas-MBP.localdomain>
2023-02-15 10:35:01 -0500

    Added Muted Variant to Documentation Text

--- packages/react/src/CTAForm/CTAForm.stories.tsx
+++ packages/react/src/CTAForm/CTAForm.stories.tsx
@@ -20,7 +20,7 @@ export const Playground: ComponentStory<typeof CTAForm> = _args => (
     <CTAForm.Confirm>
       <FormControl required>
         <FormControl.Label>
-          <Text size="300">
+          <Text size="300" variant="muted">
             I agree to the <InlineLink href="www.github.com">Privacy Policy</InlineLink> and{' '}
             <InlineLink href="www.github.com">Terms of Service</InlineLink>
           </Text>
@@ -43,7 +43,7 @@ export const Default: ComponentStory<typeof CTAForm> = _args => (
     <CTAForm.Confirm>
       <FormControl required>
         <FormControl.Label>
-          <Text size="300">
+          <Text size="300" variant="muted">
             I agree to the <InlineLink href="www.github.com">Privacy Policy</InlineLink> and{' '}
             <InlineLink href="www.github.com">Terms of Service</InlineLink>
           </Text>
