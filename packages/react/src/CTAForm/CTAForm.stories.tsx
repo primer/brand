fe0e1ee1021fa38955115d3c40aba4f1f536eef3
Josh Bowden <31220082+JoshBowdenConcepts@users.noreply.github.com>
2023-02-16 08:20:45 -0500

    CTAForm Component (#163)
    * Created Ideal Hardcoded CTAForm
    
    * Updated component and created Story file
    
    * Updated CSS Var to Base Var
    
    * Updated types to pull from their base components
    
    * Added Changeset
    
    * Added MDX Docs and Updated Properties
    
    * Removed unesesary property
    
    * Component Updates to Wrapper Architecture
    - Based on feedback in API exploration I updated the components to be wrappers
    - I updated the CSS to reflect the design and accessibility review
    - I updated the structure to more of a composite approach
    
    * Created Attribute Testing for CTAForm
    
    * Updated Styles and Docs
    - Styles change button to be auto sized
    - Docs include the new structure for the component
    
    * TODO Work
    
    * Added Tests and Updated Stories
    
    * Update poor-boats-walk.md
    
    * github-actions[bot] Regenerated snapshots
    
    * Markdown Docs Changes
    - Changed TODO to a HTML comment
    - Removed extra examples
    
    * Minor Updates
    - Fixed text capitalization in changeset
    - Updated CTAForm docs to include info about form props
    
    * Changed order of rest props and destructuring
    
    * Fixed Partial Review Parts
    - Updated media query to use rem units
    - Added controlled example to the form
    - Consolidate tests into one describe block
    - Removed TODO note that was unused.
    
    * Updated component to find FormControl and add required props
    
    * More Incremental Updates
    - Updated style to reflect expectation on desktop
    - Updated component to include required prop on the expected first input
    
    * Quick Fix for Pixel Values in SubdomainNavBar
    
    * Updated Checkbox Label To Use Text Component
    
    * Added size to the text component
    
    * Added Tests Around FormControl Properties
    
    * Updated Docs
    
    * Added Muted Variant to Documentation Text
    
    * updated changelog example
    
    * github-actions[bot] Regenerated snapshots
    
    * Updated Story and  Tests
    
    ---------
    
    Co-authored-by: Joshua Bowden <jbowden@Joshuas-MBP.localdomain>
    Co-authored-by: JoshBowdenConcepts <JoshBowdenConcepts@users.noreply.github.com>
    Co-authored-by: rezrah <rezrah@users.noreply.github.com>

---
+++ packages/react/src/CTAForm/CTAForm.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import {ComponentMeta, ComponentStory} from '@storybook/react'
+
+import {CTAForm} from './CTAForm'
+import {FormControl, TextInput, Checkbox, Text, InlineLink} from '../index'
+
+export default {
+  title: 'Components/CTAForm',
+  component: CTAForm
+} as ComponentMeta<typeof CTAForm>
+
+export const Playground: ComponentStory<typeof CTAForm> = _args => (
+  <CTAForm>
+    <CTAForm.Input>
+      <FormControl required>
+        <FormControl.Label>Your Work Email Address</FormControl.Label>
+        <TextInput placeholder="name" />
+      </FormControl>
+    </CTAForm.Input>
+    <CTAForm.Confirm>
+      <FormControl required>
+        <FormControl.Label>
+          <Text size="300" variant="muted">
+            I agree to the <InlineLink href="https://github.com">Privacy Policy</InlineLink> and{' '}
+            <InlineLink href="https://github.com">Terms of Service</InlineLink>
+          </Text>
+        </FormControl.Label>
+        <Checkbox name="confirm" />
+      </FormControl>
+    </CTAForm.Confirm>
+    <CTAForm.Action>Subscribe</CTAForm.Action>
+  </CTAForm>
+)
+
+export const Default: ComponentStory<typeof CTAForm> = _args => (
+  <CTAForm>
+    <CTAForm.Input>
+      <FormControl required>
+        <FormControl.Label>Your Work Email Address</FormControl.Label>
+        <TextInput placeholder="name" />
+      </FormControl>
+    </CTAForm.Input>
+    <CTAForm.Confirm>
+      <FormControl required>
+        <FormControl.Label>
+          <Text size="300" variant="muted">
+            I agree to the <InlineLink href="https://github.com">Privacy Policy</InlineLink> and{' '}
+            <InlineLink href="https://github.com">Terms of Service</InlineLink>
+          </Text>
+        </FormControl.Label>
+        <Checkbox name="confirm" />
+      </FormControl>
+    </CTAForm.Confirm>
+    <CTAForm.Action>Subscribe</CTAForm.Action>
+  </CTAForm>
+)
---
+++ packages/react/src/CTAForm/CTAForm.tsx
@@ -0,0 +1,78 @@
+import React, {forwardRef, type Ref} from 'react'
+import clsx from 'clsx'
+import {Button, FormControl} from '../'
+
+import styles from './CTAForm.module.css'
+
+const Root = forwardRef(
+  ({className, ...rest}: React.FormHTMLAttributes<HTMLFormElement>, ref: Ref<HTMLFormElement>) => {
+    return <form ref={ref} className={clsx(styles.CTAForm, className)} {...rest} />
+  }
+)
+
+const _Input = forwardRef(
+  ({className, children, ...rest}: React.HTMLAttributes<HTMLDivElement>, ref: Ref<HTMLDivElement>) => {
+    const transformedChildren = React.Children.toArray(children).map(child => {
+      if (React.isValidElement(child) && typeof child.type !== 'string') {
+        if (child.type === FormControl) {
+          return React.cloneElement(child, {
+            fullWidth: true,
+            size: 'large',
+            required: true,
+            ...child.props
+          })
+        }
+      }
+      return child
+    })
+
+    return (
+      <div ref={ref} className={clsx(styles['CTAForm-input'], className)} {...rest}>
+        {transformedChildren}
+      </div>
+    )
+  }
+)
+
+const _Confirm = forwardRef(
+  ({className, children, ...rest}: React.HTMLAttributes<HTMLDivElement>, ref: Ref<HTMLDivElement>) => {
+    const transformedChildren = React.Children.toArray(children).map(child => {
+      if (React.isValidElement(child) && typeof child.type !== 'string') {
+        if (child.type === FormControl) {
+          return React.cloneElement(child, {
+            fullWidth: true,
+            size: 'large',
+            ...child.props
+          })
+        }
+      }
+      return child
+    })
+    return (
+      <div ref={ref} className={clsx(styles['CTAForm-confirm'], className)} {...rest}>
+        {transformedChildren}
+      </div>
+    )
+  }
+)
+
+const _Action = forwardRef(
+  (
+    {className, children, ...rest}: Omit<React.ComponentProps<typeof Button>, 'variant' | 'type'>,
+    ref: Ref<HTMLButtonElement>
+  ) => {
+    return (
+      <Button
+        type="submit"
+        variant="secondary"
+        className={clsx(styles['CTAForm-button'], className)}
+        ref={ref}
+        {...rest}
+      >
+        {children}
+      </Button>
+    )
+  }
+)
+
+export const CTAForm = Object.assign(Root, {Input: _Input, Confirm: _Confirm, Action: _Action})
