1c7585e7ab05b7d3e650ab0bea0b2ef5123b06e0
Daniel Guillan <danielguillan@github.com>
2023-02-21 15:08:44 -0300

    Add square-shaped size examples in Storybook

--- packages/react/src/Avatar/Avatar.features.stories.tsx
+++ packages/react/src/Avatar/Avatar.features.stories.tsx
@@ -9,12 +9,22 @@ export default {
 } as ComponentMeta<typeof Avatar>
 
 export const Sizes = () => (
-  <Stack direction="horizontal" alignItems="center" justifyContent="center" gap="spacious" style={{height: '100vh'}}>
-    <Avatar size={32} src="https://i.pravatar.cc/64?u=80" alt="A random avatar picture" />
-    <Avatar size={40} src="https://i.pravatar.cc/80?u=34" alt="A random avatar picture" />
-    <Avatar size={48} src="https://i.pravatar.cc/96?u=167" alt="A random avatar picture" />
-    <Avatar size={64} src="https://i.pravatar.cc/128?u=51" alt="A random avatar picture" />
-    <Avatar size={80} src="https://i.pravatar.cc/160?u=109" alt="A random avatar picture" />
+  <Stack style={{height: '100vh'}} justifyContent="center">
+    <Stack direction="horizontal" alignItems="center" justifyContent="center" gap="spacious">
+      <Avatar size={32} src="https://i.pravatar.cc/64?u=80" alt="A random avatar picture" />
+      <Avatar size={40} src="https://i.pravatar.cc/80?u=34" alt="A random avatar picture" />
+      <Avatar size={48} src="https://i.pravatar.cc/96?u=167" alt="A random avatar picture" />
+      <Avatar size={64} src="https://i.pravatar.cc/128?u=51" alt="A random avatar picture" />
+      <Avatar size={80} src="https://i.pravatar.cc/160?u=109" alt="A random avatar picture" />
+    </Stack>
+
+    <Stack direction="horizontal" alignItems="center" justifyContent="center" gap="spacious">
+      <Avatar shape="square" size={32} src="https://i.pravatar.cc/64?u=80" alt="A random avatar picture" />
+      <Avatar shape="square" size={40} src="https://i.pravatar.cc/80?u=34" alt="A random avatar picture" />
+      <Avatar shape="square" size={48} src="https://i.pravatar.cc/96?u=167" alt="A random avatar picture" />
+      <Avatar shape="square" size={64} src="https://i.pravatar.cc/128?u=51" alt="A random avatar picture" />
+      <Avatar shape="square" size={80} src="https://i.pravatar.cc/160?u=109" alt="A random avatar picture" />
+    </Stack>
   </Stack>
 )
 
