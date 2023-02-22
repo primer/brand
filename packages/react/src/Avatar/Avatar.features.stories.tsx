3772db99a8898b8d8a49c220aaa07560dda3118a
Daniel Guillan <danielguillan@github.com>
2023-02-22 09:30:54 -0300

    Update avatar urls
    Co-authored-by: Rez <rezrah@github.com>

--- packages/react/src/Avatar/Avatar.features.stories.tsx
+++ packages/react/src/Avatar/Avatar.features.stories.tsx
@@ -11,11 +11,11 @@ export default {
 export const Sizes = () => (
   <Stack style={{height: '100vh'}} justifyContent="center">
     <Stack direction="horizontal" alignItems="center" justifyContent="center" gap="spacious">
-      <Avatar size={32} src="https://i.pravatar.cc/64?u=80" alt="A random avatar picture" />
-      <Avatar size={40} src="https://i.pravatar.cc/80?u=34" alt="A random avatar picture" />
-      <Avatar size={48} src="https://i.pravatar.cc/96?u=167" alt="A random avatar picture" />
-      <Avatar size={64} src="https://i.pravatar.cc/128?u=51" alt="A random avatar picture" />
-      <Avatar size={80} src="https://i.pravatar.cc/160?u=109" alt="A random avatar picture" />
+      <Avatar size={32} src="https://avatars.githubusercontent.com/u/92997159?v=4" alt="A random avatar picture" />
+      <Avatar size={40} src="https://avatars.githubusercontent.com/u/92997159?v=4" alt="A random avatar picture" />
+      <Avatar size={48} src="https://avatars.githubusercontent.com/u/92997159?v=4" alt="A random avatar picture" />
+      <Avatar size={64} src="https://avatars.githubusercontent.com/u/92997159?v=4" alt="A random avatar picture" />
+      <Avatar size={80} src="https://avatars.githubusercontent.com/u/92997159?v=4" alt="A random avatar picture" />
     </Stack>
 
     <Stack direction="horizontal" alignItems="center" justifyContent="center" gap="spacious">
