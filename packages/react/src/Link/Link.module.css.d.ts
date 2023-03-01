757d98e1f18d2c59e2768da37415ee8541141920
Reza Rahman <rezrah@github.com>
2023-03-01 17:21:52 +0000

    fix underline

--- packages/react/src/Link/Link.module.css.d.ts
+++ packages/react/src/Link/Link.module.css.d.ts
@@ -1,8 +1,9 @@
 declare const styles: {
-  readonly Link: string
-  readonly 'Link--label': string
-  readonly 'Link-arrow': string
-  readonly 'Link--large': string
-  readonly 'Link--reverse': string
-}
-export = styles
+  readonly "Link": string;
+  readonly "Link--label": string;
+  readonly "Link-arrow": string;
+  readonly "Link--large": string;
+  readonly "Link--reverse": string;
+};
+export = styles;
+
