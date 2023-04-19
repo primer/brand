67dd404bd8a209c81a29374ca551bae2ae5346dd
Tyler Jones <tylerjdev@github.com>
2023-04-19 17:04:47 -0400

    Fix up stories, children

--- packages/react/src/Hero/Hero.stories.tsx
+++ packages/react/src/Hero/Hero.stories.tsx
@@ -15,10 +15,6 @@ export default {
   }
 } as ComponentMeta<typeof Hero>
 
-const Template: ComponentStory<typeof Hero> = args => <Hero {...args} />
-
-// export const Default = Template.bind({})
-
 export const Default: ComponentStory<typeof Hero> = _args => (
   <Hero>
     <Hero.Heading>This is my super sweet hero heading</Hero.Heading>
@@ -26,126 +22,98 @@ export const Default: ComponentStory<typeof Hero> = _args => (
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed turpis
       felis nam pulvinar risus elementum.
     </Hero.Description>
-    <Hero.PrimaryAction href="#">Primary Action</Hero.PrimaryAction>
+    <Hero.PrimaryAction href="#">Primary action</Hero.PrimaryAction>
   </Hero>
 )
 
-// Default.args = {
-//   heading: 'This is my super sweet hero heading',
-//   description:
-//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed turpis felis nam pulvinar risus elementum.',
-//   primaryAction: {
-//     text: 'Primary action',
-//     href: '#'
-//   }
-// }
+export const Centered: ComponentStory<typeof Hero> = _args => (
+  <Hero align="center">
+    <Hero.Heading>This is my super sweet hero heading</Hero.Heading>
+    <Hero.Description>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed turpis
+      felis nam pulvinar risus elementum.
+    </Hero.Description>
+    <Hero.PrimaryAction href="#">Primary action</Hero.PrimaryAction>
+  </Hero>
+)
 
-// export const Centered = Template.bind({})
-// Centered.args = {
-//   heading: 'This is my super sweet hero heading',
-//   description:
-//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed turpis felis nam pulvinar risus elementum.',
-//   align: 'center',
-//   primaryAction: {
-//     text: 'Primary action',
-//     href: '#'
-//   }
-// }
+export const WithoutDescription: ComponentStory<typeof Hero> = _args => (
+  <Hero>
+    <Hero.Heading>This is my super sweet hero heading</Hero.Heading>
+    <Hero.PrimaryAction href="#">Primary action</Hero.PrimaryAction>
+  </Hero>
+)
 
-// export const WithoutDescription = Template.bind({})
-// WithoutDescription.args = {
-//   heading: 'This is my super sweet hero heading',
-//   primaryAction: {
-//     text: 'Primary action',
-//     href: '#'
-//   }
-// }
+export const WithSecondaryAction: ComponentStory<typeof Hero> = _args => (
+  <Hero>
+    <Hero.Heading>This is my super sweet hero heading</Hero.Heading>
+    <Hero.Description>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed turpis
+      felis nam pulvinar risus elementum.
+    </Hero.Description>
+    <Hero.PrimaryAction href="#">Primary action</Hero.PrimaryAction>
+    <Hero.SecondaryAction href="#">Secondary action</Hero.SecondaryAction>
+  </Hero>
+)
 
-// export const WithSecondaryAction = Template.bind({})
-// WithSecondaryAction.args = {
-//   heading: 'This is my super sweet hero heading',
-//   description:
-//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed turpis felis nam pulvinar risus elementum.',
-//   primaryAction: {
-//     text: 'Primary action',
-//     href: '#'
-//   },
-//   secondaryAction: {
-//     text: 'Secondary action',
-//     href: '#'
-//   }
-// }
+export const Codespaces: ComponentStory<typeof Hero> = _args => (
+  <Hero align="center">
+    <Hero.Heading>
+      <>
+        Blazing fast cloud
+        <br />
+        developer environments
+      </>
+    </Hero.Heading>
+    <Hero.Description>Visual Studio Code backed by high performance VMs that start in seconds.</Hero.Description>
+    <Hero.PrimaryAction href="#">Get started</Hero.PrimaryAction>
+  </Hero>
+)
 
-// export const Codespaces = Template.bind({})
-// Codespaces.args = {
-//   heading: (
-//     <>
-//       Blazing fast cloud
-//       <br />
-//       developer environments
-//     </>
-//   ),
-//   description: 'Visual Studio Code backed by high performance VMs that start in seconds.',
-//   align: 'center',
-//   primaryAction: {
-//     text: 'Get started',
-//     href: '#'
-//   }
-// }
+export const Issues: ComponentStory<typeof Hero> = _args => (
+  <Hero align="center">
+    <Hero.Heading>Project planning for developers</Hero.Heading>
+    <Hero.Description>
+      Create issues, break them into tasks, track relationships, add custom fields, and have conversations. Visualize
+      large projects as spreadsheets or boards, and automate everything with code.
+    </Hero.Description>
+    <Hero.PrimaryAction href="#">Watch video</Hero.PrimaryAction>
+    <Hero.SecondaryAction href="#">Start using project tables</Hero.SecondaryAction>
+  </Hero>
+)
 
-// export const Issues = Template.bind({})
-// Issues.args = {
-//   heading: 'Project planning for developers',
-//   description:
-//     'Create issues, break them into tasks, track relationships, add custom fields, and have conversations. Visualize large projects as spreadsheets or boards, and automate everything with code.',
-//   align: 'center',
-//   primaryAction: {
-//     text: 'Watch video',
-//     href: '#'
-//   },
-//   secondaryAction: {
-//     text: 'Start using project tables',
-//     href: '#'
-//   }
-// }
+export const NarrowView: ComponentStory<typeof Hero> = _args => (
+  <Hero>
+    <Hero.Heading>Project planning for developers</Hero.Heading>
+    <Hero.Description>
+      Create issues, break them into tasks, track relationships, add custom fields, and have conversations. Visualize
+      large projects as spreadsheets or boards, and automate everything with code.
+    </Hero.Description>
+    <Hero.PrimaryAction href="#">Watch video</Hero.PrimaryAction>
+    <Hero.SecondaryAction href="#">Start using project tables</Hero.SecondaryAction>
+  </Hero>
+)
 
-// export const NarrowView = Template.bind({})
-// NarrowView.parameters = {
-//   viewport: {
-//     defaultViewport: 'iphonexr'
-//   }
-// }
-// NarrowView.args = {
-//   heading: 'Project planning for developers',
-//   description:
-//     'Create issues, break them into tasks, track relationships, add custom fields, and have conversations. Visualize large projects as spreadsheets or boards, and automate everything with code.',
-//   primaryAction: {
-//     text: 'Watch video',
-//     href: '#'
-//   },
-//   secondaryAction: {
-//     text: 'Start using project tables',
-//     href: '#'
-//   }
-// }
+NarrowView.parameters = {
+  viewport: {
+    defaultViewport: 'iphonexr'
+  }
+}
 
-// export const NarrowViewCentered = Template.bind({})
-// NarrowViewCentered.parameters = {
-//   viewport: {
-//     defaultViewport: 'iphonexr'
-//   }
-// }
-// NarrowViewCentered.args = {
-//   heading: 'Project planning for developers',
-//   description:
-//     'Create issues, break them into tasks, track relationships, add custom fields, and have conversations. Visualize large projects as spreadsheets or boards, and automate everything with code.',
-//   align: 'center',
-//   primaryAction: {
-//     text: 'Watch video',
-//     href: '#'
-//   },
-//   secondaryAction: {
-//     text: 'Start using project tables',
-//     href: '#'
-//   }
-// }
+export const NarrowViewCentered: ComponentStory<typeof Hero> = _args => (
+  <Hero align="center">
+    <Hero.Heading>Project planning for developers</Hero.Heading>
+    <Hero.Description>
+      Create issues, break them into tasks, track relationships, add custom fields, and have conversations. Visualize
+      large projects as spreadsheets or boards, and automate everything with code.
+    </Hero.Description>
+    <Hero.PrimaryAction href="#">Watch video</Hero.PrimaryAction>
+    <Hero.SecondaryAction href="#">Start using project tables</Hero.SecondaryAction>
+  </Hero>
+)
+
+NarrowViewCentered.parameters = {
+  viewport: {
+    defaultViewport: 'iphonexr'
+  }
+}
--- packages/react/src/Hero/Hero.tsx
+++ packages/react/src/Hero/Hero.tsx
@@ -2,7 +2,7 @@ import React, {forwardRef} from 'react'
 import clsx from 'clsx'
 import styles from './Hero.module.css'
 import {Button} from '../Button'
-import {Heading, HeadingTags} from '../Heading'
+import {Heading, HeadingProps, HeadingTags} from '../Heading'
 import {Text} from '../Text'
 
 import type {BaseProps} from '../component-helpers'
@@ -16,7 +16,22 @@ export type HeroProps = BaseProps<HTMLDivElement> & {
 const Root = forwardRef<HTMLDivElement, HeroProps>(({className, align = 'start', children, ...rest}, ref) => {
   return (
     <section className={clsx(styles.Hero, styles[`Hero--align-${align}`], className)} ref={ref} {...rest}>
-      {children}
+      {React.Children.map(children, child => {
+        if (React.isValidElement(child)) {
+          if (child.type === HeroHeading || child.type === HeroDescription) {
+            return child
+          }
+        }
+      })}
+      <div className={styles['Hero-actions']}>
+        {React.Children.map(children, child => {
+          if (React.isValidElement(child)) {
+            if (child.type === HeroPrimaryAction || child.type === HeroSecondaryAction) {
+              return child
+            }
+          }
+        })}
+      </div>
     </section>
   )
 })
@@ -24,7 +39,7 @@ const Root = forwardRef<HTMLDivElement, HeroProps>(({className, align = 'start',
 type HeroHeadingProps = {
   as?: HeadingTags['as']
   children: React.ReactNode | React.ReactNode[]
-}
+} & HeadingProps
 
 const HeroHeading = forwardRef<HTMLHeadingElement, HeroHeadingProps>(({as = 'h1', children, ...rest}, ref) => {
   return (
@@ -66,33 +81,6 @@ const HeroSecondaryAction = forwardRef<HTMLAnchorElement, HeroActions>(({href, c
     </Button>
   )
 })
-
-// export const Hero = forwardRef<HTMLDivElement, HeroProps>(
-//   ({className, heading, description, primaryAction, secondaryAction, align = 'start', ...rest}, ref) => {
-//     return (
-//       <div className={clsx(styles.Hero, styles[`Hero--align-${align}`], className)} ref={ref} {...rest}>
-//         <Heading className={styles['Hero-heading']} as="h1">
-//           {heading}
-//         </Heading>
-//         {description ? (
-//           <Text className={styles['Hero-description']} as="p" size="400" variant="muted">
-//             {description}
-//           </Text>
-//         ) : null}
-//         <div className={styles['Hero-actions']}>
-//           <Button as="a" variant="primary" size="large" href={primaryAction.href}>
-//             {primaryAction.text}
-//           </Button>
-//           {secondaryAction ? (
-//             <Button as="a" variant="secondary" size="large" href={secondaryAction.href}>
-//               {secondaryAction.text}
-//             </Button>
-//           ) : null}
-//         </div>
-//       </div>
-//     )
-//   }
-// )
 
 export const Hero = Object.assign(Root, {
   Heading: HeroHeading,
