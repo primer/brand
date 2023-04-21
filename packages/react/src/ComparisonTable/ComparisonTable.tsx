d5bf84dc41e0ee111afa8f4a6b5f8525c1dc6b2c
Ed Gim <egim@users.noreply.github.com>
2023-04-21 05:16:17 +0000

    Import the base styles. Add margins values.

--- packages/react/src/ComparisonTable/ComparisonTable.tsx
+++ packages/react/src/ComparisonTable/ComparisonTable.tsx
@@ -7,6 +7,7 @@ import type {BaseProps} from '../component-helpers'
 /**
  * Design tokens
  */
+import '@primer/brand-primitives/lib/design-tokens/css/tokens/functional/components/comparison-table/base.css'
 import '@primer/brand-primitives/lib/design-tokens/css/tokens/functional/components/comparison-table/colors-with-modes.css'
 
 /** * Main Stylesheet (as a CSS Module) */
