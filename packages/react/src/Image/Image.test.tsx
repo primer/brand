cc676dab1a7c099fde3d02a872686e0f8d659f2c
Joshua Bowden <jbowden@Joshuas-MBP.localdomain>
2023-04-26 11:26:28 -0400

    Updated Test

--- packages/react/src/Image/Image.test.tsx
+++ packages/react/src/Image/Image.test.tsx
@@ -213,19 +213,6 @@ describe('Image', () => {
         src="https://via.placeholder.com/600x400/d3d9df/d3d9df.png"
         as="picture"
         alt="alternative text"
-        srcSet="https://via.placeholder.com/600x400/d3d9df/d3d9df.png, https://via.placeholder.com/900x600/d3d9df/d3d9df.png 1.5x"
-      />
-    )
-
-    expect(container.querySelector('source')).toBeInTheDocument()
-  })
-
-  it('should create a source element when as is equal to picture and the srcSet property is set.', async () => {
-    const {container} = render(
-      <Image
-        src="https://via.placeholder.com/600x400/d3d9df/d3d9df.png"
-        as="picture"
-        alt="alternative text"
         sources={[
           {
             srcset: 'https://via.placeholder.com/600x400/d3d9df/d3d9df.png',
