edf215bb1730ab3c57417bf85b50b4f770c9b213
Josep Martins <josepmartins@github.com>
2023-05-02 12:00:56 +0200

    Update packages/react/src/Card/Card.features.stories.tsx
    Co-authored-by: Rez <rezrah@github.com>

--- packages/react/src/Card/Card.features.stories.tsx
+++ packages/react/src/Card/Card.features.stories.tsx
@@ -112,7 +112,7 @@ export const Image: ComponentStory<typeof Card> = () => {
 export const ImageAndLabel: ComponentStory<typeof Card> = () => {
   return (
     <Card href="https://github.com">
-      <Card.Image src="https://via.placeholder.com/600x400/d3d9df/d3d9df.png" alt="" />
+      <Card.Image src="https://via.placeholder.com/600x400/d3d9df/d3d9df.png" alt="placeholder, blank area with an gray background color" />
       <Card.Label color="blue-purple">Beta</Card.Label>
       <Card.Heading>Code search & code view</Card.Heading>
       <Card.Description>
