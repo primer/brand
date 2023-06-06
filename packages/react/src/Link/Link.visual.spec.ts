b6ba70d3004a42162b0fa08795aabaea1297e3de
Josep Martins <josepmartins@github.com>
2023-06-06 13:42:02 +0200

    [Button] Design guidelines (#292)
    * Add Button design guidelines

--- packages/react/src/Link/Link.visual.spec.ts
+++ packages/react/src/Link/Link.visual.spec.ts
@@ -7,20 +7,26 @@ import {test, expect} from '@playwright/test'
 
 // eslint-disable-next-line i18n-text/no-en
 test.describe('Visual Comparison: Link', () => {
-  test('Link / Default', async ({page}) => {
-    await page.goto('http://localhost:6006/iframe.html?args=&id=components-link--default&viewMode=story')
+  test('Link / Large', async ({page}) => {
+    await page.goto('http://localhost:6006/iframe.html?args=&id=components-link-features--large&viewMode=story')
 
     expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Link / Reversed', async ({page}) => {
-    await page.goto('http://localhost:6006/iframe.html?args=&id=components-link--reversed&viewMode=story')
+    await page.goto('http://localhost:6006/iframe.html?args=&id=components-link-features--reversed&viewMode=story')
 
     expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('Link / Accent', async ({page}) => {
-    await page.goto('http://localhost:6006/iframe.html?args=&id=components-link--accent&viewMode=story')
+    await page.goto('http://localhost:6006/iframe.html?args=&id=components-link-features--accent&viewMode=story')
+
+    expect(await page.screenshot()).toMatchSnapshot()
+  })
+
+  test('Link / Default', async ({page}) => {
+    await page.goto('http://localhost:6006/iframe.html?args=&id=components-link--default&viewMode=story')
 
     expect(await page.screenshot()).toMatchSnapshot()
   })
