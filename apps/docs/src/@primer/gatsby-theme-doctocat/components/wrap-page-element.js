c739e0c81afaf763457cb0211b0386d8411754c5
Josep Martins <josepmartins@github.com>
2023-07-05 13:29:01 +0200

    Add footnotes styles

--- apps/docs/src/@primer/gatsby-theme-doctocat/components/wrap-page-element.js
+++ apps/docs/src/@primer/gatsby-theme-doctocat/components/wrap-page-element.js
@@ -1,15 +1,38 @@
 import React from 'react'
-import {BaseStyles} from '@primer/react'
+import {BaseStyles, themeGet} from '@primer/react'
 import {ColorThemeProvider} from '../../../components/content/ColorThemeContext'
+import {createGlobalStyle} from 'styled-components'
 import '../../../../../../packages/react/lib/css/main.css'
 import '../../../../../../packages/fonts/fonts.css'
+
+// Adding global styles for footnotes
+
+const GlobalStyles = createGlobalStyle`
+  .footnotes {
+    font-size: ${themeGet('fontSizes.1')};
+    color: ${themeGet('colors.fg.subtle')};
+
+    ol {
+      padding-left: ${themeGet('space.3')};
+    }
+
+  .footnote-backref {
+      font-family: ${themeGet('fonts.mono')};
+      margin-left: 2px;
+      text-decoration: none;
+    }
+  }
+`
 
 // Shadowing this Doctocat file
 
 function wrapPageElement({element}) {
   return (
     <ColorThemeProvider>
-      <BaseStyles>{element}</BaseStyles>
+      <BaseStyles>
+        <GlobalStyles />
+        {element}
+      </BaseStyles>
     </ColorThemeProvider>
   )
 }
