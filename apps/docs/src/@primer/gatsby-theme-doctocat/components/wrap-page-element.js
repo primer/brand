000a8db9a8c03bb2c589d737ed2c19d4a0c16bec
Josep Martins <josepmartins@github.com>
2023-07-12 17:45:58 +0200

    Remove PRC themeGet

--- apps/docs/src/@primer/gatsby-theme-doctocat/components/wrap-page-element.js
+++ apps/docs/src/@primer/gatsby-theme-doctocat/components/wrap-page-element.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BaseStyles, themeGet} from '@primer/react'
+import {BaseStyles} from '@primer/react'
 import {ColorThemeProvider} from '../../../components/content/ColorThemeContext'
 import {createGlobalStyle} from 'styled-components'
 import '../../../../../../packages/react/lib/css/main.css'
@@ -7,22 +7,17 @@ import '../../../../../../packages/fonts/fonts.css'
 
 // Doctocat global styles https://github.com/primer/doctocat/blob/main/theme/src/components/wrap-page-element.js
 const GlobalStyles = createGlobalStyle`
-  body {
-    color: ${themeGet('colors.fg.default')};
-    background-color: ${themeGet('colors.canvas.default')};
-  }
-
   .footnotes {
-    font-size: ${themeGet('fontSizes.1')};
-    color: ${themeGet('colors.fg.subtle')};
+    font-size: var(--brand-text-size-200);
+    color: var(--brand-color-text-subtle);
 
     ol {
-      padding-left: ${themeGet('space.3')};
+      padding-left: var(--base-size-16);
     }
 
   .footnote-backref {
-      font-family: ${themeGet('fonts.mono')};
-      margin-left: 2px;
+      font-family: var(--brand-fontStack-monospace);
+      margin-left: var(--base-size-4);
       text-decoration: none;
     }
   }
