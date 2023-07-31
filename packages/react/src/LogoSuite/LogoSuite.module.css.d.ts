47c02c6f87be623d4d6eba7d663b92348f75ba1a
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-07-31 10:55:00 +0100

    add logo suite component

---
+++ packages/react/src/LogoSuite/LogoSuite.module.css.d.ts
@@ -0,0 +1,20 @@
+declare const styles: {
+  readonly "LogoSuite": string;
+  readonly "LogoSuite__heading": string;
+  readonly "LogoSuite__description": string;
+  readonly "LogoSuite--start": string;
+  readonly "LogoSuite--center": string;
+  readonly "LogoSuite--justify": string;
+  readonly "LogoSuite__logobar": string;
+  readonly "LogoSuite__logobar--variant-muted": string;
+  readonly "LogoSuite__logobar--variant-emphasis": string;
+  readonly "LogoSuite--hasDivider": string;
+  readonly "LogoSuite__logobar--has-marquee": string;
+  readonly "LogoSuite__logobar-marqueeGroup": string;
+  readonly "LogobarScrollX": string;
+  readonly "LogoSuite__logobar-marqueeGroup--speed-default": string;
+  readonly "LogoSuite__logobar-marqueeGroup--speed-slow": string;
+  readonly "LogoSuite__logobar-marquee": string;
+};
+export = styles;
+
---
+++ packages/react/src/LogoSuite/LogoSuite.test.tsx
@@ -0,0 +1,246 @@
+import React, {render, cleanup, act} from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import {LogoSuite} from './LogoSuite'
+import '../test-utils/mocks/match-media-mock'
+import {axe, toHaveNoViolations} from 'jest-axe'
+
+expect.extend(toHaveNoViolations)
+
+describe('LogoSuite', () => {
+  const mockHeading = 'Mock heading'
+  const mockDescription = 'Mock description'
+
+  afterEach(() => {
+    cleanup()
+    jest.clearAllMocks()
+  })
+
+  it('renders correctly into the document with default options', () => {
+    const testId = 'logobar'
+    const {getByText, getByTestId} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Description>{mockDescription}</LogoSuite.Description>
+        <LogoSuite.Logobar data-testid={testId}>
+          <svg data-testid="svg1" />
+          <svg data-testid="svg2" />
+        </LogoSuite.Logobar>
+      </LogoSuite>,
+    )
+
+    expect(getByText(mockHeading)).toBeInTheDocument()
+    expect(getByText(mockDescription)).toBeInTheDocument()
+    expect(getByTestId(testId).children.length).toBe(2)
+    expect(getByTestId('svg1')).toBeInTheDocument()
+    expect(getByTestId('svg2')).toBeInTheDocument()
+  })
+
+  it('renders logos correctly using a fragment', () => {
+    const {getByTestId} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Logobar>
+          <>
+            <svg data-testid="svg1" />
+            <svg data-testid="svg2" />
+          </>
+        </LogoSuite.Logobar>
+      </LogoSuite>,
+    )
+    const svg1Element = getByTestId('svg1')
+    const svg2Element = getByTestId('svg2')
+    expect(svg1Element).toBeInTheDocument()
+    expect(svg2Element).toBeInTheDocument()
+  })
+
+  it('renders logos correctly using anchors', () => {
+    const {getByLabelText, getAllByRole} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Logobar>
+          <a href="/" aria-label="Logo 1">
+            <svg role="img">
+              <title>Logo 1</title>
+            </svg>
+          </a>
+          <a href="/" aria-label="Logo 2">
+            <svg role="img">
+              <title>Logo 2</title>
+            </svg>
+          </a>
+        </LogoSuite.Logobar>
+      </LogoSuite>,
+    )
+    const logo1Element = getByLabelText('Logo 1')
+    const logo2Element = getByLabelText('Logo 2')
+    expect(logo1Element).toBeInTheDocument()
+    expect(logo2Element).toBeInTheDocument()
+
+    expect(getAllByRole('link').length).toBe(2)
+    expect(getAllByRole('img').length).toBe(2)
+  })
+
+  it('can render a heading as visually hidden', () => {
+    const {getByText} = render(
+      <LogoSuite>
+        <LogoSuite.Heading visuallyHidden>{mockHeading}</LogoSuite.Heading>
+      </LogoSuite>,
+    )
+    const headingEl = getByText(mockHeading)
+    expect(headingEl.classList).toContain('visually-hidden')
+  })
+
+  it('has no a11y violations', async () => {
+    const {container} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Description>{mockDescription}</LogoSuite.Description>
+        <LogoSuite.Logobar>
+          <svg />
+        </LogoSuite.Logobar>
+      </LogoSuite>,
+    )
+
+    const results = await axe(container)
+
+    expect(results).toHaveNoViolations()
+  })
+
+  it('renders the correct default heading tag', () => {
+    const expectedTag = 'h2'
+
+    const {getByText} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+      </LogoSuite>,
+    )
+    const header = getByText(mockHeading)
+
+    expect(header.tagName).toBe(expectedTag.toUpperCase())
+  })
+
+  it('renders the correct heading tag when an override is used', () => {
+    const expectedTag = 'h3'
+
+    const {getByText} = render(
+      <LogoSuite>
+        <LogoSuite.Heading as={expectedTag}>{mockHeading}</LogoSuite.Heading>
+      </LogoSuite>,
+    )
+    const header = getByText(mockHeading)
+
+    expect(header.tagName).toBe(expectedTag.toUpperCase())
+  })
+
+  it('renders a divider by default', () => {
+    const {getByTestId} = render(
+      <LogoSuite data-testid="root">
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+      </LogoSuite>,
+    )
+    const expectedClass = 'LogoSuite--hasDivider'
+
+    const rootEl = getByTestId('root')
+    expect(rootEl.classList).toContain(expectedClass)
+  })
+
+  it('displays text in the center by default', () => {
+    const {getByTestId} = render(
+      <LogoSuite data-testid="root">
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+      </LogoSuite>,
+    )
+    const expectedClass = 'LogoSuite--center'
+
+    const rootEl = getByTestId('root')
+    expect(rootEl.classList).toContain(expectedClass)
+  })
+
+  it('can optionally display text in start alignment', () => {
+    const {getByTestId} = render(
+      <LogoSuite data-testid="root" align="start">
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Description>{mockDescription}</LogoSuite.Description>
+      </LogoSuite>,
+    )
+    const expectedClass = 'LogoSuite--start'
+
+    const rootEl = getByTestId('root')
+    expect(rootEl.classList).toContain(expectedClass)
+  })
+
+  it('can optionally display text in justify alignment', () => {
+    const {getByTestId} = render(
+      <LogoSuite data-testid="root" align="justify">
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Description>{mockDescription}</LogoSuite.Description>
+      </LogoSuite>,
+    )
+    const expectedClass = 'LogoSuite--justify'
+
+    const rootEl = getByTestId('root')
+    expect(rootEl.classList).toContain(expectedClass)
+  })
+
+  it('renders a marquee when the marquee prop is true', () => {
+    const {getByTestId} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Logobar marquee data-testid="logobar">
+          <svg data-testid="svg1" />
+          <svg data-testid="svg2" />
+        </LogoSuite.Logobar>
+      </LogoSuite>,
+    )
+
+    const logobarEl = getByTestId('logobar')
+    const expectedClass = 'LogoSuite__logobar--has-marquee'
+
+    expect(logobarEl.classList).toContain(expectedClass)
+  })
+
+  it('renders a marquee at normal speed by default', () => {
+    const {getByTestId} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Logobar marquee>
+          <svg data-testid="svg1" />
+          <svg data-testid="svg2" />
+        </LogoSuite.Logobar>
+      </LogoSuite>,
+    )
+
+    const el = getByTestId(LogoSuite.testIds.marqueeGroup)
+    const expectedClass = 'LogoSuite__logobar-marqueeGroup--speed-normal'
+
+    expect(el.classList).toContain(expectedClass)
+  })
+
+  it('can optionally render a marquee at slower speed ', () => {
+    const {getByTestId} = render(
+      <LogoSuite>
+        <LogoSuite.Heading>{mockHeading}</LogoSuite.Heading>
+        <LogoSuite.Logobar marquee marqueeSpeed="slow">
+          <svg data-testid="svg1" />
+          <svg data-testid="svg2" />
+        </LogoSuite.Logobar>
+      </LogoSuite>,
+    )
+
+    const el = getByTestId(LogoSuite.testIds.marqueeGroup)
+    const expectedClass = 'LogoSuite__logobar-marqueeGroup--speed-slow'
+
+    expect(el.classList).toContain(expectedClass)
+  })
+
+  it('logs a warning message when HeadingChild is not provided', () => {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    const consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {})
+    render(<LogoSuite />)
+    expect(consoleWarnSpy).toHaveBeenCalledWith(
+      'LogoSuite: Heading child is required. You may use `visuallyHidden` to hide it from view.',
+    )
+    consoleWarnSpy.mockRestore()
+  })
+})
