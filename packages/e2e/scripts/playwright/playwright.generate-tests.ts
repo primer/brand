afac3a2efc9f746524d89bbd9965c10aeb03777f
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-07-31 12:30:33 +0100

    add visual snapshots

--- packages/e2e/scripts/playwright/playwright.generate-tests.ts
+++ packages/e2e/scripts/playwright/playwright.generate-tests.ts
@@ -83,6 +83,11 @@
     'components-animations-examples--logo-bar', // animation only
     'components-animations-examples--timeline-bar', // animation only
     'components-animations--playground', // animation only
+    'components-logosuite-features--marquee', // animation only
+    'components-logosuite-features--slower-marquee-speed', // animation only
+    'components-logosuite-features--mixed-width', // animation only
+    'components-logosuite-features--only-description', // animation only
+    'components-logosuite-features--following-hero', // animation only
   ]
 
   const categorisedStories = Object.keys(stories as Stories).reduce((acc, key) => {
