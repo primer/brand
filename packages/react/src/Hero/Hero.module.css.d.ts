4f17d86a3beae316f8e587a5a8a031bc263b189b
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-08-01 14:10:43 +0100

    add baseline feature

--- packages/react/src/Hero/Hero.module.css.d.ts
+++ packages/react/src/Hero/Hero.module.css.d.ts
@@ -1,8 +1,13 @@
 declare const styles: {
   readonly "Hero": string;
+  readonly "Hero--layout-image": string;
+  readonly "Hero--image-pos-inline-end": string;
+  readonly "Hero--image-pos-block-end": string;
   readonly "Hero--align-start": string;
   readonly "Hero--align-center": string;
   readonly "Hero-actions": string;
+  readonly "Hero-content": string;
+  readonly "Hero-image": string;
   readonly "Hero-heading": string;
   readonly "Hero-description": string;
 };
--- packages/react/src/Hero/Hero.tsx
+++ packages/react/src/Hero/Hero.tsx
@@ -7,6 +7,7 @@ import {Text, TextSizes, TextWeightVariants, ResponsiveWeightMap} from '../Text'
 
 import type {BaseProps} from '../component-helpers'
 import '@primer/brand-primitives/lib/design-tokens/css/tokens/functional/components/hero/base.css'
+import {Image, ImageProps} from '../Image'
 
 export type HeroProps = BaseProps<HTMLElement> & {
   align?: 'start' | 'center'
@@ -14,36 +15,53 @@ export type HeroProps = BaseProps<HTMLElement> & {
 
 const Root = forwardRef<HTMLElement, PropsWithChildren<HeroProps>>(
   ({className, align = 'start', children, ...rest}, ref) => {
-    const {HeroActions, HeroChildren} = useMemo(
+    const {HeroActions, HeroChildren, HeroImageChild} = useMemo(
       () =>
         React.Children.toArray(children).reduce<{
           HeroActions: React.ReactElement[]
+          HeroImageChild?: React.ReactElement
           HeroChildren: React.ReactElement[]
         }>(
           (acc, child) => {
             if (React.isValidElement(child)) {
               if (child.type === HeroPrimaryAction || child.type === HeroSecondaryAction) {
                 acc.HeroActions.push(child)
+              } else if (child.type === HeroImage) {
+                acc.HeroImageChild = child
               } else {
                 acc.HeroChildren.push(child)
               }
             }
             return acc
           },
-          {HeroActions: [], HeroChildren: []},
+          {HeroActions: [], HeroChildren: [], HeroImageChild: undefined},
         ),
       [children],
     )
 
+    const imagePosition = HeroImageChild?.props?.imagePosition || 'block-end'
+
+    const heroLayoutClass = HeroImageChild ? styles['Hero--layout-image'] : styles['Hero--layout-default']
+
+    const ContentTag = HeroImageChild ? 'div' : React.Fragment
+
     return (
       <section
-        className={clsx(styles.Hero, styles[`Hero--align-${align}`], className)}
+        className={clsx(
+          styles.Hero,
+          imagePosition !== 'inline-end' && styles[`Hero--align-${align}`],
+          heroLayoutClass,
+          HeroImageChild && styles[`Hero--image-pos-${imagePosition}`],
+        )}
         ref={ref}
         aria-labelledby="hero-section-brand-heading"
         {...rest}
       >
-        {HeroChildren}
-        <div className={styles['Hero-actions']}>{HeroActions}</div>
+        <ContentTag className={styles['Hero-content']}>
+          {HeroChildren}
+          <div className={styles['Hero-actions']}>{HeroActions}</div>
+        </ContentTag>
+        {HeroImageChild && HeroImageChild}
       </section>
     )
   },
@@ -69,6 +87,17 @@ function HeroDescription({size = '400', weight, children}: PropsWithChildren<Her
     <Text className={styles['Hero-description']} as="p" size={size} weight={weight} variant="muted">
       {children}
     </Text>
+  )
+}
+
+type HeroImageProps = {
+  imagePosition?: 'inline-end' | 'block-end'
+} & ImageProps &
+  BaseProps<HTMLImageElement>
+
+function HeroImage({imagePosition = 'block-end', className, ...rest}: PropsWithChildren<HeroImageProps>) {
+  return (
+    <Image className={clsx(styles['Hero-image'], styles[`Hero-image--pos-${imagePosition}`], className)} {...rest} />
   )
 }
 
@@ -107,4 +136,5 @@ export const Hero = Object.assign(Root, {
   Description: HeroDescription,
   PrimaryAction: HeroPrimaryAction,
   SecondaryAction: HeroSecondaryAction,
+  Image: HeroImage,
 })
