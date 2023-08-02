bf90d29909caf5e9babe58cac87fce5153c6574c
Josh Bowden <31220082+JoshBowdenConcepts@users.noreply.github.com>
2023-08-02 17:21:38 -0400

    Updated Play Icon Size

--- packages/react/src/VideoPlayer/VideoPlayer.tsx
+++ packages/react/src/VideoPlayer/VideoPlayer.tsx
@@ -152,7 +152,7 @@ export function VideoPlayer({
       <button className={styles.VideoPlayer__playButton} onClick={handleVideoPlayback}>
         <span>
           {!playing && (
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none">
               <path
                 d="M1.56489 23.8112C0.884817 24.2389 1.07491e-06 23.7501 1.0398e-06 22.9467L8.28679e-08 1.05473C4.73246e-08 0.241596 0.904067 -0.245404 1.58307 0.201969L18.5829 11.4026C19.2032 11.8113 19.1935 12.7244 18.5647 13.1198L1.56489 23.8112Z"
                 fill="currentColor"
