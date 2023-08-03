6d721ae1613a5216cd06caf09edc62e9ca67e7fe
Josh Bowden <31220082+JoshBowdenConcepts@users.noreply.github.com>
2023-08-03 15:13:38 -0400

    Added Starter Documentation

---
+++ packages/react/src/VideoPlayer/VideoPlayer.module.css.d.ts
@@ -0,0 +1,24 @@
+declare const styles: {
+  readonly "VideoPlayer__container": string;
+  readonly "VideoPlayer": string;
+  readonly "VideoPlayer__playButton": string;
+  readonly "VideoPlayer__title": string;
+  readonly "VideoPlayer__showOverlays": string;
+  readonly "VideoPlayer__overlays": string;
+  readonly "VideoPlayer__controls": string;
+  readonly "VideoPlayer__iconControl": string;
+  readonly "VideoPlayer__tooltip": string;
+  readonly "VideoPlayer__progressTime": string;
+  readonly "VideoPlayer__progressBar2": string;
+  readonly "VideoPlayer__closedCaption": string;
+  readonly "VideoPlayer__ccOff": string;
+  readonly "VideoPlayer__ccText": string;
+  readonly "VideoPlayer__range": string;
+  readonly "VideoPlayer__rangeInput": string;
+  readonly "VideoPlayer__rangeProgress": string;
+  readonly "VideoPlayer__tooltipContent": string;
+  readonly "VideoPlayer__tooltipText": string;
+  readonly "VideoPlayer__controlTextColor": string;
+};
+export = styles;
+
