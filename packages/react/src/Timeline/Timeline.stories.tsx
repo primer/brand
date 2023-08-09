3872a501084a8b9930c9f41aef9ff13f3be1cf4b
Daniel Guillan <danielguillan@github.com>
2023-08-09 16:20:24 +0200

    Add more tests and stories

--- packages/react/src/Timeline/Timeline.stories.tsx
+++ packages/react/src/Timeline/Timeline.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import {StoryFn, Meta} from '@storybook/react'
+import { StoryFn, Meta } from '@storybook/react'
 
-import {Timeline} from '.'
+import { Timeline } from '.'
 
 export default {
   title: 'Components/Timeline',
@@ -11,15 +11,13 @@ export default {
 const Template: StoryFn<typeof Timeline> = () => (
   <Timeline>
     <Timeline.Item>
-      <em>GitHub Codespaces</em> offers a complete dev environment in seconds, so you can code, build, test, and open
-      pull requests from any repo anywhere.
+      GitHub Codespaces offers a complete dev environment in seconds, so you can code, build, test, and open pull requests from any repo anywhere.
     </Timeline.Item>
     <Timeline.Item>
-      <em>GitHub Copilot</em> is your AI pair programmer that empowers you to complete tasks 55% faster by turning
-      natural language prompts into coding suggestions.
+      GitHub Copilot is your AI pair programmer that empowers you to complete tasks 55% faster by turning natural language prompts into coding suggestions.
     </Timeline.Item>
     <Timeline.Item>
-      <em>GitHub Actions</em> automates your build, test, and deployment workflow with simple and secure CI/CD.
+      GitHub Actions automates your build, test, and deployment workflow with simple and secure CI/CD.
     </Timeline.Item>
   </Timeline>
 )
