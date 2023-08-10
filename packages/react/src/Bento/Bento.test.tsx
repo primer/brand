bc08543135ccc4c9e80bd2d497ee21b286db7768
Josh Bowden <31220082+JoshBowdenConcepts@users.noreply.github.com>
2023-08-10 15:35:25 -0400

    Added 100% test coverage to this point

--- packages/react/src/Bento/Bento.test.tsx
+++ packages/react/src/Bento/Bento.test.tsx
@@ -1,7 +1,7 @@
 import React, {render, cleanup} from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-import {Bento} from './Bento'
+import {Bento, ColumnIndex} from './Bento'
 import {Heading, Text, Link} from '../'
 import {axe, toHaveNoViolations} from 'jest-axe'
 
@@ -46,6 +46,46 @@ describe('Bento.Item', () => {
 
     const BentoItemEl = getByTestId(testId)
     expect(BentoItemEl.classList).toContain(expectedClass)
+  })
+
+  it('adds the class for a column span based on the value of that span 1 - 12', () => {
+    const columnSpanValue = Math.floor(Math.random() * 12) + 1 // Can be any number 1 - 12
+    const {getByTestId} = render(<Bento.Item data-testid={testId} columnSpan={columnSpanValue as ColumnIndex} />)
+
+    const BentoItemEl = getByTestId(testId)
+    expect(BentoItemEl.classList).toContain(`Bento__Item--column-span-${columnSpanValue}`)
+  })
+
+  it('adds the class for a column start based on the value of that span 1 - 12', () => {
+    const columnSpanValue = Math.floor(Math.random() * 12) + 1 // Can be any number 1 - 12
+    const {getByTestId} = render(<Bento.Item data-testid={testId} columnStart={columnSpanValue as ColumnIndex} />)
+
+    const BentoItemEl = getByTestId(testId)
+    expect(BentoItemEl.classList).toContain(`Bento__Item--column-start-${columnSpanValue}`)
+  })
+
+  it('adds the class for a row span based on the value of that span', () => {
+    const columnSpanValue = 20
+    const {getByTestId} = render(<Bento.Item data-testid={testId} rowSpan={columnSpanValue} />)
+
+    const BentoItemEl = getByTestId(testId)
+    expect(BentoItemEl.classList).toContain(`Bento__Item--row-span-${columnSpanValue}`)
+  })
+
+  it('adds the class for a row start based on the value of that span', () => {
+    const columnSpanValue = 20
+    const {getByTestId} = render(<Bento.Item data-testid={testId} rowStart={columnSpanValue} />)
+
+    const BentoItemEl = getByTestId(testId)
+    expect(BentoItemEl.classList).toContain(`Bento__Item--row-start-${columnSpanValue}`)
+  })
+
+  it('adds the class for flow', () => {
+    const flow = 'row'
+    const {getByTestId} = render(<Bento.Item data-testid={testId} flow={flow} />)
+
+    const BentoItemEl = getByTestId(testId)
+    expect(BentoItemEl.classList).toContain(`Bento__Item-flow--${flow}`)
   })
 
   it('has no a11y violations', async () => {
@@ -100,8 +140,79 @@ describe('Bento.Content', () => {
     expect(getByText(linkText)).toBeInTheDocument()
   })
 
+  it('adds the class for padding', () => {
+    const padding = 'condensed'
+    const {getByTestId} = render(<Bento.Content data-testid={testId} padding={padding} />)
+
+    const BentoContentEl = getByTestId(testId)
+    expect(BentoContentEl.classList).toContain(`Bento-padding--${padding}`)
+  })
+
   it('has no a11y violations', async () => {
     const {container} = render(<Bento.Content />)
+
+    const results = await axe(container)
+
+    expect(results).toHaveNoViolations()
+  })
+})
+
+describe('Bento.Visual', () => {
+  const testId = 'test'
+  afterEach(() => {
+    cleanup()
+    jest.clearAllMocks()
+  })
+
+  it('renders correctly into the document', () => {
+    const expectedClass = 'Bento.Visual'
+
+    const {getByTestId} = render(<Bento.Visual data-testid={testId} className={expectedClass} />)
+
+    const BentoVisualEl = getByTestId(testId)
+    expect(BentoVisualEl.classList).toContain(expectedClass)
+  })
+
+  it('adds objectPosition style to img elements as children', () => {
+    const {getByTestId} = render(
+      <Bento.Visual data-testid={testId}>
+        <img src="test.png" />
+      </Bento.Visual>,
+    )
+
+    const imgEl = getByTestId(testId).querySelector('img')
+    expect(imgEl).toHaveStyle('object-position: 50% 50%')
+  })
+
+  it('renders non img children without style', () => {
+    const {getByTestId} = render(
+      <Bento.Visual data-testid={testId}>
+        <svg />
+      </Bento.Visual>,
+    )
+
+    const svgEl = getByTestId(testId).querySelector('svg')
+    expect(svgEl).not.toHaveStyle('object-position: 50% 50%')
+  })
+
+  it('adds the class for padding', () => {
+    const padding = 'condensed'
+    const {getByTestId} = render(<Bento.Visual data-testid={testId} padding={padding} />)
+
+    const BentoVisualEl = getByTestId(testId)
+    expect(BentoVisualEl.classList).toContain(`Bento-padding--${padding}`)
+  })
+
+  it('adds the class for fillMedia', () => {
+    const fillMedia = false
+    const {getByTestId} = render(<Bento.Visual data-testid={testId} fillMedia={fillMedia} />)
+
+    const BentoVisualEl = getByTestId(testId)
+    expect(BentoVisualEl.classList).toContain('Bento__Visual-no-fill')
+  })
+
+  it('has no a11y violations', async () => {
+    const {container} = render(<Bento.Visual />)
 
     const results = await axe(container)
 
--- packages/react/src/Bento/Bento.tsx
+++ packages/react/src/Bento/Bento.tsx
@@ -4,7 +4,7 @@ import {Heading, Text, Link, HeadingProps, TextProps, LinkProps} from '../'
 
 import styles from './Bento.module.css'
 
-type ColumnIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+export type ColumnIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 type ResponsiveMap = {
   xs?: 2
   sm?: 4
