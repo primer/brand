4f1a241a8834f09a467dbeed8d13a84734df248e
Mike Perrotti <mperrotti@github.com>
2023-08-14 11:24:48 -0400

    updates CSS types

--- packages/react/src/River/River.module.css.d.ts
+++ packages/react/src/River/River.module.css.d.ts
@@ -1,14 +1,14 @@
 declare const styles: {
   readonly "River": string;
+  readonly "River--breakout": string;
+  readonly "River__text": string;
+  readonly "River__hiddenBreakoutHeading": string;
   readonly "River--align-center": string;
   readonly "River__visual": string;
   readonly "River__visual--fill-media": string;
   readonly "River__visual--has-shadow": string;
   readonly "River__heading": string;
   readonly "River__call-to-action": string;
-  readonly "River__text": string;
-  readonly "River__hiddenBreakoutHeading": string;
-  readonly "River__breakoutCopy": string;
   readonly "River--50-50": string;
   readonly "River--60-40": string;
   readonly "River--align-start": string;
