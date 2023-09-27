fc95bf32c43492009de62398e058bcd75d63ed61
Josep Martins <josepmartins@github.com>
2023-09-27 13:42:31 +0200

    Style fixes and examples

--- packages/react/src/Prose/Prose.features.stories.tsx
+++ packages/react/src/Prose/Prose.features.stories.tsx
@@ -18,7 +18,10 @@ export default {
 const ExampleHtmlMarkup = `
 <h2>Heading level 2</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempor lectus ipsum, consectetur convallis diam pretium quis. Proin ut felis ut eros tristique tincidunt.</p>
-    <blockquote>Nulla ac odio eu magna hendrerit porta. Donec nec eros quis tortor tincidunt vulputate. Aenean id pharetra diam, sit amet auctor leo. Aliquam erat volutpat.</blockquote>
+    <blockquote>
+      <p>Nulla ac odio eu magna hendrerit porta. Donec nec eros quis tortor tincidunt vulputate. Aenean id pharetra diam, sit amet auctor leo. Aliquam erat volutpat.</p>
+      <figcaption>Lisa Vanderschuit, Engineering Program Manager, Shopify</figcaption>
+    </blockquote>
     <p>Integer pellentesque pretium nulla viverra molestie. Praesent quis pretium sapien. Sed convallis eget lectus et pulvinar:</p>
     <ul>
       <li>
@@ -50,6 +53,7 @@ const ExampleHtmlMarkup = `
     </ol>
     <h6>Heading level 6</h6>
     <p>Pellentesque non ornare ligula. Suspendisse nibh purus, pretium id tortor sit amet, tincidunt gravida augue.</p>
+    <p><code>for-each-ref</code> is extremely useful for listing references, finding which references point at a given object (with <code>--points-at</code>), which references have been merged into a given branch (with <code>--merged</code>), or which references contain a given commit (with <code>--contains</code>).</p>        
     <p>Nunc velit odio, posuere eu felis eget, consectetur fermentum nisi. Aenean tempor odio id ornare ultrices. Quisque blandit condimentum tellus, semper efficitur sapien dapibus nec. </p>
 `
 
