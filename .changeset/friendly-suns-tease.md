3227a7dd0366961618899e236b48398272e7a7c1
Josep Martins <josepmartins@github.com>
2023-10-09 15:19:59 +0200

    [Prose] Add `code` and `blockquote` (#435)

---
+++ .changeset/friendly-suns-tease.md
@@ -0,0 +1,5 @@
+---
+'@primer/react-brand': patch
+---
+
+Adds `Blockquote`, `Code`, `Figure` and `Figcaption` to the `Prose` component.
--- packages/react/src/Prose/Prose.stories.tsx
+++ packages/react/src/Prose/Prose.stories.tsx
@@ -22,15 +22,20 @@ export default {
 const ExampleHtmlMarkup = `
 <h2>Heading level 2</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tempor lectus ipsum, consectetur convallis diam pretium quis. Proin ut felis ut eros tristique tincidunt.</p>
-    <blockquote>Nulla ac odio eu magna hendrerit porta. Donec nec eros quis tortor tincidunt vulputate. Aenean id pharetra diam, sit amet auctor leo. Aliquam erat volutpat.</blockquote>
+    <figure>
+      <blockquote>
+        <p>Nulla ac odio eu magna hendrerit porta. Donec nec eros quis tortor tincidunt vulputate. Aenean id pharetra diam, sit amet auctor leo. Aliquam erat volutpat.</p>
+        <figcaption>Lisa Vanderschuit, Engineering Program Manager, Shopify</figcaption>
+      </blockquote>
+    </figure>
     <p>Integer pellentesque pretium nulla viverra molestie. Praesent quis pretium sapien. Sed convallis eget lectus et pulvinar:</p>
     <ul>
       <li>
-        Vivamus eu risus nec lectus consequat rutrum at vel lacus.
+        <strong>Vivamus</strong> eu risus nec lectus consequat rutrum at vel lacus.
       </li>
-      <li>Donec at dolor ut metus imperdiet congue vel porta nunc.
+      <li><strong>Donec</strong> at dolor ut metus imperdiet congue vel porta nunc.
       </li>
-      <li>Quisque eu tortor suscipit, congue quam in, bibendum tellus.</li>
+      <li><strong>Quisque</strong> eu tortor suscipit, congue quam in, bibendum tellus.</li>
     </ul>
     <h3>Heading level 3</h3>
     <p>Pellentesque non ornare ligula. Suspendisse nibh purus, pretium id tortor sit amet, tincidunt gravida augue. Ut malesuada, nisl vel dignissim mollis</p>
@@ -52,6 +57,7 @@ const ExampleHtmlMarkup = `
       </li>
       <li>Quisque eu tortor suscipit, congue quam in, bibendum tellus.</li>
     </ol>
+    <p><code>for-each-ref</code> is extremely useful for listing references, finding which references point at a given object (with <code>--points-at</code>), which references have been merged into a given branch (with <code>--merged</code>), or which references contain a given commit (with <code>--contains</code>).</p>    
     <h6>Heading level 6</h6>
     <p>Pellentesque non ornare ligula. Suspendisse nibh purus, pretium id tortor sit amet, tincidunt gravida augue.</p>
     <p>Nunc velit odio, posuere eu felis eget, consectetur fermentum nisi. Aenean tempor odio id ornare ultrices. Quisque blandit condimentum tellus, semper efficitur sapien dapibus nec. </p>
