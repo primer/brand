1327bb80b3f76d781c67b0104a360f4f334569b0
Joseph Lozano <joseph-lozano@github.com>
2023-10-16 08:39:47 -0400

    change listStyleImageUrl to imageUrl

--- packages/design-tokens/src/tokens/functional/components/prose/colors.js
+++ packages/design-tokens/src/tokens/functional/components/prose/colors.js
@@ -1,7 +1,7 @@
 module.exports = {
   brand: {
     Prose: {
-      listStyleImageUrl: {
+      imageUrl: {
         default: {
           value: `url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' aria-hidden='true' focusable='false' role='img' viewBox='0 -3 16 16' width='16' height='16' fill='black' style='display: inline-block; vertical-align: text-bottom; overflow: visible;'%3e%3cpath d='M2 7.75A.75.75 0 0 1 2.75 7h10a.75.75 0 0 1 0 1.5h-10A.75.75 0 0 1 2 7.75Z'%3e%3c/path%3e%3c/svg%3e ")`,
           dark: `url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' aria-hidden='true' focusable='false' role='img' viewBox='0 -3 16 16' width='16' height='16' fill='white' style='display: inline-block; vertical-align: text-bottom; overflow: visible;'%3e%3cpath d='M2 7.75A.75.75 0 0 1 2.75 7h10a.75.75 0 0 1 0 1.5h-10A.75.75 0 0 1 2 7.75Z'%3e%3c/path%3e%3c/svg%3e ")`,
