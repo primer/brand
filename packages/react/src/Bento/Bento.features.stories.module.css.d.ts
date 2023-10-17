8b2e0951668071c3a348ebfee7ad9be64a194df7
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-10-17 12:49:37 +0100

    add custom animation example to Bento

--- packages/react/src/Bento/Bento.features.stories.module.css.d.ts
+++ packages/react/src/Bento/Bento.features.stories.module.css.d.ts
@@ -7,6 +7,11 @@ declare const styles: {
   readonly "Mixed3__customColumns": string;
   readonly "bentoImageBg": string;
   readonly "Mixed3__image": string;
+  readonly "CustomVisual__item": string;
+  readonly "CustomElement": string;
+  readonly "moveGradient": string;
+  readonly "CustomElement--start": string;
+  readonly "CustomElement__icon": string;
 };
 export = styles;
 
