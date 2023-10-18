39ff37ebde8b48d263db516e9bdad3124bf8b74c
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2023-10-18 16:35:58 +0100

    torch naming

--- packages/react/src/Card/CardSkewEffect.tsx
+++ packages/react/src/Card/CardSkewEffect.tsx
@@ -19,7 +19,7 @@ export const CardSkewEffect = ({
 }: PropsWithChildren<CardSkewEffectProps>) => {
   const {colorMode} = useTheme()
   const boundingRef = useRef<HTMLDivElement | null>(null)
-  const shineRef = useRef<HTMLDivElement | null>(null)
+  const torchRef = useRef<HTMLDivElement | null>(null)
 
   useEffect(() => {
     if (colorMode === 'light') return
@@ -27,7 +27,7 @@ export const CardSkewEffect = ({
     const motionQuery = window.matchMedia('(prefers-reduced-motion: reduce)')
 
     const boundingElement = boundingRef.current
-    const shineElement = shineRef.current
+    const torchElement = torchRef.current
 
     if (!boundingElement) {
       return
@@ -43,13 +43,13 @@ export const CardSkewEffect = ({
 
     const handleMouseMove = e => {
       const rect = boundingElement.getBoundingClientRect()
-      const x = shineElement ? rect.width - (e.clientX - rect.left) - shineElement.offsetWidth / 2 : 0
-      const y = shineElement ? rect.height - (e.clientY - rect.top) - shineElement.offsetHeight / 2 : 0
+      const x = torchElement ? rect.width - (e.clientX - rect.left) - torchElement.offsetWidth / 2 : 0
+      const y = torchElement ? rect.height - (e.clientY - rect.top) - torchElement.offsetHeight / 2 : 0
       const skewY = !disableSkew ? -((e.clientX - rect.left - rect.width / 2) / rect.width) * 3 : 0
       const skewX = !disableSkew ? ((e.clientY - rect.top - rect.height / 2) / rect.height) * 2 : 0
 
-      if (shineElement) {
-        shineElement.style.transform = `translate(${-x}px, ${-y / 2}px)`
+      if (torchElement) {
+        torchElement.style.transform = `translate(${-x}px, ${-y / 2}px)`
       }
 
       if (!disableSkew) {
@@ -66,7 +66,7 @@ export const CardSkewEffect = ({
 
     return () => {
       boundingElement.removeEventListener('mousemove', handleMouseMove)
-      shineElement?.removeEventListener('mousemove', handleMouseMove)
+      torchElement?.removeEventListener('mousemove', handleMouseMove)
       motionQuery.removeEventListener('change', handleReducedMotionChange)
     }
   }, [disableSkew, perspective, colorMode])
@@ -75,7 +75,7 @@ export const CardSkewEffect = ({
     <div ref={boundingRef} className={styles['Card--skew']} {...rest}>
       <div className={styles['Card__skew-bounding']}>
         {children}
-        <div ref={shineRef} className={clsx(styles.Card__shine, styles[`Card--skew-colorMode-${colorMode}`])} />
+        <div ref={torchRef} className={clsx(styles.Card__torch, styles[`Card--skew-colorMode-${colorMode}`])} />
       </div>
     </div>
   )
