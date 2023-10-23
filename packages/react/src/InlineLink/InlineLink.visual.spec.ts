17618b2be2c617786375c81303a11832e8e334c0
Joseph Lozano <joseph-lozano@github.com>
2023-10-23 12:48:24 -0400

    reset visual specs

--- packages/react/src/InlineLink/InlineLink.visual.spec.ts
+++ packages/react/src/InlineLink/InlineLink.visual.spec.ts
@@ -11,20 +11,27 @@ test.describe('Visual Comparison: InlineLink', () => {
     await page.goto('http://localhost:6006/iframe.html?args=&id=components-inlinelink--default&viewMode=story')
 
     await page.waitForTimeout(500)
-    expect(await page.screenshot({fullPage: true})).toMatchSnapshot()
+    expect(await page.screenshot()).toMatchSnapshot()
+  })
+
+  test('InlineLink / Scale', async ({page}) => {
+    await page.goto('http://localhost:6006/iframe.html?args=&id=components-inlinelink--scale&viewMode=story')
+
+    await page.waitForTimeout(500)
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('InlineLink / Example', async ({page}) => {
     await page.goto('http://localhost:6006/iframe.html?args=&id=components-inlinelink--example&viewMode=story')
 
     await page.waitForTimeout(500)
-    expect(await page.screenshot({fullPage: true})).toMatchSnapshot()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 
   test('InlineLink / Size Inheritence', async ({page}) => {
     await page.goto('http://localhost:6006/iframe.html?args=&id=components-inlinelink--size-inheritence&viewMode=story')
 
     await page.waitForTimeout(500)
-    expect(await page.screenshot({fullPage: true})).toMatchSnapshot()
+    expect(await page.screenshot()).toMatchSnapshot()
   })
 })
