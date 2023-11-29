c63806d9bef67e711a9064291e299f646fdb0bb2
Katie Langerman <18661030+langermank@users.noreply.github.com>
2023-11-29 22:46:59 +0000

    remove unused tokens

--- packages/design-tokens/src/tokens/functional/components/action-menu/colors.js
+++ packages/design-tokens/src/tokens/functional/components/action-menu/colors.js
@@ -2,20 +2,6 @@ module.exports = {
   brand: {
     ActionMenu: {
       color: {
-        border: {
-          rest: {
-            value: 'var(--brand-color-border-default)',
-            dark: 'var(--brand-color-border-default)',
-          },
-          hover: {
-            value: 'var(--base-color-scale-black-0)',
-            dark: 'var(--base-color-scale-white-0)',
-          },
-          active: {
-            value: 'var(--base-color-scale-black-0)',
-            dark: 'var(--base-color-scale-white-0)',
-          },
-        },
         item: {
           hover: {
             value: 'var(--base-color-scale-gray-1)',
