b6d9b3975ef07b40e7da95254410601a55eb03bc
Nikola Stamatović <nikola@stamat.info>
2024-02-26 14:59:56 +0000

    correctly merge

--- packages/react/src/SubdomainNavBar/SubdomainNavBar.tsx
+++ packages/react/src/SubdomainNavBar/SubdomainNavBar.tsx
@@ -253,34 +253,7 @@ function Root({
                 </button>
               )}
 
-            {!isMedium && (
-              <div
-                className={clsx(
-                  styles['SubdomainNavBar-menu-wrapper'],
-                  menuHidden && styles['SubdomainNavBar-menu-wrapper--close'],
-                )}
-              >
-                <div>
-                  {title && titleHref && (
-                    <Text as="p">
-                      <a
-                        href={titleHref}
-                        aria-label={`${title} home`}
-                        className={clsx(styles['SubdomainNavBar-link'], styles['SubdomainNavBar-link--title'])}
-                      >
-                        {title}
-                      </a>
-                    </Text>
-                  )}
-                  {hasLinks && !menuHidden && (
-                    <NavigationVisbilityObserver
-                      showOnlyOnNarrow
-                      className={clsx(styles['SubdomainNavBar-primary-nav-list--visible'])}
-                    >
-                      {menuItems}
-                    </NavigationVisbilityObserver>
-                  )}
-                </div>
+              {isMedium && (
                 <div
                   className={clsx(
                     styles['SubdomainNavBar-button-area'],
@@ -320,14 +293,27 @@ function Root({
                     menuHidden && styles['SubdomainNavBar-menu-wrapper--close'],
                   )}
                 >
-                  {hasLinks && !menuHidden && (
-                    <NavigationVisbilityObserver
-                      showOnlyOnNarrow
-                      className={clsx(styles['SubdomainNavBar-primary-nav-list--visible'])}
-                    >
-                      {menuItems}
-                    </NavigationVisbilityObserver>
-                  )}
+                  <div>
+                    {title && titleHref && (
+                      <Text as="p">
+                        <a
+                          href={titleHref}
+                          aria-label={`${title} home`}
+                          className={clsx(styles['SubdomainNavBar-link'], styles['SubdomainNavBar-link--title'])}
+                        >
+                          {title}
+                        </a>
+                      </Text>
+                    )}
+                    {hasLinks && !menuHidden && (
+                      <NavigationVisbilityObserver
+                        showOnlyOnNarrow
+                        className={clsx(styles['SubdomainNavBar-primary-nav-list--visible'])}
+                      >
+                        {menuItems}
+                      </NavigationVisbilityObserver>
+                    )}
+                  </div>
                   <div
                     className={clsx(
                       styles['SubdomainNavBar-button-area'],
