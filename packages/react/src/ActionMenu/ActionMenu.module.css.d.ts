1e5cbe2d6a52d336448f27fab90330f4b5ccff73
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2024-03-01 15:47:37 +0000

    reduce paddings on small action menu variant

--- packages/react/src/ActionMenu/ActionMenu.module.css.d.ts
+++ packages/react/src/ActionMenu/ActionMenu.module.css.d.ts
@@ -3,11 +3,11 @@ declare const styles: {
   readonly "ActionMenu--disabled": string;
   readonly "ActionMenu__button": string;
   readonly "ActionMenu__menu": string;
+  readonly "ActionMenu__menu--small": string;
+  readonly "ActionMenu__menu--medium": string;
   readonly "ActionMenu__menu--visible": string;
   readonly "ActionMenu__menu--pos-outside-bottom": string;
   readonly "ActionMenu__menu--pos-outside-top": string;
-  readonly "ActionMenu__menu--medium": string;
-  readonly "ActionMenu__menu--small": string;
   readonly "ActionMenu__item": string;
   readonly "ActionMenu__item--medium": string;
   readonly "ActionMenu__item--small": string;
