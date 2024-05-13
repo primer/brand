ddcd8b1889101ff142bf2bd4ecd2b724c15da75d
Josh Farrant <joshfarrant@github.com>
2024-05-13 17:16:41 +0100

    SEO templates accessibility fixes (#591)
    * add aria-current to category page topic nav
    
    * add label to topics nav
    
    * correct "DevOps" casing
    
    * identify table of contents label in nav
    
    * fix typo
    
    * remove unnecessary PropsWithChildren
    
    * github-actions[bot] Regenerated snapshots
    
    ---------
    
    Co-authored-by: joshfarrant <joshfarrant@users.noreply.github.com>

--- packages/react/src/recipes/seo/Article/Article.tsx
+++ packages/react/src/recipes/seo/Article/Article.tsx
@@ -35,7 +35,7 @@ import styles from './Article.module.css'
 const realWorldContent = [
   // eslint-disable-next-line github/unescaped-html-literal
   `<p>
-    While DevOps is sometimes referred to as a methodology, it&apos;s better understood as a set of practices, 
+    While DevOps is sometimes referred to as a methodology, it&apos;s better understood as a set of practices,
     ideas, and methodologies that share a common philosophy: continuous value delivery. Put simply, there&apos;s no one-size-fits-all approach to DevOps—and successful examples of DevOps can look very different from one organization to another.
   </p>
   <p>
@@ -156,7 +156,7 @@ const systemContent = [
     </li>
     <li>Quisque eu tortor suscipit, congue quam in, bibendum tellus.</li>
   </ol>
-  <p><code>for-each-ref</code> is extremely useful for listing references, finding which references point at a given object (with <code>--points-at</code>), which references have been merged into a given branch (with <code>--merged</code>), or which references contain a given commit (with <code>--contains</code>).</p>    
+  <p><code>for-each-ref</code> is extremely useful for listing references, finding which references point at a given object (with <code>--points-at</code>), which references have been merged into a given branch (with <code>--merged</code>), or which references contain a given commit (with <code>--contains</code>).</p>
   <h6>Heading level 6</h6>
   <p>Pellentesque non ornare ligula. Suspendisse nibh purus, pretium id tortor sit amet, tincidunt gravida augue.</p>
   <p>Nunc velit odio, posuere eu felis eget, consectetur fermentum nisi. Aenean tempor odio id ornare ultrices. Quisque blandit condimentum tellus, semper efficitur sapien dapibus nec. </p>
@@ -168,10 +168,8 @@ const contentMap = {
   system: systemContent,
 }
 
-const AsideHeading = ({as = 'h2', children}: HeadingProps) => (
-  <Heading as={as} size="subhead-medium" font="monospace" className={styles.asideHeading} weight="medium">
-    {children}
-  </Heading>
+const AsideHeading = ({as = 'h2', ...props}: HeadingProps) => (
+  <Heading as={as} size="subhead-medium" font="monospace" className={styles.asideHeading} weight="medium" {...props} />
 )
 
 const TableOfContents = ({content = 'real-world', active}) => {
@@ -244,7 +242,7 @@ const TableOfContents = ({content = 'real-world', active}) => {
   }, [setNarrowMenuOpen, narrowMenuOpen])
 
   return (
-    <nav aria-label="Table of contents">
+    <nav aria-labelledby="table-of-contents-heading">
       <Stack
         direction="vertical"
         gap={64}
@@ -257,7 +255,7 @@ const TableOfContents = ({content = 'real-world', active}) => {
       >
         <Stack direction="vertical" padding="none" gap={24}>
           <Stack direction="horizontal" padding="none" justifyContent="space-between" alignItems="center">
-            <AsideHeading>Table of contents</AsideHeading>
+            <AsideHeading id="table-of-contents-heading">Table of contents</AsideHeading>
             <button className={styles.tableOfContentsMenuToggle} onClick={handleNarrowMenu}>
               {narrowMenuOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
             </button>
@@ -415,7 +413,7 @@ export function Article({
                 <header>
                   <Box marginBlockStart={20}>
                     <Link href="#" arrowDirection="start">
-                      Devops
+                      DevOps
                     </Link>
                   </Box>
                   <Box animate="fade-in" marginBlockStart={64} marginBlockEnd={32}>
--- packages/react/src/recipes/seo/Category/CategoryPage.tsx
+++ packages/react/src/recipes/seo/Category/CategoryPage.tsx
@@ -7,6 +7,7 @@ import {
   Card,
   Grid,
   Heading,
+  HeadingProps,
   MinimalFooter,
   Pagination,
   Stack,
@@ -28,7 +29,7 @@ import styles from './CategoryPage.module.css'
 
 const cardImagePlaceholders = [cardCover1, cardCover2, cardCover3, cardCover4, cardCover5, cardCover6, cardCover7]
 
-const NavListHeading = ({children}) => (
+const NavListHeading = (props: HeadingProps) => (
   <Box borderBlockStartWidth="thin" borderStyle="solid" borderColor="subtle" paddingBlockStart={12}>
     <Heading
       as="h2"
@@ -37,9 +38,8 @@ const NavListHeading = ({children}) => (
       className={styles.navListHeading}
       weight="medium"
       stretch="expanded"
-    >
-      {children}
-    </Heading>
+      {...props}
+    />
   </Box>
 )
 
@@ -51,7 +51,12 @@ const NavListItem = ({
 }: PropsWithChildren<
   {children: string; selected?: boolean; href: string; onClick: (e) => void} & BaseProps<HTMLAnchorElement>
 >) => (
-  <a className={clsx(styles.navListItem, selected && styles.navListItemSelected)} href={href} onClick={onClick}>
+  <a
+    className={clsx(styles.navListItem, selected && styles.navListItemSelected)}
+    href={href}
+    onClick={onClick}
+    aria-current={selected ? 'page' : undefined}
+  >
     <Text size="200">{children}</Text>
   </a>
 )
@@ -137,9 +142,9 @@ export function CategoryPage({accentColor, variant, gridOverlay = false, colorMo
           </Grid.Column>
           <Grid.Column span={{xsmall: 12, large: 3}}>
             <Stack direction="vertical" padding="none" gap={24}>
-              <NavListHeading>Topics</NavListHeading>
+              <NavListHeading id="topics-heading">Topics</NavListHeading>
               <Box marginBlockEnd={24}>
-                <nav>
+                <nav aria-labelledby="topics-heading">
                   <ul className={styles.navList}>
                     <li>
                       <NavListItem
