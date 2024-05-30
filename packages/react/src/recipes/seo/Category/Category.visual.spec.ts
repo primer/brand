d38e44101b3a2b8031c0c68031b2ec0c66cff252
Josh Farrant <joshfarrant@github.com>
2024-05-30 10:57:20 +0100

    fix linter issues (#598)

--- packages/react/src/recipes/seo/Category/Category.visual.spec.ts
+++ packages/react/src/recipes/seo/Category/Category.visual.spec.ts
@@ -6,4 +6,11 @@
 import {test, expect} from '@playwright/test'
 
 // eslint-disable-next-line i18n-text/no-en
-test.describe('Visual Comparison: Category', () => {})
+test.describe('Visual Comparison: Category', () => {
+  test('Category / Playground', async ({page}) => {
+    await page.goto('http://localhost:6006/iframe.html?args=&id=recipes-seo-category-page--playground&viewMode=story')
+
+    await page.waitForTimeout(500)
+    expect(await page.screenshot()).toMatchSnapshot()
+  })
+})
