3c370dcc99ade6bc6c064ab5ecb385dd70c7f6ae
Josh Farrant <joshfarrant@github.com>
2024-06-25 17:28:53 +0100

    fix image component story controls

--- packages/react/src/Image/Image.stories.tsx
+++ packages/react/src/Image/Image.stories.tsx
@@ -16,57 +16,41 @@ export default {
   argTypes: {
     src: {
       description: 'Sets the image source',
-      control: {
-        type: 'text',
-      },
+      control: 'text',
     },
     alt: {
       description: 'Sets the image alt text',
-      control: {
-        type: 'text',
-      },
+      control: 'text',
     },
     as: {
       description: 'Sets the underlying HTML element',
-      control: {
-        type: 'radio',
-        options: ['img', 'picture'],
-      },
+      control: 'radio',
+      options: ['img', 'picture'],
     },
     aspectRatio: {
       description: 'Sets the image aspect ratio. A custom ratio can be provided in the design tokens.',
-      control: {
-        type: 'radio',
-        options: ['1:1', '4:3', '16:10', '16:9', 'custom'],
-      },
+      control: 'radio',
+      options: ['1:1', '16:9', '16:10', '4:3', 'custom'],
     },
     width: {
       description: 'The width of the image',
-      control: {
-        type: 'number',
-      },
+      control: 'number',
     },
     height: {
       description: 'The height of the image',
-      control: {
-        type: 'number',
-      },
+      control: 'number',
     },
     loading: {
       description:
         'The loading attribute specifies whether a browser should load an image immediately or to defer loading of off-screen images until for example the user scrolls near them.',
-      control: {
-        type: 'radio',
-        options: ['eager', 'lazy'],
-      },
+      control: 'radio',
+      options: ['eager', 'lazy'],
     },
     decoding: {
       description:
         'Sets the image decoding strategy. Representing a hint given to the browser on how it should decode the image.',
-      control: {
-        type: 'radio',
-        options: ['auto', 'sync', 'async'],
-      },
+      control: 'radio',
+      options: ['auto', 'sync', 'async'],
     },
   },
 } as Meta<typeof Image>
