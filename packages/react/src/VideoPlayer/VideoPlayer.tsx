e23a774b51f47f0fe1985da30681a9efe4a6e7b3
Josh Farrant <joshfarrant@github.com>
2024-06-28 16:00:53 +0100

    implement new hooks and controls in VideoPlayer

--- packages/react/src/VideoPlayer/VideoPlayer.tsx
+++ packages/react/src/VideoPlayer/VideoPlayer.tsx
@@ -1,10 +1,11 @@
-import React, {useState, useRef, useEffect, useCallback} from 'react'
+import React, {useRef, useEffect, forwardRef, useCallback} from 'react'
 import clsx from 'clsx'
-import type {BaseProps} from '../component-helpers'
 import {Text} from '../Text'
+import {type AnimateProps} from '../animation'
 import {Controls} from './components'
 import {MarkGithubIcon} from '@primer/octicons-react'
 import {useProvidedRefOrCreate} from '../hooks/useRef'
+import {useVideoMetadata} from './hooks/useVideoMetadata'
 
 /**
  * Design tokens
@@ -14,185 +15,177 @@ import '@primer/brand-primitives/lib/design-tokens/css/tokens/functional/compone
 
 /** * Main Stylesheet (as a CSS Module) */
 import styles from './VideoPlayer.module.css'
+import {useIsElementFullScreen} from './hooks/useIsElementFullScreen'
 
 type VideoPlayerProps = {
   poster?: string
   title: string
   branding?: boolean
   children: React.ReactElement | React.ReactElement[]
-  ref?: React.RefObject<HTMLVideoElement>
-} & React.HTMLProps<HTMLVideoElement> &
-  BaseProps<HTMLVideoElement>
+  animate?: AnimateProps
+} & React.HTMLProps<HTMLVideoElement>
 
-const Root = ({
-  poster,
-  title,
-  branding = true,
-  children,
-  className,
-  onPlay,
-  onPause,
-  onLoadedMetadata,
-  onPlaying,
-  onTimeUpdate,
-  ref,
-  ...rest
-}: VideoPlayerProps) => {
-  const videoWrapperRef = useRef<HTMLDivElement>(null)
-  const videoRef = useProvidedRefOrCreate(ref)
-  const [playing, setPlaying] = useState(false)
-  const [playedTime, setPlayedTime] = useState(0)
-  const [totalTime, setTotalTime] = useState(0)
-  const [trackInformation, setTrackInformation] = useState<TextTrackCueList | undefined>(undefined)
+const Root = forwardRef<HTMLVideoElement, VideoPlayerProps>(
+  ({poster, title, branding = true, children, className, ...rest}, forwardedRef) => {
+    const videoRef = useProvidedRefOrCreate(forwardedRef)
+    const videoWrapperRef = useRef<HTMLDivElement>(null)
 
-  const handleOnPlay = useCallback(
-    e => {
-      setPlaying(true)
-      onPlay && onPlay(e)
-    },
-    [onPlay],
-  )
+    const {isPlaying, currentTime, duration, trackInformation, volume} = useVideoMetadata(videoRef)
+    const isFullScreen = useIsElementFullScreen(videoWrapperRef)
 
-  const handleOnPause = useCallback(
-    e => {
-      setPlaying(false)
-      onPause && onPause(e)
-    },
-    [onPause],
-  )
+    useEffect(() => {
+      // TODO Do we need this? what's it doing?
+      if (videoRef.current) {
+        for (let i = 0; i < videoRef.current.textTracks.length; i++) {
+          videoRef.current.textTracks[0].mode = 'hidden'
+        }
+      }
+    }, [videoRef])
 
-  const handleOnPlaying = useCallback(
-    e => {
-      videoRef.current?.textTracks[0].cues && setTrackInformation(videoRef.current.textTracks[0].cues)
-      onPlaying && onPlaying(e)
-    },
-    [onPlaying, videoRef],
-  )
+    useEffect(() => {
+      // TODO - A good use case for container queries?
+      const handleResize = () => {
+        const breakpoint = videoWrapperRef.current?.getBoundingClientRect().width
+        if (breakpoint && breakpoint < 650) {
+          videoWrapperRef.current.classList.add(styles['VideoPlayer__container--small'])
+        } else {
+          videoWrapperRef.current?.classList.remove(styles['VideoPlayer__container--small'])
+        }
+      }
 
-  const handleOnLoadedMetadata = useCallback(
-    e => {
-      setTotalTime(videoRef.current?.duration || 0)
-      onLoadedMetadata && onLoadedMetadata(e)
-    },
-    [onLoadedMetadata, videoRef],
-  )
+      const resizeObserver = new ResizeObserver(_ => {
+        if (videoWrapperRef.current) {
+          handleResize()
+        }
+      })
 
-  const handleOnTimeUpdate = useCallback(
-    e => {
-      setPlayedTime(videoRef.current?.currentTime || 0)
-      onTimeUpdate && onTimeUpdate(e)
-    },
-    [onTimeUpdate, videoRef],
-  )
+      handleResize()
 
-  const handleVideoPlayback = () => {
-    if (videoRef.current) {
-      if (videoRef.current.paused) {
+      const currentRef = videoWrapperRef.current
+
+      resizeObserver.observe(videoWrapperRef.current as Element)
+      return () => {
+        resizeObserver.unobserve(currentRef as Element)
+      }
+    }, [videoWrapperRef, isPlaying])
+
+    const play = useCallback(() => {
+      if (videoRef.current) {
         videoRef.current.play()
-      } else {
+      }
+    }, [videoRef])
+
+    const pause = useCallback(() => {
+      if (videoRef.current) {
         videoRef.current.pause()
       }
-    }
-  }
+    }, [videoRef])
 
-  useEffect(() => {
-    if (videoRef.current) {
-      for (let i = 0; i < videoRef.current.textTracks.length; i++) {
-        videoRef.current.textTracks[0].mode = 'hidden'
-      }
-    }
-  }, [videoRef])
-
-  useEffect(() => {
-    const handleResize = () => {
-      const breakpoint = videoWrapperRef.current?.getBoundingClientRect().width
-      if (breakpoint && breakpoint < 650) {
-        videoWrapperRef.current.classList.add(styles['VideoPlayer__container--small'])
+    const togglePlaying = useCallback(() => {
+      if (isPlaying) {
+        pause()
       } else {
-        videoWrapperRef.current?.classList.remove(styles['VideoPlayer__container--small'])
+        play()
       }
-    }
+    }, [isPlaying, play, pause])
 
-    const resizeObserver = new ResizeObserver(_ => {
-      if (videoWrapperRef.current) {
-        handleResize()
-      }
-    })
+    const seek = useCallback(
+      (time: number) => {
+        if (videoRef.current) {
+          videoRef.current.currentTime = time
+        }
+      },
+      [videoRef],
+    )
 
-    handleResize()
+    const setVolume = useCallback(
+      (value: number) => {
+        if (videoRef.current) {
+          videoRef.current.volume = value
+        }
+      },
+      [videoRef],
+    )
 
-    const currentRef = videoWrapperRef.current
+    const setIsFullScreen = useCallback(
+      (value: boolean) => {
+        if (!videoWrapperRef.current || isFullScreen === value) return
 
-    resizeObserver.observe(videoWrapperRef.current as Element)
-    return () => {
-      resizeObserver.unobserve(currentRef as Element)
-    }
-  }, [videoWrapperRef, playing])
+        if (value) {
+          videoWrapperRef.current.requestFullscreen()
+        } else {
+          document.exitFullscreen()
+        }
+      },
+      [videoWrapperRef, isFullScreen],
+    )
 
-  return (
-    <div
-      className={clsx(
-        styles.VideoPlayer__container,
-        styles.VideoPlayer__overlays,
-        !playing && styles.VideoPlayer__showOverlays,
-      )}
-      ref={videoWrapperRef}
-    >
-      <video
-        ref={videoRef}
-        title={title}
-        controls={false}
-        poster={poster}
-        className={clsx(styles.VideoPlayer, className)}
-        onPlay={handleOnPlay}
-        onPause={handleOnPause}
-        onPlaying={handleOnPlaying}
-        onLoadedMetadata={handleOnLoadedMetadata}
-        onTimeUpdate={handleOnTimeUpdate}
-        {...rest}
-      >
-        {children}
-        <track kind="captions" />
-      </video>
-      {title && (
-        <div className={styles.VideoPlayer__title}>
-          {branding && <MarkGithubIcon size={48} />}
-          <Text size="500" weight="medium" className={styles.VideoPlayer__controlTextColor}>
-            {title}
-          </Text>
-        </div>
-      )}
-      <button className={styles.VideoPlayer__playButton} onClick={handleVideoPlayback}>
-        {!playing && (
-          <span className={styles.VideoPlayer__playButtonInner}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              aria-hidden="true"
-            >
-              <path
-                d="M1.56489 23.8112C0.884817 24.2389 1.07491e-06 23.7501 1.0398e-06 22.9467L8.28679e-08 1.05473C4.73246e-08 0.241596 0.904067 -0.245404 1.58307 0.201969L18.5829 11.4026C19.2032 11.8113 19.1935 12.7244 18.5647 13.1198L1.56489 23.8112Z"
-                fill="currentColor"
-              />
-            </svg>
-          </span>
+    return (
+      <div
+        className={clsx(
+          styles.VideoPlayer__container,
+          styles.VideoPlayer__overlays,
+          !isPlaying && styles.VideoPlayer__showOverlays,
         )}
-        <span className="visually-hidden">Play</span>
-      </button>
-      <Controls
-        videoRef={videoRef}
-        videoWrapperRef={videoWrapperRef}
-        totalTime={totalTime}
-        currentTime={playedTime}
-        playing={playing}
-        trackInformation={trackInformation}
-      />
-    </div>
-  )
-}
+        ref={videoWrapperRef}
+      >
+        <video
+          ref={videoRef}
+          title={title}
+          controls={false}
+          poster={poster}
+          className={clsx(styles.VideoPlayer, className)}
+          {...rest}
+        >
+          {children}
+          <track kind="captions" />
+        </video>
+        {title && (
+          <div className={styles.VideoPlayer__title}>
+            {branding && <MarkGithubIcon size={48} />}
+            <Text size="500" weight="medium" className={styles.VideoPlayer__controlTextColor}>
+              {title}
+            </Text>
+          </div>
+        )}
+        <button className={styles.VideoPlayer__playButton} onClick={() => togglePlaying()}>
+          {isPlaying ? null : (
+            <span className={styles.VideoPlayer__playButtonInner}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                aria-hidden="true"
+              >
+                <path
+                  d="M1.56489 23.8112C0.884817 24.2389 1.07491e-06 23.7501 1.0398e-06 22.9467L8.28679e-08 1.05473C4.73246e-08 0.241596 0.904067 -0.245404 1.58307 0.201969L18.5829 11.4026C19.2032 11.8113 19.1935 12.7244 18.5647 13.1198L1.56489 23.8112Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </span>
+          )}
+          <span className="visually-hidden">Play</span>
+        </button>
+        <Controls
+          videoRef={videoRef}
+          duration={duration}
+          currentTime={currentTime}
+          isPlaying={isPlaying}
+          trackInformation={trackInformation}
+          play={play}
+          pause={pause}
+          seek={seek}
+          volume={volume}
+          setVolume={setVolume}
+          isFullScreen={isFullScreen}
+          setIsFullScreen={setIsFullScreen}
+        />
+      </div>
+    )
+  },
+)
 
 const VideoPlayerSource = (props: React.HTMLProps<HTMLSourceElement>) => {
   return <source {...props} />
@@ -206,3 +199,15 @@ export const VideoPlayer = Object.assign(Root, {
   Source: VideoPlayerSource,
   Track: VideoPlayerTrack,
 })
+
+// TODO
+// Escape key needs to toggle fullScreen state
+// F should go fullscreen
+// Space should play/pause
+// K should play/pause
+// M should mute
+// Arrow keys should seek
+// J and L should seek
+// C should toggle captions
+// Contrast on the captions feels very low
+// Pointer area on blue play button only covers icon, not the blue bit
