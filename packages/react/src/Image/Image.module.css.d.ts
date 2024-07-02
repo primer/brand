552132eb4c7aaf08fa5a7a3a05efdc191a36d1c6
Josh Farrant <joshfarrant@github.com>
2024-07-02 12:22:29 +0100

    add aspectRatio prop back in

--- packages/react/src/Image/Image.module.css.d.ts
+++ packages/react/src/Image/Image.module.css.d.ts
@@ -1,5 +1,10 @@
 declare const styles: {
   readonly "Image": string;
+  readonly "Image--aspectRatio-custom": string;
+  readonly "Image--aspectRatio-1-1": string;
+  readonly "Image--aspectRatio-16-9": string;
+  readonly "Image--aspectRatio-16-10": string;
+  readonly "Image--aspectRatio-4-3": string;
   readonly "Image--borderRadius-small": string;
   readonly "Image--borderRadius-medium": string;
   readonly "Image--borderRadius-large": string;
--- packages/react/src/Image/Image.tsx
+++ packages/react/src/Image/Image.tsx
@@ -5,19 +5,41 @@ import {AnimateProps, useAnimation} from '../animation'
 
 export const ImageBorderRadiusOptions = ['small', 'medium', 'large', 'xlarge', 'full'] as const
 export type ImageBorderRadiusOptions = (typeof ImageBorderRadiusOptions)[number]
+export type ImageAspectRatio = '1:1' | '16:9' | '16:10' | '4:3' | 'custom'
 
 export type ImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
   alt: string
   animate?: AnimateProps
   borderRadius?: ImageBorderRadiusOptions
+  aspectRatio?: ImageAspectRatio
 }
 
-export const Image = ({alt, animate, borderRadius, className, style, ...props}: ImageProps) => {
+const getAspectRatioClassName = (aspectRatio?: ImageAspectRatio): string | null => {
+  if (!aspectRatio) {
+    return null
+  }
+
+  if (aspectRatio === 'custom') {
+    return styles['Image--aspectRatio-custom']
+  }
+
+  const className = `Image--aspectRatio-${aspectRatio.replace(':', '-')}`
+
+  return styles[className]
+}
+
+export const Image = ({alt, animate, borderRadius, className, style, aspectRatio, ...props}: ImageProps) => {
   const {classes: animationClasses, styles: animationInlineStyles} = useAnimation(animate)
 
   return (
     <img
-      className={clsx(animationClasses, styles.Image, styles[`Image--borderRadius-${borderRadius}`], className)}
+      className={clsx(
+        animationClasses,
+        styles.Image,
+        styles[`Image--borderRadius-${borderRadius}`],
+        getAspectRatioClassName(aspectRatio),
+        className,
+      )}
       style={{...animationInlineStyles, ...style}}
       alt={alt}
       {...props}
