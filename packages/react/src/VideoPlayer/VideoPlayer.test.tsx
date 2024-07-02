d459c531374455a1274680515df02eca78836a16
Josh Farrant <joshfarrant@github.com>
2024-07-02 10:33:24 +0100

    fix lint error

--- packages/react/src/VideoPlayer/VideoPlayer.test.tsx
+++ packages/react/src/VideoPlayer/VideoPlayer.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {render, cleanup, waitFor, fireEvent, act} from '@testing-library/react'
+import {render, cleanup, waitFor, fireEvent} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
