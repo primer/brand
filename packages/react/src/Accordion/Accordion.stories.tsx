0e021c0a87bd90d9881faee60d6d63dd5fd36dd4
Charlotte Dann <pouretrebelle@github.com>
2024-07-23 13:18:49 +0100

    Add Playground story to Accordion

--- packages/react/src/Accordion/Accordion.stories.tsx
+++ packages/react/src/Accordion/Accordion.stories.tsx
@@ -16,3 +16,63 @@ export const Default = () => (
     </Accordion.Content>
   </Accordion>
 )
+
+export const Playground = args => (
+  <Accordion variant={args.variant}>
+    <Accordion.Heading reversedToggles={args.reversedToggles}>{args.heading}</Accordion.Heading>
+    <Accordion.Content>{args.content}</Accordion.Content>
+  </Accordion>
+)
+Playground.argTypes = {
+  variant: {
+    name: 'variant',
+    control: {
+      type: 'radio',
+    },
+    options: ['default', 'emphasis'],
+  },
+  heading: {
+    name: 'text',
+    control: {
+      type: 'text',
+    },
+    table: {
+      category: 'Accordion.Heading',
+    },
+  },
+  reversedToggles: {
+    name: 'reversedToggles',
+    control: {
+      type: 'boolean',
+    },
+    table: {
+      category: 'Accordion.Heading',
+    },
+  },
+  headingAs: {
+    name: 'as',
+    control: {
+      type: 'inline-radio',
+    },
+    options: ['h2', 'h3', 'h4', 'h5', 'h6'],
+    table: {
+      category: 'Accordion.Heading',
+    },
+  },
+  content: {
+    name: 'text',
+    control: {
+      type: 'text',
+    },
+    table: {
+      category: 'Accordion.Content',
+    },
+  },
+}
+Playground.args = {
+  variant: 'default',
+  heading: 'Heading',
+  reversedToggles: false,
+  headingAs: 'h4',
+  content: 'Content',
+}
