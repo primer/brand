87a689abdb47b50f864235c2cafb859d41002688
Daniel Guillan <danielguillan@github.com>
2024-07-30 19:03:28 +0200

    Add playground story

--- packages/react/src/Section/Section.stories.tsx
+++ packages/react/src/Section/Section.stories.tsx
@@ -1,17 +1,56 @@
 import React from 'react'
-import {Meta} from '@storybook/react'
+import {StoryFn, Meta} from '@storybook/react'
 import {Section} from '.'
 import {SectionIntro} from '..'
 
 export default {
   title: 'Components/Section',
   component: Section,
+  args: {
+    paddingBlockStart: 'normal',
+    paddingBlockEnd: 'normal',
+    backgroundColor: undefined,
+    backgroundImageSrc: undefined,
+    backgroundImagePosition: undefined,
+    backgroundImageSize: undefined,
+    fullWidth: false,
+    rounded: false,
+    as: 'section',
+  },
+  argTypes: {
+    paddingBlockStart: {
+      control: 'inline-radio',
+      options: ['none', 'condensed', 'normal', 'spacious'],
+    },
+    paddingBlockEnd: {
+      control: 'inline-radio',
+      options: ['none', 'condensed', 'normal', 'spacious'],
+    },
+    backgroundColor: {
+      control: 'text',
+    },
+    backgroundImageSrc: {
+      control: 'text',
+    },
+    backgroundImageSize: {
+      control: 'text',
+    },
+    backgroundImagePosition: {
+      control: 'text',
+    },
+    fullWidth: {
+      control: 'boolean',
+    },
+    rounded: {
+      control: 'boolean',
+    },
+  },
 } as Meta<typeof Section>
 
-export const Default = () => (
-  <Section>
+const Template: StoryFn<typeof Section> = args => (
+  <Section {...args}>
     <SectionIntro>
-      <SectionIntro.Heading>Section heading 1</SectionIntro.Heading>
+      <SectionIntro.Heading>Section heading</SectionIntro.Heading>
       <SectionIntro.Description>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed turpis
         felis nam pulvinar risus elementum.
@@ -19,3 +58,7 @@ export const Default = () => (
     </SectionIntro>
   </Section>
 )
+
+export const Default = Template.bind({})
+
+export const Playground = Template.bind({})
