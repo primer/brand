2758f7800ea31d29ec5c8fd2a2ebb9be7d1042f3
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2024-08-12 13:49:37 +0100

    fix linter

--- packages/react/src/animation/TextRevealAnimation/TextRevealAnimation.tsx
+++ packages/react/src/animation/TextRevealAnimation/TextRevealAnimation.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from 'react'
+import React, {useEffect} from 'react'
 import clsx from 'clsx'
 import {useTextRevealAnimationLines} from './useTextRevealAnimationLines'
 
