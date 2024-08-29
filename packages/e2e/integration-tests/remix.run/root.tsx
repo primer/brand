ef7ade6661532af50d3a8835408e8ee82327a4b7
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2024-08-29 18:39:20 +0100

    update root.tsx

--- packages/e2e/integration-tests/remix.run/root.tsx
+++ packages/e2e/integration-tests/remix.run/root.tsx
@@ -3,18 +3,12 @@
  * Modified to add Primer Brand stylesheet
  */
 import {Links, Meta, Outlet, Scripts, ScrollRestoration} from '@remix-run/react'
-import primerBrandStyles from '@primer/react-brand/lib/css/main.css'
-import stylesheet from '~/tailwind.css'
-
-export const links: LinksFunction = () => [
-  {rel: 'stylesheet', href: stylesheet},
-  {rel: 'stylesheet', href: primerBrandStyles},
-  ...(cssBundleHref ? [{rel: 'stylesheet', href: cssBundleHref}] : []),
-]
+import '@primer/react-brand/lib/css/main.css'
+import './tailwind.css'
 
 export function Layout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="en" dir="ltr">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
