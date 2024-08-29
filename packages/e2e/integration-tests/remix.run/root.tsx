291771215a7d586d85d49adfe9429958e64088fd
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2024-08-29 17:29:07 +0100

    switch remix template to default

--- packages/e2e/integration-tests/remix.run/root.tsx
+++ packages/e2e/integration-tests/remix.run/root.tsx
@@ -1,9 +1,8 @@
-import {cssBundleHref} from '@remix-run/css-bundle'
-import type {LinksFunction, LoaderArgs} from '@remix-run/node'
-import {json} from '@remix-run/node'
-import {Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration} from '@remix-run/react'
-
-import {getUser} from '~/session.server'
+/**
+ * Created using npx create-remix
+ * Modified to add Primer Brand stylesheet
+ */
+import {Links, Meta, Outlet, Scripts, ScrollRestoration} from '@remix-run/react'
 import primerBrandStyles from '@primer/react-brand/lib/css/main.css'
 import stylesheet from '~/tailwind.css'
 
@@ -13,25 +12,24 @@ export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{rel: 'stylesheet', href: cssBundleHref}] : []),
 ]
 
-export const loader = async ({request}: LoaderArgs) => {
-  return json({user: await getUser(request)})
-}
-
-export default function App() {
+export function Layout({children}: {children: React.ReactNode}) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
-        <Outlet />
+      <body>
+        {children}
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   )
+}
+
+export default function App() {
+  return <Outlet />
 }
