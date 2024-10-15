7535420c390a4ca60eaa173dc2d00a23f8150094
Mike Perrotti <mperrotti@github.com>
2024-10-15 18:14:52 -0400

    updates header nav content and layout

---
+++ apps/docs/src/components/side-panel/side-panel.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import {useState, useRef} from 'react'
+import {IconButton} from '@primer/react'
+import {Dialog} from '@primer/react/experimental'
+import {ThreeBarsIcon, XIcon} from '@primer/octicons-react'
+import styles from './side-panel.module.css'
+import {Sidebar} from '@primer/gatsby-theme-doctocat'
+
+export function SidePanel() {
+  const [isOpen, setIsOpen] = useState(false)
+  const closeButtonRef = useRef<HTMLButtonElement>(null)
+  const openButtonRef = useRef<HTMLButtonElement>(null)
+
+  const openDialog = () => {
+    setIsOpen(true)
+  }
+
+  const closeDialog = () => {
+    setIsOpen(false)
+  }
+
+  const renderHeader = ({dialogLabelId}: {dialogLabelId: string}) => (
+    <div
+      className={styles.Header}
+      id={dialogLabelId}
+      aria-label="Navigation"
+      role="heading"
+      aria-level={1}
+    >
+      <IconButton
+        ref={closeButtonRef}
+        icon={XIcon}
+        variant="default"
+        aria-label="Close Navigation"
+        onClick={closeDialog}
+      />
+    </div>
+  )
+
+  return (
+    <div className={styles.SidePanel}>
+      <IconButton
+        ref={openButtonRef}
+        icon={ThreeBarsIcon}
+        variant="default"
+        aria-label="Navigation"
+        onClick={openDialog}
+      />
+      {isOpen && (
+        <Dialog
+          onClose={closeDialog}
+          width="small"
+          // TODO: get `@primer/react` updated to a version that supports the `position` prop on experimental `Dialog` component
+          position="right"
+          renderHeader={renderHeader}
+          returnFocusRef={openButtonRef}
+        >
+          <Sidebar />
+        </Dialog>
+      )}
+    </div>
+  )
+}
