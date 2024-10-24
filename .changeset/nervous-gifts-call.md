b3e34be569a1cb639325ce10220be07c3a35ea84
Rez <13340707+rezrah@users.noreply.github.com>
2024-10-24 18:04:58 +0100

    Fix riverstoryscroll on narrow viewports (#797)
    * fix riverstoryscroll on narrow viewports
    
    * fix dodgy snapshot
    
    * Update .changeset/nervous-gifts-call.md
    
    Co-authored-by: Josh Farrant <joshfarrant@github.com>
    
    * nudge pagination left
    
    ---------
    
    Co-authored-by: Josh Farrant <joshfarrant@github.com>

---
+++ .changeset/nervous-gifts-call.md
@@ -0,0 +1,5 @@
+---
+'@primer/react-brand': patch
+---
+
+Fixed visual bug in `RiverStoryScroll` layout on narrow viewports. Also adjusted positioning of the pagination to be closer to the scrolled items.
--- packages/react/src/river/RiverStoryScroll/RiverStoryScroll.module.css.d.ts
+++ packages/react/src/river/RiverStoryScroll/RiverStoryScroll.module.css.d.ts
@@ -5,12 +5,12 @@ declare const styles: {
   readonly "RiverStoryScroll__image": string;
   readonly "RiverStoryScroll--enabled": string;
   readonly "RiverStoryScroll__internal-river": string;
+  readonly "RiverStoryScroll__visual-container-inner": string;
   readonly "RiverStoryScroll__visual-container": string;
   readonly "RiverStoryScroll__tracker": string;
   readonly "in-viewport": string;
   readonly "outside-viewport": string;
   readonly "RiverStoryScroll__content-container--below": string;
-  readonly "RiverStoryScroll__visual-container-inner": string;
   readonly "RiverStoryScroll__visual--below": string;
   readonly "story-scroll-text": string;
   readonly "tracker-outside-viewport": string;
