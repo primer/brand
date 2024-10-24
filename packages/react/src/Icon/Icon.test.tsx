20f8c5846b5c8b265611093eacc2a75fad235869
Josh Farrant <joshfarrant@github.com>
2024-10-24 11:35:00 +0100

    add tests

--- packages/react/src/Icon/Icon.test.tsx
+++ packages/react/src/Icon/Icon.test.tsx
@@ -3,7 +3,7 @@ import '@testing-library/jest-dom'
 import {axe, toHaveNoViolations} from 'jest-axe'
 import {GitMergeIcon} from '@primer/octicons-react'
 
-import {Icon} from './Icon'
+import {Icon, IconSize, iconSizes, numericIconSizes} from './Icon'
 
 expect.extend(toHaveNoViolations)
 
@@ -32,19 +32,46 @@ describe('Icon', () => {
     expect(getByLabelText('Git merge icon')).toHaveClass('Icon--color-blue')
   })
 
-  it('sets the size of the icon', () => {
-    const {getByLabelText} = render(<Icon icon={GitMergeIcon} aria-label="Git merge icon" size="large" />)
+  it('sets the size of the icon when `size` is a number', () => {
+    const {getByLabelText} = render(<Icon icon={GitMergeIcon} aria-label="Git merge icon" size={16} />)
     const icon = getByLabelText('Git merge icon')
 
-    expect(icon.getAttribute('width')).toBe('44')
-    expect(icon.getAttribute('height')).toBe('44')
+    expect(icon.getAttribute('width')).toBe('16')
+    expect(icon.getAttribute('height')).toBe('16')
   })
 
-  it('overrides the size of the icon to `small` when `hasBackground` is `true`', () => {
-    const {getByLabelText} = render(<Icon icon={GitMergeIcon} aria-label="Git merge icon" size="large" hasBackground />)
+  it.each`
+    namedSize   | size
+    ${'small'}  | ${20}
+    ${'medium'} | ${32}
+    ${'large'}  | ${44}
+  `('sets the size of the icon to $size when `size=$namedSize`', ({namedSize, size}) => {
+    const {getByLabelText} = render(<Icon icon={GitMergeIcon} aria-label="Git merge icon" size={namedSize} />)
     const icon = getByLabelText('Git merge icon')
 
-    expect(icon.getAttribute('width')).toBe('20')
-    expect(icon.getAttribute('height')).toBe('20')
+    expect(icon.getAttribute('width')).toBe(size.toString())
+    expect(icon.getAttribute('height')).toBe(size.toString())
+  })
+
+  it.each`
+    size        | padding
+    ${12}       | ${8}
+    ${16}       | ${8}
+    ${20}       | ${8}
+    ${24}       | ${8}
+    ${28}       | ${12}
+    ${32}       | ${12}
+    ${36}       | ${12}
+    ${40}       | ${12}
+    ${44}       | ${12}
+    ${48}       | ${16}
+    ${52}       | ${20}
+    ${'small'}  | ${8}
+    ${'medium'} | ${12}
+    ${'large'}  | ${12}
+  `('sets the padding to $padding when `size=$size`', ({size, padding}) => {
+    const {getByLabelText} = render(<Icon icon={GitMergeIcon} aria-label="Git merge icon" size={size} />)
+
+    expect(getByLabelText('Git merge icon')).toHaveClass(`Icon--padding-${padding}`)
   })
 })
