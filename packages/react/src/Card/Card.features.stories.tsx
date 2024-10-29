80183da988693e2c23edb32515fee490a1adf53c
Josh Farrant <joshfarrant@github.com>
2024-10-29 10:41:24 +0000

    fix ts error

--- packages/react/src/Card/Card.features.stories.tsx
+++ packages/react/src/Card/Card.features.stories.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import {StoryFn, Meta} from '@storybook/react'
 import {Card, CardIconColors} from '.'
-import {Stack, LabelColors, Grid, ThemeProvider, Box, type IconProps} from '..'
+import {Stack, LabelColors, Grid, ThemeProvider, Box} from '..'
 import placeholderImage from '../fixtures/images/placeholder-600x400.png'
 import {CopilotIcon, ZapIcon, RocketIcon, GitBranchIcon, HeartIcon} from '@primer/octicons-react'
+import {IconProps} from '../Icon'
 
 export default {
   title: 'Components/Card/features',
