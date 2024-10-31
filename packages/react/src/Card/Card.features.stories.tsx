eb51421e0bf218a14e67a17e40874b21391c3aba
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2024-10-31 17:10:11 +0000

    restore storybook example for native svg elements

--- packages/react/src/Card/Card.features.stories.tsx
+++ packages/react/src/Card/Card.features.stories.tsx
@@ -121,21 +121,20 @@ export const IconColors: StoryFn<typeof Card> = () => {
 export const WithIconSVG = () => (
   <Card href="https://github.com">
     <Card.Icon
-      icon={props => (
+      icon={
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
           width="20"
           height="20"
           aria-label="Magnifying glass icon"
-          {...props}
         >
           <path
             d="M10.68 11.74a6 6 0 0 1-7.922-8.982 6 6 0 0 1 8.982 7.922l3.04 3.04a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215ZM11.5 7a4.499 4.499 0 1 0-8.997 0A4.499 4.499 0 0 0 11.5 7Z"
             fill="currentColor"
           ></path>
         </svg>
-      )}
+      }
       hasBackground
       color="purple"
     />
