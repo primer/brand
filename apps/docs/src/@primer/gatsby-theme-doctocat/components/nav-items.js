ba21aa54df7baaf1faf16e6c243caffb1dd61a43
Mike Perrotti <mperrotti@github.com>
2024-12-02 19:37:02 -0500

    revises to match latest primer-docs chrome

--- apps/docs/src/@primer/gatsby-theme-doctocat/components/nav-items.js
+++ apps/docs/src/@primer/gatsby-theme-doctocat/components/nav-items.js
@@ -3,6 +3,7 @@ import {useLocation} from '@reach/router'
 import {Link as GatsbyLink, withPrefix} from 'gatsby'
 import React from 'react'
 import VisuallyHidden from '@primer/gatsby-theme-doctocat/src/components/visually-hidden'
+import styles from './nav-items.module.css'
 
 function NavItem({href, children}) {
   const location = useLocation()
@@ -24,32 +25,34 @@ function NavItems({items}) {
       <VisuallyHidden>
         <h3>Site navigation</h3>
       </VisuallyHidden>
-      <NavList aria-label="Site">
-        {items.map((item) => (
-          <React.Fragment key={item.title}>
-            {item.children ? (
-              <NavList.Group title={item.title}>
-                {item.children.map((child) => (
-                  <NavItem key={child.title} href={child.url}>
-                    {child.title}
-                    {child.children ? (
-                      <NavList.SubNav>
-                        {child.children.map((subChild) => (
-                          <NavItem key={subChild.title} href={subChild.url}>
-                            {subChild.title}
-                          </NavItem>
-                        ))}
-                      </NavList.SubNav>
-                    ) : null}
-                  </NavItem>
-                ))}
-              </NavList.Group>
-            ) : (
-              <NavItem href={item.url}>{item.title}</NavItem>
-            )}
-          </React.Fragment>
-        ))}
-      </NavList>
+      <div className={styles.NavList__Container}>
+        <NavList aria-label="Site">
+          {items.map((item) => (
+            <React.Fragment key={item.title}>
+              {item.children ? (
+                <NavList.Group title={item.title}>
+                  {item.children.map((child) => (
+                    <NavItem key={child.title} href={child.url}>
+                      {child.title}
+                      {child.children ? (
+                        <NavList.SubNav>
+                          {child.children.map((subChild) => (
+                            <NavItem key={subChild.title} href={subChild.url}>
+                              {subChild.title}
+                            </NavItem>
+                          ))}
+                        </NavList.SubNav>
+                      ) : null}
+                    </NavItem>
+                  ))}
+                </NavList.Group>
+              ) : (
+                <NavItem href={item.url}>{item.title}</NavItem>
+              )}
+            </React.Fragment>
+          ))}
+        </NavList>
+      </div>
     </>
   )
 }
