b425448ff24a2acc1932b310385e66a61e0e5ab2
Josh Farrant <joshfarrant@github.com>
2025-01-10 10:17:20 +0000

    refactored Link stories

--- packages/react/src/Link/Link.stories.tsx
+++ packages/react/src/Link/Link.stories.tsx
@@ -1,17 +1,37 @@
-import React from 'react'
-import {Meta, StoryFn} from '@storybook/react'
-import {Link} from '.'
+import type {Meta, StoryObj} from '@storybook/react'
 
-export default {
+import {Link, LinkArrowDirections, LinkSizes, LinkVariants, type LinkProps} from '.'
+
+const meta: Meta<LinkProps> = {
   title: 'Components/Link',
   component: Link,
-} as Meta<typeof Link>
-
-const Template: StoryFn<typeof Link> = args => <Link {...args} />
-
-export const Default = Template.bind({})
-Default.args = {
-  // size: 'medium',
-  href: '#',
-  children: 'Primary action',
+  args: {
+    children: 'Learn more',
+    size: 'medium',
+    arrowDirection: 'end',
+    variant: 'default',
+  },
+  argTypes: {
+    children: {
+      control: 'text',
+    },
+    arrowDirection: {
+      options: LinkArrowDirections,
+      control: {type: 'inline-radio'},
+    },
+    size: {
+      options: LinkSizes,
+      control: {type: 'inline-radio'},
+    },
+    variant: {
+      options: LinkVariants,
+      control: {type: 'inline-radio'},
+    },
+  },
 }
+
+export default meta
+
+type Story = StoryObj<LinkProps>
+
+export const Default: Story = {}
