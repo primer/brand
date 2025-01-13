965a7865f8f2aa4fd5c6a150ae58bf72c0a457c9
Rez <13340707+rezrah@users.noreply.github.com>
2025-01-13 15:25:00 +0000

    Adjust spacing in river breakout (#883)
    * riverbreakout spacing updates
    
    * add vertical axis token to replace fixed, non-responsive version
    
    * update snapshots

--- packages/react/src/river/RiverBreakout/RiverBreakout.stories.tsx
+++ packages/react/src/river/RiverBreakout/RiverBreakout.stories.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import {Meta, StoryFn} from '@storybook/react'
 import {RiverBreakout} from '.'
-import {Link, Text, Timeline} from '../../'
-import {Container} from '../../component-helpers'
+import {Grid, Link, Text, Timeline} from '../../'
 import placeholderImage from '../../fixtures/images/placeholder-600x400.png'
 
 export default {
   title: 'Components/RiverBreakout',
   component: RiverBreakout,
+  decorators: [
+    Story => (
+      <Grid>
+        <Grid.Column>
+          <Story />
+        </Grid.Column>
+      </Grid>
+    ),
+  ],
 } as Meta<typeof RiverBreakout>
 
 const TrailingTimeline = () => (
@@ -26,55 +34,49 @@ const PlaceholderImage = () => (
 )
 
 export const Default: StoryFn<typeof RiverBreakout> = () => (
-  <Container>
-    <RiverBreakout>
-      <RiverBreakout.A11yHeading>Accelerate workflows</RiverBreakout.A11yHeading>
-      <RiverBreakout.Visual>
-        <PlaceholderImage />
-      </RiverBreakout.Visual>
-      <RiverBreakout.Content trailingComponent={TrailingTimeline}>
-        <Text>
-          Accelerate your workflows and scale your business fast with access to millions of open source projects on
-          GitHub, the largest source code host.
-        </Text>
-        <Link href="#">Call to action</Link>
-      </RiverBreakout.Content>
-    </RiverBreakout>
-  </Container>
+  <RiverBreakout>
+    <RiverBreakout.A11yHeading>Accelerate workflows</RiverBreakout.A11yHeading>
+    <RiverBreakout.Visual>
+      <PlaceholderImage />
+    </RiverBreakout.Visual>
+    <RiverBreakout.Content trailingComponent={TrailingTimeline}>
+      <Text>
+        Accelerate your workflows and scale your business fast with access to millions of open source projects on
+        GitHub, the largest source code host.
+      </Text>
+      <Link href="#">Call to action</Link>
+    </RiverBreakout.Content>
+  </RiverBreakout>
 )
 
 export const HighlightedPortion: StoryFn<typeof RiverBreakout> = () => (
-  <Container>
-    <RiverBreakout>
-      <RiverBreakout.A11yHeading>River breakout highlight</RiverBreakout.A11yHeading>
-      <RiverBreakout.Visual>
-        <PlaceholderImage />
-      </RiverBreakout.Visual>
-      <RiverBreakout.Content trailingComponent={TrailingTimeline}>
-        <Text>
-          <b>This first sentence is a river breakout headline.</b> And this is where the body copy starts. Remember to
-          keep these nice and succinct.
-        </Text>
-        <Link href="#">Call to action</Link>
-      </RiverBreakout.Content>
-    </RiverBreakout>
-  </Container>
+  <RiverBreakout>
+    <RiverBreakout.A11yHeading>River breakout highlight</RiverBreakout.A11yHeading>
+    <RiverBreakout.Visual>
+      <PlaceholderImage />
+    </RiverBreakout.Visual>
+    <RiverBreakout.Content trailingComponent={TrailingTimeline}>
+      <Text>
+        <b>This first sentence is a river breakout headline.</b> And this is where the body copy starts. Remember to
+        keep these nice and succinct.
+      </Text>
+      <Link href="#">Call to action</Link>
+    </RiverBreakout.Content>
+  </RiverBreakout>
 )
 
 export const WithoutTrailingComponent: StoryFn<typeof RiverBreakout> = () => (
-  <Container>
-    <RiverBreakout>
-      <RiverBreakout.A11yHeading>Accelerate workflows</RiverBreakout.A11yHeading>
-      <RiverBreakout.Visual>
-        <PlaceholderImage />
-      </RiverBreakout.Visual>
-      <RiverBreakout.Content>
-        <Text>
-          Accelerate your workflows and scale your business fast with access to millions of open source projects on
-          GitHub, the largest source code host.
-        </Text>
-        <Link href="#">Call to action</Link>
-      </RiverBreakout.Content>
-    </RiverBreakout>
-  </Container>
+  <RiverBreakout>
+    <RiverBreakout.A11yHeading>Accelerate workflows</RiverBreakout.A11yHeading>
+    <RiverBreakout.Visual>
+      <PlaceholderImage />
+    </RiverBreakout.Visual>
+    <RiverBreakout.Content>
+      <Text>
+        Accelerate your workflows and scale your business fast with access to millions of open source projects on
+        GitHub, the largest source code host.
+      </Text>
+      <Link href="#">Call to action</Link>
+    </RiverBreakout.Content>
+  </RiverBreakout>
 )
