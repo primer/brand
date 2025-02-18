146bc779de14ca30374fee5d00e9b68788647339
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2025-02-18 19:08:19 +0000

    add preview deployment

--- apps/next-docs/next.config.ts
+++ apps/next-docs/next.config.ts
@@ -4,7 +4,12 @@ export default {
   ...withDoctocat({
     transpilePackages: ['@primer/doctocat-nextjs'],
     output: 'export',
-    basePath: process.env.GITHUB_ACTIONS === 'true' && process.env.IS_PROD ? '/brand' : '',
+    basePath:
+      process.env.GITHUB_ACTIONS === 'true' && process.env.IS_PROD
+        ? '/brand/next'
+        : process.env.IS_PREVIEW
+          ? '/next'
+          : '',
     typescript: {
       tsconfigPath: './tsconfig.json',
     },
