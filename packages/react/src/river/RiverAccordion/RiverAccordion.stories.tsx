21025603c4bd8d7a1fdde98c77eab46cd801c97f
Josh Farrant <joshfarrant@github.com>
2025-03-10 08:25:28 +0000

    wip

--- packages/react/src/river/RiverAccordion/RiverAccordion.stories.tsx
+++ packages/react/src/river/RiverAccordion/RiverAccordion.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import type {Meta, StoryObj} from '@storybook/react'
 
 // import placeholderImage from '../../fixtures/images/placeholder.png'
-import {Link, RiverAccordion, type RiverAccordionProps, Text} from '../../'
+import {Accordion, Heading, Link, River, RiverAccordion, type RiverAccordionProps, Text} from '../../'
 
 export type MetaProps = RiverAccordionProps
 
@@ -28,53 +28,56 @@ export const Playground: Story = {
   render: args => {
     return (
       <RiverAccordion {...args}>
-        <RiverAccordion.Item>
-          <RiverAccordion.Heading>Heading 1</RiverAccordion.Heading>
-          <RiverAccordion.Visual
-            src="https://picsum.photos/seed/hello/1000/800"
-            alt="placeholder, blank area with a gray background color"
-          />
-
-          <RiverAccordion.Content>
+        <River>
+          <River.Visual>
+            <img
+              src="https://picsum.photos/seed/hello/1000/800"
+              alt="placeholder, blank area with a gray background color"
+            />
+          </River.Visual>
+          <River.Content>
+            <Heading>Heading 1</Heading>
             <Text>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed
               turpis felis nam pulvinar risus elementum.
             </Text>
             <Link href="#">Call to action</Link>
-          </RiverAccordion.Content>
-        </RiverAccordion.Item>
+          </River.Content>
+        </River>
 
-        <RiverAccordion.Item>
-          <RiverAccordion.Heading>Heading 2</RiverAccordion.Heading>
-          <RiverAccordion.Visual
-            src="https://picsum.photos/seed/beans/1000/800"
-            alt="placeholder, blank area with a gray background color"
-          />
-
-          <RiverAccordion.Content>
+        <River>
+          <River.Visual>
+            <img
+              src="https://picsum.photos/seed/beans/1000/800"
+              alt="placeholder, blank area with a gray background color"
+            />
+          </River.Visual>
+          <River.Content>
+            <Heading>Heading 2</Heading>
             <Text>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed
               turpis felis nam pulvinar risus elementum.
             </Text>
             <Link href="#">Call to action</Link>
-          </RiverAccordion.Content>
-        </RiverAccordion.Item>
+          </River.Content>
+        </River>
 
-        <RiverAccordion.Item>
-          <RiverAccordion.Heading>Heading 3</RiverAccordion.Heading>
-          <RiverAccordion.Visual
-            src="https://picsum.photos/seed/badger/1000/800"
-            alt="placeholder, blank area with a gray background color"
-          />
-
-          <RiverAccordion.Content>
+        <River>
+          <River.Visual>
+            <img
+              src="https://picsum.photos/seed/badger/1000/800"
+              alt="placeholder, blank area with a gray background color"
+            />
+          </River.Visual>
+          <River.Content>
+            <Heading>Heading 3</Heading>
             <Text>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sapien sit ullamcorper id. Aliquam luctus sed
               turpis felis nam pulvinar risus elementum.
             </Text>
             <Link href="#">Call to action</Link>
-          </RiverAccordion.Content>
-        </RiverAccordion.Item>
+          </River.Content>
+        </River>
       </RiverAccordion>
     )
   },
