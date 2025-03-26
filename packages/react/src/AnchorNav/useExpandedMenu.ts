997a318c63ede18c5439559819b082bb1aba6e09
Rez <13340707+rezrah@users.noreply.github>
2025-03-26 14:43:36 +0000

    fix build issues and import paths

--- packages/react/src/AnchorNav/useExpandedMenu.ts
+++ packages/react/src/AnchorNav/useExpandedMenu.ts
@@ -1,12 +1,13 @@
 import React, {useEffect} from 'react'
+import styles from './AnchorNav.module.css'
 
 export const useExpandedMenu = (open: boolean, containerRef: React.RefObject<HTMLElement>, isNarrow: boolean) => {
   // Prevent background scroll when menu is open
   useEffect(() => {
     if (open && isNarrow) {
-      document.body.classList.add('AnchorNav--no-bg-scroll')
+      document.body.classList.add(styles['AnchorNav--no-bg-scroll'])
     } else {
-      document.body.classList.remove('AnchorNav--no-bg-scroll')
+      document.body.classList.remove(styles['AnchorNav--no-bg-scroll'])
     }
   }, [open, isNarrow])
 
