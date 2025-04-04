d91be6d81a985b8279f741e9bb9380139849da01
Daniel Guillan <danielguillan@github.com>
2025-04-04 13:26:29 +0200

    Fix linting errors

--- apps/next-docs/content/components/ActionMenu/react.tsx
+++ apps/next-docs/content/components/ActionMenu/react.tsx
@@ -1,18 +1,26 @@
 'use client'
 import {PropTableValues} from '@primer/doctocat-nextjs/components'
 
-export const ActionMenuSizesProp = () => (
-  <PropTableValues values={['small', 'medium']} commaSeparated />
-)
-
-export const ActionMenuSelectionVariantProp = () => (
-  <PropTableValues values={['single', 'none']} commaSeparated />
-)
-
-export const ActionMenuAlignmentProp = () => (
-  <PropTableValues values={['start', 'end']} commaSeparated />
-)
+export const ActionMenuSizesProp = () => <PropTableValues values={['small', 'medium']} commaSeparated />
+export const ActionMenuSelectionVariantProp = () => <PropTableValues values={['single', 'none']} commaSeparated />
+export const ActionMenuAlignmentProp = () => <PropTableValues values={['start', 'end']} commaSeparated />
 
 export const ActionMenuOverlaySidesProp = () => (
-  <PropTableValues values={['bottom', 'bottom-end', 'bottom-start', 'left', 'left-end', 'left-start', 'right', 'right-end', 'right-start', 'top', 'top-end', 'top-start']} commaSeparated />
+  <PropTableValues
+    values={[
+      'bottom',
+      'bottom-end',
+      'bottom-start',
+      'left',
+      'left-end',
+      'left-start',
+      'right',
+      'right-end',
+      'right-start',
+      'top',
+      'top-end',
+      'top-start',
+    ]}
+    commaSeparated
+  />
 )
--- apps/next-docs/content/components/FAQ/react.tsx
+++ apps/next-docs/content/components/FAQ/react.tsx
@@ -3,14 +3,9 @@ import {PropTableValues} from '@primer/doctocat-nextjs/components'
 import {HeadingTags} from '../../../../../packages/react/src/Heading/Heading'
 import {AccordionToggleColors} from '../../../../../packages/react/src/Accordion/Accordion'
 
-// TODO Fix missing key prop error
 export const FAQChildrenProp = () => (
   <PropTableValues
-    values={[
-      <a href="#faq-heading">FAQ.Heading</a>,
-      <a href="#faq-subheading">FAQ.Subheading</a>,
-      <a href="#faq-item">FAQ.Item</a>,
-    ]}
+    values={['FAQ.Heading (#faq-heading)', 'FAQ.Subheading (#faq-subheading)', 'FAQ.Item (#faq-item)']}
     addLineBreaks
   />
 )
@@ -18,22 +13,16 @@ export const FAQChildrenProp = () => (
 export const FAQHeadingAlignProp = () => <PropTableValues values={['start', 'center']} addLineBreaks />
 export const FAQHeadingSizeProp = () => <PropTableValues values={['medium', 'large']} addLineBreaks />
 export const FAQHeadingAsProp = () => <PropTableValues values={[...HeadingTags]} addLineBreaks />
-export const FAQHeadingToggleColorProp = () => <PropTableValues values={AccordionToggleColors} addLineBreaks />
+export const FAQHeadingToggleColorProp = () => <PropTableValues values={[...AccordionToggleColors]} addLineBreaks />
 export const FAQSubheadingAsProp = () => <PropTableValues values={HeadingTags.slice(1)} addLineBreaks />
 export const FAQQuestionAsProp = () => <PropTableValues values={HeadingTags.slice(1)} addLineBreaks />
 
 export const FAQGroupChildrenProp = () => (
-  <PropTableValues
-    values={[<a href="#faq-required">FAQ</a>, <a href="#faqgroup-heading">FAQGroup.Heading</a>]}
-    addLineBreaks
-  />
+  <PropTableValues values={['FAQ (#faq-required)', 'FAQGroup.Heading (#faqgroup-heading)']} addLineBreaks />
 )
 
 export const FAQItemChildrenProp = () => (
-  <PropTableValues
-    values={[<a href="#faq-question">FAQ.Question</a>, <a href="#faq-answer">FAQ.Answer</a>]}
-    addLineBreaks
-  />
+  <PropTableValues values={['FAQ.Question (#faq-question)', 'FAQ.Answer (#faq-answer)']} addLineBreaks />
 )
 
-export const FAQGroupHeadingAsProp = () => <PropTableValues values={HeadingTags} addLineBreaks />
+export const FAQGroupHeadingAsProp = () => <PropTableValues values={[...HeadingTags]} addLineBreaks />
