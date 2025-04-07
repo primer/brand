d20099b41e765423c0452827118d52f62ffdc783
Daniel Guillan <danielguillan@github.com>
2025-04-07 13:05:39 +0200

    Move CustomVideo example in Card back to content file

--- apps/next-docs/content/components/Card/react.tsx
+++ apps/next-docs/content/components/Card/react.tsx
@@ -1,6 +1,6 @@
 'use client'
 import {PropTableValues} from '@primer/doctocat-nextjs/components'
-import CustomVideoPlayer from '../../../../docs/src/components/custom-video-player'
+
 
 export const CardVariantsProp = () => <PropTableValues values={['default', 'minimal']} commaSeparated />
 
@@ -65,10 +65,3 @@ export const CardLabelColorsProp = () => (
 )
 
 export const CardHeadingAsProp = () => <PropTableValues values={['h2', 'h3', 'h4', 'h5', 'h6']} commaSeparated />
-
-export const VerticalCardsVideo = () => (
-  <CustomVideoPlayer
-    width="100%"
-    src="https://github.com/primer/brand/assets/6951037/a9ceec00-59c9-4471-a274-f516790f43c9"
-  />
-)
