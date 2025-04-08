7faf8a2dcafab6edde1f4a34ab038a1069b4e128
Daniel Guillan <danielguillan@github.com>
2025-04-08 12:59:37 +0200

    Add isExternal prop to Link component

--- packages/react/src/Link/Link.module.css.d.ts
+++ packages/react/src/Link/Link.module.css.d.ts
@@ -1,9 +1,11 @@
 declare const styles: {
   readonly "Link": string;
   readonly "Link--accent": string;
+  readonly "Link--arrow-end": string;
   readonly "Link--arrow-none": string;
   readonly "Link--arrow-start": string;
   readonly "Link--default": string;
+  readonly "Link--is-external": string;
   readonly "Link--label": string;
   readonly "Link--large": string;
   readonly "Link-arrow": string;
