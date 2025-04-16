094389b42f17235324ec5afe845cf56ebe2e843b
rezrah <13340707+rezrah@users.noreply.github>
2025-04-16 14:02:03 +0100

    fix build error using static values

--- apps/next-docs/content/components/River/react.tsx
+++
@@ -1,6 +0,0 @@
-'use client'
-
-import {defaultRiverImageTextRatio, defaultRiverAlign} from '../../../../../packages/react/src/river/River'
-
-export const RiverAlignPropDefault = () => <>{defaultRiverAlign}</>
-export const RiverImageTextRatioDefault = () => <>{defaultRiverImageTextRatio}</>
