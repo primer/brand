a618f8f24c9c9974d36064ae8648934506e11dc3
Daniel Guillan <danielguillan@github.com>
2025-04-17 13:06:13 +0200

    Fixes

--- apps/next-docs/content/components/River/react.tsx
+++ apps/next-docs/content/components/River/react.tsx
@@ -2,5 +2,5 @@
 
 import {defaultRiverImageTextRatio, defaultRiverAlign} from '../../../../../packages/react/src/river/River'
 
-export const RiverAlignProp = () => <>{defaultRiverAlign}</>
-export const RiverImageTextRatio = () => <>{defaultRiverImageTextRatio}</>
+export const RiverAlignProp = () => defaultRiverAlign
+export const RiverImageTextRatio = () => defaultRiverImageTextRatio
