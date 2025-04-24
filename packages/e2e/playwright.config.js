fe09fd07b40d29a70493187d3883e462280f4659
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2025-04-24 15:40:36 +0100

    restore previous job name

--- packages/e2e/playwright.config.js
+++ packages/e2e/playwright.config.js
@@ -3,7 +3,7 @@ const config = {
   testDir: '../../packages/react',
   testMatch: /.*\.visual.spec\.ts/,
   outputDir: './playwright-test-results',
-  workers: process.env.CI ? 16 : undefined, // Use all available cores in GitHub Actions
+  workers: process.env.CI ? 16 : undefined, // Note: assumes a 16 core runner is enabled in the Action workflow
   use: {
     screenshot: 'only-on-failure',
     headless: true,
