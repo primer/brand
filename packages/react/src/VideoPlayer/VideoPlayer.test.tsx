c05050eb5dfd96fecdad84c6a82079728511da9b
Josh Farrant <joshfarrant@github.com>
2025-06-16 10:45:12 +0100

    add test asserting branding label

--- packages/react/src/VideoPlayer/VideoPlayer.test.tsx
+++ packages/react/src/VideoPlayer/VideoPlayer.test.tsx
@@ -188,4 +188,26 @@ describe('VideoPlayer', () => {
 
     expect(video.currentTime).toBe(42)
   })
+
+  it('shows the GitHub branding when showBranding is true', () => {
+    const {getByLabelText} = render(
+      <VideoPlayer poster="/example-poster.jpg" title="test video" showBranding>
+        <VideoPlayer.Source src="/example.mp4" />
+        <VideoPlayer.Track src="/example.vtt" default kind="subtitles" srcLang="en" label="English" />
+      </VideoPlayer>,
+    )
+
+    expect(getByLabelText('GitHub logo')).toBeInTheDocument()
+  })
+
+  it('does not show the GitHub branding when showBranding is false', () => {
+    const {queryByLabelText} = render(
+      <VideoPlayer poster="/example-poster.jpg" title="test video" showBranding={false}>
+        <VideoPlayer.Source src="/example.mp4" />
+        <VideoPlayer.Track src="/example.vtt" default kind="subtitles" srcLang="en" label="English" />
+      </VideoPlayer>,
+    )
+
+    expect(queryByLabelText('GitHub logo')).not.toBeInTheDocument()
+  })
 })
