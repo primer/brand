f9404a64b1431b6bcc94a7a257ef1812f810ea24
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2025-07-21 19:26:31 +0100

    add actionmenu translations

--- packages/react/src/ActionMenu/ActionMenu.stories.tsx
+++ packages/react/src/ActionMenu/ActionMenu.stories.tsx
@@ -1,23 +1,28 @@
 import {Meta, StoryFn} from '@storybook/react'
 import React from 'react'
 import {ActionMenu} from './ActionMenu'
+import {useTranslation} from 'react-i18next'
 
 export default {
   title: 'Components/ActionMenu',
   component: ActionMenu,
 } as Meta<typeof ActionMenu>
 
-const Template: StoryFn<typeof ActionMenu> = args => (
-  <ActionMenu {...args} onSelect={newValue => alert(newValue)}>
-    <ActionMenu.Button>Open menu</ActionMenu.Button>
-    <ActionMenu.Overlay aria-label="Actions">
-      <ActionMenu.Item value="Copy link pressed" selected>
-        Copy link
-      </ActionMenu.Item>
-      <ActionMenu.Item value="Quote reply pressed">Quote reply</ActionMenu.Item>
-      <ActionMenu.Item value="Edit comment pressed">Edit comment</ActionMenu.Item>
-    </ActionMenu.Overlay>
-  </ActionMenu>
-)
+const Template: StoryFn<typeof ActionMenu> = args => {
+  const {t} = useTranslation('ActionMenu')
+  
+  return (
+    <ActionMenu {...args} onSelect={newValue => alert(newValue)}>
+      <ActionMenu.Button>{t('open_menu')}</ActionMenu.Button>
+      <ActionMenu.Overlay aria-label={t('actions')}>
+        <ActionMenu.Item value="Copy link pressed" selected>
+          {t('copy_link')}
+        </ActionMenu.Item>
+        <ActionMenu.Item value="Quote reply pressed">{t('quote_reply')}</ActionMenu.Item>
+        <ActionMenu.Item value="Edit comment pressed">{t('edit_comment')}</ActionMenu.Item>
+      </ActionMenu.Overlay>
+    </ActionMenu>
+  )
+}
 
 export const Default = Template.bind({})
