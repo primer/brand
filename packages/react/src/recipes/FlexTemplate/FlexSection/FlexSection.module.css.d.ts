3874686c1a79c0ac11bf4ef28f177317d0638cce
Stefan KP <stefankp@github.com>
2025-08-07 15:23:28 +0200

    Copy FlexSection setup from F2

---
+++ packages/react/src/recipes/FlexTemplate/FlexSection/FlexSection.module.css.d.ts
@@ -0,0 +1,11 @@
+declare const styles: {
+  readonly "FlexSection": string;
+  readonly "FlexSection__ctaBanner": string;
+  readonly "FlexSection__heroBg": string;
+  readonly "FlexSection__heroImageContainer": string;
+  readonly "FlexSection__trailingSection": string;
+  readonly "Hero": string;
+  readonly "RiverContent": string;
+};
+export = styles;
+
