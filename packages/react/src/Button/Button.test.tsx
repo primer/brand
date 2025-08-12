bd133cbfde9d2b87d410d6cf2295b6d2dbdd2005
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2025-08-12 19:51:21 +0100

    revert debugging

--- packages/react/src/Button/Button.test.tsx
+++ packages/react/src/Button/Button.test.tsx
@@ -2,13 +2,14 @@ import React, {render, cleanup, act} from '@testing-library/react'
 import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 
-import {Button} from './Button'
+import {Button, ButtonSizes, ButtonVariants} from './Button'
 import {axe, toHaveNoViolations} from 'jest-axe'
 import {SearchIcon} from '@primer/octicons-react'
 
 expect.extend(toHaveNoViolations)
 
 describe('Button', () => {
+  const disabledClass = 'Button--disabled'
   afterEach(() => {
     cleanup()
   })
@@ -35,6 +36,26 @@ describe('Button', () => {
     expect(results).toHaveNoViolations()
   })
 
+  it('renders disabled state', async () => {
+    const {getByRole} = render(
+      <Button variant="primary" size="medium" disabled>
+        Primary Button
+      </Button>,
+    )
+    const btnEl = getByRole('button')
+
+    expect(btnEl).toHaveAttribute('disabled')
+    expect(btnEl.classList).toContain(disabledClass)
+  })
+
+  it('sets the disabled attribute to true when disabled prop is true and component is rendered as a button', () => {
+    const {getByRole} = render(<Button disabled>Primary Button</Button>)
+    const btnEl = getByRole('button')
+
+    expect(btnEl).toHaveAttribute('disabled')
+    expect(btnEl).not.toHaveAttribute('aria-disabled')
+  })
+
   it('sets the aria-disabled attribute to true when disabled prop is true and component is rendered as a link', () => {
     const {getByRole} = render(
       <Button disabled as="a" href="#">
@@ -45,6 +66,132 @@ describe('Button', () => {
 
     expect(linkEl).toHaveAttribute('aria-disabled')
     expect(linkEl).not.toHaveAttribute('disabled')
+  })
+
+  it('sets neither the disabled nor the aria-disabled attribute when disabled prop is false and component is rendered as a button', () => {
+    const {getByRole} = render(<Button disabled={false}>Primary Button</Button>)
+    const btnEl = getByRole('button')
+
+    expect(btnEl).not.toHaveAttribute('aria-disabled')
+    expect(btnEl).not.toHaveAttribute('disabled')
+  })
+
+  it('sets neither the disabled nor the aria-disabled attribute when disabled prop is false and component is rendered as a link', () => {
+    const {getByRole} = render(
+      <Button disabled={false} as="a" href="#">
+        Primary Button
+      </Button>,
+    )
+    const linkEl = getByRole('link', {name: 'Primary Button'})
+
+    expect(linkEl).not.toHaveAttribute('aria-disabled')
+    expect(linkEl).not.toHaveAttribute('disabled')
+  })
+
+  it('sets neither the disabled nor the aria-disabled attribute when disabled prop is undefined and component is rendered as a button', () => {
+    const {getByRole} = render(<Button disabled={undefined}>Primary Button</Button>)
+    const btnEl = getByRole('button')
+
+    expect(btnEl).not.toHaveAttribute('aria-disabled')
+    expect(btnEl).not.toHaveAttribute('disabled')
+  })
+
+  it('sets neither the disabled nor the aria-disabled attribute when disabled prop is undefined and component is rendered as a link', () => {
+    const {getByRole} = render(
+      <Button disabled={undefined} as="a" href="#">
+        Primary Button
+      </Button>,
+    )
+    const linkEl = getByRole('link', {name: 'Primary Button'})
+
+    expect(linkEl).not.toHaveAttribute('aria-disabled')
+    expect(linkEl).not.toHaveAttribute('disabled')
+  })
+
+  it('triggers focus event if not disabled and aria-disabled equals false', async () => {
+    const consoleSpy = jest.spyOn(global.console, 'log').mockImplementation()
+
+    render(
+      // eslint-disable-next-line no-console
+      <Button variant="primary" size="medium" aria-disabled="false" onFocus={() => console.log('Focused')}>
+        Primary Button
+      </Button>,
+    )
+    await act(async () => {
+      await userEvent.tab()
+    })
+
+    expect(consoleSpy).toHaveBeenCalled()
+  })
+
+  it('does not trigger focus event if disabled', async () => {
+    const consoleSpy = jest.spyOn(global.console, 'log').mockImplementation()
+
+    render(
+      // eslint-disable-next-line no-console
+      <Button variant="primary" size="medium" disabled onFocus={() => console.log('Focused')}>
+        Primary Button
+      </Button>,
+    )
+    await act(async () => {
+      await userEvent.tab()
+    })
+
+    expect(consoleSpy).toHaveBeenCalled()
+  })
+
+  it('does not trigger event if aria-disabled', async () => {
+    const consoleSpy = jest.spyOn(global.console, 'log').mockImplementation()
+
+    render(
+      // eslint-disable-next-line no-console
+      <Button variant="primary" size="medium" aria-disabled="true" onFocus={() => console.log('Focused')}>
+        Primary Button
+      </Button>,
+    )
+    await act(async () => {
+      await userEvent.tab()
+    })
+
+    expect(consoleSpy).toHaveBeenCalled()
+  })
+
+  it('does not trigger press event if disabled', async () => {
+    const consoleSpy = jest.spyOn(global.console, 'log').mockImplementation()
+
+    const {getByRole} = render(
+      // eslint-disable-next-line no-console
+      <Button variant="primary" size="medium" disabled onClick={() => console.log('Pressed')}>
+        Primary Button
+      </Button>,
+    )
+    const btnEl = getByRole('button')
+    btnEl.click()
+
+    expect(consoleSpy).toHaveBeenCalled()
+  })
+
+  it.each(ButtonVariants)('hides the arrow by default for all button variants except subtle', variant => {
+    const variantsWithArrow = ['subtle']
+
+    const {getByRole} = render(<Button variant={variant}>{variant}</Button>)
+    const btnEl = getByRole('button')
+    if (variantsWithArrow.includes(variant)) {
+      expect(btnEl.querySelector('.ExpandableArrow')).toBeInTheDocument()
+    } else {
+      expect(btnEl.querySelector('.ExpandableArrow')).not.toBeInTheDocument()
+    }
+  })
+
+  it.each(ButtonVariants)('can apply optional arrow for all variants', variant => {
+    const {getByRole} = render(
+      <Button variant={variant} hasArrow>
+        {variant}
+      </Button>,
+    )
+    const btnEl = getByRole('button')
+
+    expect(btnEl.querySelector('.ExpandableArrow')).toBeInTheDocument()
   })
 
   it('triggers correct animations on button arrow during a hover event', async () => {
@@ -159,6 +306,65 @@ describe('Button', () => {
       const el = getByTestId(visualId).querySelector('svg')
 
       expect(el).toHaveAttribute('aria-hidden', 'true')
+    }
+  })
+
+  it('passes the correct css class to icons when button is disabled', () => {
+    const expectedClass = 'Button__icon-visual--disabled'
+
+    const {getByTestId} = render(
+      <Button trailingVisual={<SearchIcon />} leadingVisual={<SearchIcon />} disabled>
+        Primary Button
+      </Button>,
+    )
+    const trailingVisualEl = getByTestId(Button.testIds.trailingVisual).querySelector('svg')
+    const leadingVisualEl = getByTestId(Button.testIds.leadingVisual).querySelector('svg')
+
+    expect(leadingVisualEl?.classList).toContain(expectedClass)
+    expect(trailingVisualEl?.classList).toContain(expectedClass)
+  })
+
+  it('accepts a native svg as a valid trailing or leading visual', () => {
+    const mockId = 'mock-svg'
+    const mockSvg = <svg data-testid={mockId} aria-label="Mock SVG" />
+
+    const {getByTestId} = render(
+      <Button trailingVisual={mockSvg} leadingVisual={mockSvg}>
+        Primary Button
+      </Button>,
+    )
+
+    const trailingIconSlot = getByTestId(Button.testIds.trailingVisual)
+    const leadingIconSlot = getByTestId(Button.testIds.leadingVisual)
+
+    expect(trailingIconSlot.childNodes).toContain(trailingIconSlot.querySelector(`[data-testid="${mockId}"]`))
+    expect(leadingIconSlot.childNodes).toContain(leadingIconSlot.querySelector(`[data-testid="${mockId}"]`))
+  })
+
+  it('spans full width when block prop is true', () => {
+    const {getByRole} = render(<Button block={true}>Full-Width Button</Button>)
+
+    const btnEl = getByRole('button')
+
+    expect(btnEl.classList).toContain('Button--block')
+  })
+
+  it('is not block by default', () => {
+    const {getByRole} = render(<Button>Default Button</Button>)
+
+    const btnEl = getByRole('button')
+
+    expect(btnEl.classList).not.toContain('Button--block')
+  })
+
+  it('applies the correct class for each size', () => {
+    for (const i in ButtonSizes) {
+      const size = ButtonSizes[i]
+      const {getAllByRole} = render(<Button size={size}>Button</Button>)
+
+      const btnEl = getAllByRole('button')
+
+      expect(btnEl[i].classList).toContain(`Button--size-${size}`)
     }
   })
 })
