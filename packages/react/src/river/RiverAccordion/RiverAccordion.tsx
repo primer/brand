1993c9457238d7967806d348b6dbfee459919d30
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2025-12-02 14:44:44 +0000

    move inert usage to effect

--- packages/react/src/river/RiverAccordion/RiverAccordion.tsx
+++ packages/react/src/river/RiverAccordion/RiverAccordion.tsx
@@ -1,4 +1,4 @@
-import React, {createContext, forwardRef, useCallback, useContext, useMemo, useState} from 'react'
+import React, {createContext, forwardRef, useCallback, useContext, useMemo, useState, useRef, useEffect} from 'react'
 import {clsx} from 'clsx'
 import {PlusIcon} from '@primer/octicons-react'
 
@@ -110,6 +110,13 @@ const RiverAccordionItem = ({className, index, children, ...props}: RiverAccordi
   const {openIndex} = useRiverAccordionContext()
   const panelId = useId()
   const isOpen = index === openIndex
+  const panelRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (panelRef.current) {
+      panelRef.current.toggleAttribute('inert', !isOpen)
+    }
+  }, [isOpen])
 
   const itemContextValue = useMemo(
     () => ({
@@ -157,12 +164,7 @@ const RiverAccordionItem = ({className, index, children, ...props}: RiverAccordi
         {...props}
       >
         {heading}
-        <div
-          className={styles.RiverAccordion__panel}
-          id={panelId}
-          aria-hidden={!isOpen}
-          {...(!isOpen ? ({inert: true} as React.HTMLAttributes<HTMLDivElement>) : undefined)}
-        >
+        <div ref={panelRef} className={styles.RiverAccordion__panel} id={panelId} aria-hidden={!isOpen}>
           {content}
           {visual}
         </div>
