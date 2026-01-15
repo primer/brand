b2f94d5b9afdd30d1df1c2e6768756b7def89ede
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2026-01-15 13:27:05 +0000

    fix cursor positioning

--- packages/react/src/animation/Animation.module.css.d.ts
+++ packages/react/src/animation/Animation.module.css.d.ts
@@ -1,19 +1,20 @@
 declare const styles: {
-  readonly Animation: string
-  readonly 'Animation--active': string
-  readonly 'Animation--fade-in': string
-  readonly 'Animation--fade-out': string
-  readonly 'Animation--fill-in-right': string
-  readonly 'Animation--fill-in-top': string
-  readonly 'Animation--scale-in': string
-  readonly 'Animation--scale-in-down': string
-  readonly 'Animation--scale-in-left': string
-  readonly 'Animation--scale-in-right': string
-  readonly 'Animation--scale-in-up': string
-  readonly 'Animation--slide-in-down': string
-  readonly 'Animation--slide-in-left': string
-  readonly 'Animation--slide-in-right': string
-  readonly 'Animation--slide-in-up': string
-  readonly 'Animation--reveal-in-up': string
-}
-export = styles
+  readonly "Animation": string;
+  readonly "Animation--active": string;
+  readonly "Animation--fade-in": string;
+  readonly "Animation--fade-out": string;
+  readonly "Animation--fill-in-right": string;
+  readonly "Animation--fill-in-top": string;
+  readonly "Animation--reveal-in-up": string;
+  readonly "Animation--scale-in": string;
+  readonly "Animation--scale-in-down": string;
+  readonly "Animation--scale-in-left": string;
+  readonly "Animation--scale-in-right": string;
+  readonly "Animation--scale-in-up": string;
+  readonly "Animation--slide-in-down": string;
+  readonly "Animation--slide-in-left": string;
+  readonly "Animation--slide-in-right": string;
+  readonly "Animation--slide-in-up": string;
+};
+export = styles;
+
