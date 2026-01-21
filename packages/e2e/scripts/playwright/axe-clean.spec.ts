56df4575ad66966c7d74e65221da26d842172e18
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2026-01-21 10:19:24 +0000

    increase timeout on recipe examples

--- packages/e2e/scripts/playwright/axe-clean.spec.ts
+++ packages/e2e/scripts/playwright/axe-clean.spec.ts
@@ -123,6 +123,10 @@ function printViolations(violations: Result[]) {
 
 const testsWithCustomDelay = {
   'components-subdomainnavbar--mobile-menu-open': 5000, // takes a while for the menu to open
+  'components-hero-examples--custom-background-inline-end-padded-video': 5000, // recipe / example that features long animation sequence
+  'components-hero-examples--custom-background-block-end-video': 5000, // recipe / example that features long animation sequence
+  'components-hero-examples--custom-background-inline-end-padded-image': 5000, // recipe / example that features long animation sequence
+  'components-hero-examples--custom-background-block-end-image': 5000, // recipe / example that features long animation sequence
 }
 const defaultDelay = 1000
 
