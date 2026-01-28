d6d648af735f4673a413dcb1bc7880ac4089d051
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2026-01-28 12:58:06 +0000

    add enableBorder prop

--- packages/react/src/Hero/sub-components/HeroImage.tsx
+++ packages/react/src/Hero/sub-components/HeroImage.tsx
@@ -8,6 +8,7 @@ import styles from '../Hero.module.css'
 
 export type HeroImageProps = {
   position?: HeroMediaPositions
+  enableBorder?: boolean
 } & ImageProps &
   BaseProps<HTMLImageElement>
 
