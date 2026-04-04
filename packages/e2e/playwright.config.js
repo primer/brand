a1b05732e0b23a35ad2844228570c689bf97890b
Reza Rahman <13340707+rezrah@users.noreply.github.com>
2026-04-04 17:46:51 +0100

    increase timeouts and delays for CI

--- packages/e2e/playwright.config.js
+++ packages/e2e/playwright.config.js
@@ -4,10 +4,10 @@ const config = {
   testMatch: /.*\.visual.spec\.ts/,
   outputDir: './playwright-test-results',
   workers:
-    Number(process.env.VRT_WORKERS) || (process.env.CI ? 8 : Math.max(4, Math.min(6, require('os').cpus().length / 2))),
+    Number(process.env.VRT_WORKERS) || (process.env.CI ? 6 : Math.max(4, Math.min(6, require('os').cpus().length / 2))),
   fullyParallel: true,
   retries: process.env.CI ? 1 : 0,
-  timeout: 15000,
+  timeout: process.env.CI ? 30000 : 15000,
   maxFailures: 2,
   use: {
     screenshot: 'only-on-failure',
@@ -17,14 +17,15 @@ const config = {
     env: {
       NODE_ENV: 'test',
     },
-    actionTimeout: 10000,
-    navigationTimeout: 10000,
+    actionTimeout: process.env.CI ? 15000 : 10000,
+    navigationTimeout: process.env.CI ? 15000 : 10000,
     launchOptions: {
       args: ['--disable-dev-shm-usage', '--disable-extensions', '--no-sandbox'],
     },
   },
   expect: {
     toHaveScreenshot: {
+      timeout: process.env.CI ? 10000 : 5000,
       maxDiffPixelRatio: 0.0002,
       animations: 'disabled',
     },
