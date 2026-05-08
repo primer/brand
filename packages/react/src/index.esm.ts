97a3f50e5d2f44d8863c3c785f212ee7c3d46e84
Rez <13340707+rezrah@users.noreply.github.com>
2026-05-08 11:12:22 +0100

    Add ESM bundle support (#1280)
    * add ESM support
    
    * address copilot feedback
    
    * fix icons
    
    * fix linter
    
    * fix octicons externals
    
    * fix typing erroprs
    
    * fix cjs path for astro
    
    * upgrade astro to v6 and switch to ESM
    
    * update astro to support matrix strategy for cjs and esm
    
    * add stylesheets file to side effects
    
    * upgrade to vite 8
    
    * update package-lock
    
    * fix build
    
    * update vite config
    
    * pin postcss
    
    * fix side-effects path

---
+++ packages/react/src/index.esm.ts
@@ -0,0 +1,7 @@
+/**
+ * ESM entry point for @primer/react-brand.
+ *
+ * Re-exports everything from the main index, which includes
+ * stylesheets.ts (global CSS: reset, design tokens, utilities).
+ */
+export * from './index'
