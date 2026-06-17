99af142ad59cd719801058f11ab61c2eeeefcbaa
Daniel Guillan <danielguillan@github.com>
2026-06-17 11:14:09 +0200

    Add link small variant (#1376)

--- packages/react/src/Link/Link.features.stories.tsx
+++ packages/react/src/Link/Link.features.stories.tsx
@@ -1,6 +1,8 @@
+import React from 'react'
 import type {Meta, StoryObj} from '@storybook/react'
 
-import {type LinkProps} from '.'
+import {Link, type LinkProps} from '.'
+import {Stack} from '../Stack'
 import baseMeta from './Link.stories'
 
 const meta: Meta<LinkProps> = {...baseMeta, title: 'Components/Link/Features'}
@@ -8,12 +10,20 @@ export default meta
 
 type Story = StoryObj<LinkProps>
 
-export const Large: Story = {
-  args: {
-    size: 'large',
-    href: '#',
-    children: 'Large size',
-  },
+export const Sizes: Story = {
+  render: () => (
+    <Stack direction="vertical" alignItems="flex-start" padding="none">
+      <Link href="#" size="small">
+        Small size
+      </Link>
+      <Link href="#" size="medium">
+        Medium size
+      </Link>
+      <Link href="#" size="large">
+        Large size
+      </Link>
+    </Stack>
+  ),
 }
 
 export const ArrowStart: Story = {
