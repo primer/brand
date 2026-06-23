bd95d2ca70dbeed1e6e928fcff13b68f7cd5b3ca
Daniel Guillan <danielguillan@github.com>
2026-06-23 12:24:56 +0200

    Update snapshots

--- packages/react/src/NavList/NavList.visual.spec.ts
+++ packages/react/src/NavList/NavList.visual.spec.ts
@@ -70,6 +70,17 @@ test.describe('Visual Comparison: NavList', () => {
     await expect(page).toHaveScreenshot({fullPage: true})
   })
 
+  test('NavList / With Visuals', async ({page}) => {
+    await page.goto(
+      'http://localhost:6006/iframe.html?args=&id=components-navlist-features--with-visuals&viewMode=story',
+      {waitUntil: 'networkidle'},
+    )
+    await page.locator('body.sb-show-main').waitFor({state: 'visible'})
+
+    await page.waitForTimeout(500)
+    await expect(page).toHaveScreenshot({fullPage: true})
+  })
+
   test('NavList / Dark Mode', async ({page}) => {
     await page.goto(
       'http://localhost:6006/iframe.html?args=&id=components-navlist-features--dark-mode&viewMode=story',
