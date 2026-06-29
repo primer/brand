04e5a9dbf4e5aa666978e2631c0f315738e40233
Rez <13340707+rezrah@users.noreply.github.com>
2026-06-29 10:59:03 +0100

    Bundle docs in the package and clean up private files (#1382)
    * bundle docs and clean up package files
    
    * fix merge conflict
    
    * clean comments
    
    * more cleanup

--- packages/react/eslint.config.mjs
+++ packages/react/eslint.config.mjs
@@ -3,6 +3,9 @@ import rootConfig from '../../eslint.config.mjs'
 export default [
   ...rootConfig,
   {
+    ignores: ['docs/**'], // docs generated and bundled in the package, not published docs
+  },
+  {
     languageOptions: {
       parserOptions: {
         tsconfigRootDir: import.meta.dirname,
